b30c6523185d782b7989697ab9e05151e9a9d91b
Peter Wang <tjaden@users.sourceforge.net>
2000-12-21 01:36:24 +0000

    Grzegorz updated extrans to work in any color depth

--- examples/extrans.c
+++ examples/extrans.c
@@ -27,23 +27,21 @@ int main(int argc, char *argv[])
 {
    PALETTE pal;
    BITMAP *s;
-   BITMAP *temp;
    BITMAP *spotlight;
+   BITMAP *truecolor_spotlight;
    BITMAP *background;
    int i, x, y;
    char buf[256];
    char *filename;
 
    allegro_init(); 
-   install_keyboard(); 
-   install_timer(); 
+   install_keyboard();
+   install_timer();
    install_mouse(); 
 
-   if (set_gfx_mode(GFX_AUTODETECT, 320, 200, 0, 0) != 0) {
-      if (set_gfx_mode(GFX_AUTODETECT, 640, 480, 0, 0) != 0) {
-	 allegro_message("Couldn't set an 8bpp resolution!?!\n%s", allegro_error);
-	 return 1;
-      }
+   if (set_gfx_mode(GFX_SAFE, 320, 200, 0, 0) != 0) {
+      allegro_message("Couldn't set a graphic resolution!\n%s", allegro_error);
+      return 1;
    }
 
    /* load the main screen image */
@@ -74,12 +72,12 @@ int main(int argc, char *argv[])
    set_palette(pal);
 
    s = create_bitmap(320, 200);
-   spotlight = create_bitmap(128, 128);
-   temp = create_bitmap(128, 128);
+   spotlight = create_bitmap_ex(8, 128, 128);
+   truecolor_spotlight = create_bitmap_ex(32, 128, 128);
 
-   /* generate a spotlight image */
+   /* generate an 8bpp spotlight image */
    clear(spotlight);
-   for(i=0; i<256; i++)
+   for (i=0; i<256; i++)
       circlefill(spotlight, 64, 64, 64-i/4, i);
 
    /* select the lighting table */
@@ -89,19 +87,44 @@ int main(int argc, char *argv[])
    do {
       poll_mouse();
 
-      x = mouse_x - 64;
-      y = mouse_y - 64;
+      x = mouse_x - SCREEN_W/2 - 64 + 160;
+      y = mouse_y - SCREEN_H/2 - 64 + 100;
 
       clear(s);
 
-      blit(background, s, x, y, x, y, 128, 128);
-      draw_trans_sprite(s, spotlight, x, y);
+      /* unluckily we have to do something 'weird' for truecolor modes */
+      if (bitmap_color_depth(screen) != 8) {
+	 /* copy background on the truecolor spotlight */
+	 blit(background, truecolor_spotlight, x, y , 0, 0, 127, 127);
+	 /* set special write alpha blender */
+	 set_write_alpha_blender();
+	 drawing_mode(DRAW_MODE_TRANS, 0, 0, 0);
+	 /* draw the alpha channel */
+	 rectfill(truecolor_spotlight, 0, 0, 128, 128, 0);
+	 draw_trans_sprite(truecolor_spotlight, spotlight, 0, 0);
+	 /* set alpha blender and draw on the 's' bitmap */
+	 drawing_mode(DRAW_MODE_SOLID, 0, 0, 0);
+	 set_alpha_blender();
+	 draw_trans_sprite(s, truecolor_spotlight, x, y);
+	 /* restore a not-alpha blender */
+	 set_trans_blender(0, 0, 0, 128);
+      }
+      else {
+         blit(background, s, x, y, x, y, 128, 128);
+         draw_trans_sprite(s, spotlight, x, y);
+      }
 
-      blit(s, screen, 0, 0, 0, 0, 320, 200);
+      blit(s, screen, 0, 0, SCREEN_W/2 - 160, SCREEN_H/2 - 100, 320, 200);
 
    } while (!keypressed());
 
    clear_keybuf();
+
+   /* for the next part we want spotlight and s to share color depth */
+   if (bitmap_color_depth(spotlight) != bitmap_color_depth(s)) {
+      destroy_bitmap(spotlight);
+      spotlight = create_bitmap(128, 128);
+   }
 
    /* generate an overlay image (just shrink the main image) */
    stretch_blit(background, spotlight, 0, 0, 320, 200, 0, 0, 128, 128);
@@ -109,17 +132,20 @@ int main(int argc, char *argv[])
    /* select the translucency table */
    color_map = &trans_table;
 
+   /* select translucency blender */
+   set_trans_blender(0, 0, 0, 128);
+
    /* display a translucent overlay */
    do {
       poll_mouse();
 
-      x = mouse_x - 64;
-      y = mouse_y - 64;
+      x = mouse_x - SCREEN_W/2 - 64 + 160;
+      y = mouse_y - SCREEN_H/2 - 64 + 100;
 
       blit(background, s, 0, 0, 0, 0, 320, 200);
       draw_trans_sprite(s, spotlight, x, y);
 
-      blit(s, screen, 0, 0, 0, 0, 320, 200);
+      blit(s, screen, 0, 0, SCREEN_W/2 - 160, SCREEN_H/2 - 100, 320, 200);
 
    } while (!keypressed());
 
@@ -127,7 +153,7 @@ int main(int argc, char *argv[])
 
    destroy_bitmap(s);
    destroy_bitmap(spotlight);
-   destroy_bitmap(temp);
+   destroy_bitmap(truecolor_spotlight);
    destroy_bitmap(background);
 
    return 0;
