24b54ac16b90685cfd433d63037d8bc8f1ec1a3b
Isaac Cruz <icruz@users.sourceforge.net>
2001-01-16 19:28:31 +0000

    DIGMID added by Javier Gonz�lez

--- src/win/wmidi.c
+++ src/win/wmidi.c
@@ -67,7 +67,7 @@ _DRIVER_INFO *_get_midi_driver_list()
       if (num_drivers)  /* include the MIDI mapper, which is -1 */
 	 num_drivers++;
 
-      driver_list = malloc(sizeof(_DRIVER_INFO) * (num_drivers+2));
+      driver_list = malloc(sizeof(_DRIVER_INFO) * (num_drivers+3));
 
       for (i=0; i<num_drivers; i++) {
 	 driver = malloc(sizeof(MIDI_DRIVER));
@@ -92,13 +92,17 @@ _DRIVER_INFO *_get_midi_driver_list()
 	 driver_list[i].autodetect = TRUE;
       }
 
-      driver_list[i].id = MIDI_NONE;
-      driver_list[i].driver = &midi_none;
+      driver_list[i].id = MIDI_DIGMID;
+      driver_list[i].driver = &midi_digmid;
       driver_list[i].autodetect = TRUE;
 
-      driver_list[i+1].id = 0;
-      driver_list[i+1].driver = NULL;
-      driver_list[i+1].autodetect = FALSE;
+      driver_list[i+1].id = MIDI_NONE;
+      driver_list[i+1].driver = &midi_none;
+      driver_list[i+1].autodetect = TRUE;
+
+      driver_list[i+2].id = 0;
+      driver_list[i+2].driver = NULL;
+      driver_list[i+2].autodetect = FALSE;
    }
 
    return driver_list;
