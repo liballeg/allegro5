276490f4325a89f655a33cd5bd5cb32959e29b2b
Eric Botcazou <ebotcazou@users.sourceforge.net>
2001-06-09 09:54:00 +0000

    Post Annie's patch update

--- src/fsel.c
+++ src/fsel.c
@@ -890,7 +890,6 @@ int file_select_ex(AL_CONST char *message, char *path, AL_CONST char *ext, int s
  */
 int file_select(AL_CONST char *message, char *path, AL_CONST char *ext)
 {
-   return file_select_ex(message, path, ext, 80*uwidth_max(U_CURRENT),
-			 OLD_FILESEL_WIDTH, OLD_FILESEL_HEIGHT);
+   return file_select_ex(message, path, ext, 512, OLD_FILESEL_WIDTH, OLD_FILESEL_HEIGHT);
 }
 
