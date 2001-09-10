99f59fde17e9172a1fcd9535e057d2d73569844f
Angelo Mottola <lillo@users.sourceforge.net>
2001-09-10 12:25:56 +0000

    Small aesthetic fix

--- examples/excolmap.c
+++ examples/excolmap.c
@@ -138,6 +138,10 @@ int main()
 
    create_color_table(&negative_table, pal, return_negative_color, callback_func);
 
+   #ifdef ALLEGRO_CONSOLE_OK
+      printf("\n\n");
+   #endif
+   
    if (set_gfx_mode(GFX_SAFE, 320, 200, 0, 0) != 0) {
       set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
       allegro_message("Unable to set any graphic mode\n%s\n", allegro_error);
