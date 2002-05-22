37db7416f3ce40f276352fae414618aab501e3d0
Peter Wang <tjaden@users.sourceforge.net>
2002-05-22 01:53:11 +0000

    Fixed a deadlock that occurred when we received an X close event

--- src/unix/uthreads.c
+++ src/unix/uthreads.c
@@ -30,19 +30,39 @@ static void bg_man_pthreads_enable_interrupts (void);
 static void bg_man_pthreads_disable_interrupts (void);
 
 #define MAX_FUNCS 16
+
 static bg_func funcs[MAX_FUNCS];
+static bg_func stale_funcs[MAX_FUNCS];
 static int max_func; /* highest+1 used entry */
 
 static pthread_t thread;
 static pthread_mutex_t cli_mutex;
 static pthread_cond_t cli_cond;
 static int cli_count;
+static pthread_mutex_t stale_mutex;
 
-static void block_all_signals(void)
+static void block_all_signals (void)
 {
 	sigset_t mask;
-	sigfillset(&mask);
-	pthread_sigmask(SIG_BLOCK, &mask, NULL);
+	sigfillset (&mask);
+	pthread_sigmask (SIG_BLOCK, &mask, NULL);
+}
+
+static int really_unregister_func (bg_func f)
+{
+	int i;
+
+	for (i = 0; i < max_func && funcs[i] != f; i++);
+	if (i == max_func)
+		return -1;
+	else {
+		funcs[i] = NULL;			
+		if (i+1 == max_func)
+			do {
+				max_func--;
+			} while ((max_func > 0) && !funcs[max_func-1]);
+		return 0;
+	}
 }
 
 static void *bg_man_pthreads_threadfunc (void *arg)
@@ -68,10 +88,23 @@ static void *bg_man_pthreads_threadfunc (void *arg)
 			i = i * (TIMERS_PER_SECOND/100) / 10000L;
 
 			pthread_mutex_lock (&cli_mutex);
+			
+			/* wait until interrupts are enabled */
 			while (cli_count > 0)
 				pthread_cond_wait (&cli_cond, &cli_mutex);
+
+			/* call all the callbacks */
 			for (n = 0; n < max_func; n++)
 				if (funcs[n]) funcs[n](1);
+
+			/* remove stale functions */
+			pthread_mutex_lock (&stale_mutex);
+			for (n = 0; stale_funcs[n]; n++) {
+				really_unregister_func (stale_funcs[n]);
+				stale_funcs[n] = NULL;
+			}
+			pthread_mutex_unlock (&stale_mutex);
+
 			pthread_mutex_unlock (&cli_mutex);
 		}
 
@@ -88,16 +121,18 @@ static int bg_man_pthreads_init (void)
 {
 	int i;
 	for (i = 0; i < MAX_FUNCS; i++)
-		funcs[i] = NULL;
+		funcs[i] = stale_funcs[i] = NULL;
 	max_func = 0;
 
 	cli_count = 0;
 	pthread_mutex_init (&cli_mutex, NULL);
 	pthread_cond_init (&cli_cond, NULL);
+	pthread_mutex_init (&stale_mutex, NULL);	
 
 	if (pthread_create (&thread, NULL, bg_man_pthreads_threadfunc, NULL)) {
 		pthread_mutex_destroy (&cli_mutex);
 		pthread_cond_destroy (&cli_cond);
+		pthread_mutex_destroy (&stale_mutex);
 		return -1;
 	}
 
@@ -110,6 +145,7 @@ static void bg_man_pthreads_exit (void)
 	pthread_join (thread, NULL);
 	pthread_mutex_destroy (&cli_mutex);
 	pthread_cond_destroy (&cli_cond);
+	pthread_mutex_destroy (&stale_mutex);
 }
 
 static int bg_man_pthreads_register_func (bg_func f)
@@ -131,21 +167,43 @@ static int bg_man_pthreads_register_func (bg_func f)
 
 static int bg_man_pthreads_unregister_func (bg_func f)
 {
-	int i, ret = 0;
-	bg_man_pthreads_disable_interrupts ();
+	/* We used to just bg_man_pthread_disable_interrupts(), remove `f',
+	 * then bg_man_pthread_enable_interrupts().
+	 *
+	 * However, the X system driver's input handler is a bg_man callback.
+	 * When it receives a close event, it calls exit(), which calls
+	 * allegro_exit().  Eventually various subsystems will try to
+	 * unregister their callbacks, i.e. call this function.  But we're in
+	 * the middle of input handler call, and `cli_mutex' is still locked.
+	 *
+	 * So we have to queue up the unregistration requests and process
+	 * them later.  This introduces the subtlety that unregistration is
+	 * no longer immediate.
+	 */
 
-	for (i = 0; funcs[i] != f && i < max_func; i++);
-	if (i == max_func)
-		ret = -1;
+	int i, ret;
+
+	if (pthread_mutex_trylock (&cli_mutex) == EBUSY) {
+		pthread_mutex_lock (&stale_mutex);
+		for (i = 0; i < max_func && funcs[i] != f; i++);
+		if (i == max_func)
+			ret = -1;
+		else {
+			for (i = 0; i < MAX_FUNCS; i++) {
+				if (!stale_funcs[i]) {
+					stale_funcs[i] = f;
+					break;
+				}
+			}
+			ret = 0;
+		}
+		pthread_mutex_unlock (&stale_mutex);
+	}
 	else {
-		funcs[i] = NULL;
-		if (i+1 == max_func)
-			do {
-				max_func--;
-			} while ((max_func > 0) && !funcs[max_func-1]);
+		ret = really_unregister_func (f);
+		pthread_mutex_unlock (&cli_mutex);
 	}
 
-	bg_man_pthreads_enable_interrupts ();
 	return ret;
 }
 
