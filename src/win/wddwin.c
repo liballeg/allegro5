ffa382d1f7eb9c012155767a70cf40abec6d7b20
Eric Botcazou <ebotcazou@users.sourceforge.net>
2002-06-22 07:27:39 +0000

    Fixed a race condition related to the direct updating mode toggling option

--- src/win/wddwin.c
+++ src/win/wddwin.c
@@ -444,6 +444,10 @@ static int wnd_set_windowed_coop(void)
  */
 static int verify_color_depth (int color_depth)
 {
+   AL_CONST char *ddu;
+   char tmp1[64], tmp2[128];
+   int i;
+
    if ((gfx_directx_compare_color_depth(color_depth) == 0) && (color_depth != 8)) {
       /* the color depths match */ 
       update_window = update_matching_window;
@@ -460,6 +464,18 @@ static int verify_color_depth (int color_depth)
          return -1;
 
       update_window = update_colorconv_window;
+
+      /* read direct updating configuration variable */
+      ddu = get_config_string(uconvert_ascii("graphics", tmp1),
+                              uconvert_ascii("disable_direct_updating", tmp2),
+                              NULL);
+
+      if ((ddu) && ((i = ugetc(ddu)) != 0) && ((i == 'y') || (i == 'Y') || (i == '1')))
+         direct_updating_mode_enabled = FALSE;
+      else
+         direct_updating_mode_enabled = TRUE;
+
+      direct_updating_mode_on = direct_updating_mode_enabled;
    }
 
    return 0;
@@ -535,8 +551,6 @@ static struct BITMAP *init_directx_win(int w, int h, int v_w, int v_h, int color
 {
    RECT win_size;
    unsigned char *cmap;
-   char tmp1[64], tmp2[128];
-   AL_CONST char *ddu;
    HRESULT hr;
    int i;
 
@@ -671,18 +685,6 @@ static struct BITMAP *init_directx_win(int w, int h, int v_w, int v_h, int color
 
    /* grab input devices */
    win_grab_input();
-
-   /* read direct updating configuration variable */
-   ddu = get_config_string(uconvert_ascii("graphics", tmp1),
-                           uconvert_ascii("disable_direct_updating", tmp2),
-                           NULL);
-
-   if ((ddu) && ((i = ugetc(ddu)) != 0) && ((i == 'y') || (i == 'Y') || (i == '1')))
-      direct_updating_mode_enabled = FALSE;
-   else
-      direct_updating_mode_enabled = TRUE;
-
-   direct_updating_mode_on = direct_updating_mode_enabled;
 
    _exit_critical();
 
