df1ea88706ded33e1892662ee7b19be4680b6a8c
Eric Botcazou <ebotcazou@users.sourceforge.net>
2003-05-26 14:48:40 +0000

    Backported from 4.1.x: - lillo fixed the weird 8bpp C draw_trans_sprite() behaviour, - fixed the behaviour of the C version of draw_trans_sprite() with 32-bit RGBA sprites.

--- src/c/cspr.h
+++ src/c/cspr.h
@@ -460,10 +460,15 @@ void FUNC_LINEAR_DRAW_TRANS_SPRITE(BITMAP *dst, BITMAP *src, int dx, int dy)
 
 	 for (x = w - 1; x >= 0; s++, INC_PIXEL_PTR(ds), INC_PIXEL_PTR(dd), x--) {
 	    unsigned long c = *s;
+#if PP_DEPTH == 8
+	    c = DTS_BLEND(blender, GET_PIXEL(ds), c);
+	    PUT_PIXEL(dd, c);
+#else
             if (!IS_SPRITE_MASK(src, c)) {
 	       c = DTS_BLEND(blender, GET_PIXEL(ds), c);
 	       PUT_PIXEL(dd, c);
 	    }
+#endif
 	 }
       }
 
@@ -479,10 +484,15 @@ void FUNC_LINEAR_DRAW_TRANS_SPRITE(BITMAP *dst, BITMAP *src, int dx, int dy)
 
 	 for (x = w - 1; x >= 0; INC_PIXEL_PTR(s), INC_PIXEL_PTR(ds), INC_PIXEL_PTR(dd), x--) {
 	    unsigned long c = GET_MEMORY_PIXEL(s);
+#if PP_DEPTH == 8
+	    c = DTS_BLEND(blender, GET_PIXEL(ds), c);
+	    PUT_PIXEL(dd, c);
+#else
 	    if (!IS_SPRITE_MASK(src, c)) {
 	       c = DTS_BLEND(blender, GET_PIXEL(ds), c);
 	       PUT_PIXEL(dd, c);
 	    }
+#endif
 	 }
       }
 
@@ -495,10 +505,15 @@ void FUNC_LINEAR_DRAW_TRANS_SPRITE(BITMAP *dst, BITMAP *src, int dx, int dy)
 
 	 for (x = w - 1; x >= 0; INC_PIXEL_PTR(s), INC_PIXEL_PTR(d), x--) {
 	    unsigned long c = GET_MEMORY_PIXEL(s);
+#if PP_DEPTH == 8
+	    c = DTS_BLEND(blender, GET_MEMORY_PIXEL(d), c);
+	    PUT_MEMORY_PIXEL(d, c);
+#else
 	    if (!IS_SPRITE_MASK(src, c)) {
 	       c = DTS_BLEND(blender, GET_MEMORY_PIXEL(d), c);
 	       PUT_MEMORY_PIXEL(d, c);
 	    }
+#endif
 	 }
       }
    }
@@ -560,8 +575,10 @@ void FUNC_LINEAR_DRAW_TRANS_RGBA_SPRITE(BITMAP *dst, BITMAP *src, int dx, int dy
       for (x = w - 1; x >= 0; s++, INC_PIXEL_PTR(ds), INC_PIXEL_PTR(dd), x--) {
 	 unsigned long c = *s;
 
-	 c = RGBA_BLEND(blender, GET_PIXEL(ds), c);
-	 PUT_PIXEL(dd, c);
+	 if (c != MASK_COLOR_32) {
+	    c = RGBA_BLEND(blender, GET_PIXEL(ds), c);
+	    PUT_PIXEL(dd, c);
+	 }
       }
    }
 
