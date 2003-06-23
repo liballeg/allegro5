ebf8850c7d8c478ffbec925ec4cf46c1095bc2ce
Eric Botcazou <ebotcazou@users.sourceforge.net>
2003-06-23 07:59:24 +0000

    Updated to minimize differences with mainline.

--- misc/zipup.sh
+++ misc/zipup.sh
@@ -71,14 +71,13 @@ utod()
    done
 }
 
-export UNIX_TOOLS=1
 
 # generate dependencies for DJGPP
 echo "Generating DJGPP dependencies..."
 
 ./fix.sh djgpp --quick
 
-make depend
+make depend UNIX_TOOLS=1 CC=gcc
 
 
 # generate dependencies for Watcom
@@ -86,7 +85,7 @@ echo "Generating Watcom dependencies..."
 
 ./fix.sh watcom --quick
 
-make depend
+make depend UNIX_TOOLS=1 CC=gcc
 
 
 # generate dependencies for MSVC
@@ -94,7 +93,7 @@ echo "Generating MSVC dependencies..."
 
 ./fix.sh msvc --quick
 
-make depend
+make depend UNIX_TOOLS=1 CC=gcc
 
 
 # generate dependencies for MinGW32
@@ -102,7 +101,7 @@ echo "Generating MinGW32 dependencies..."
 
 ./fix.sh mingw32 --quick
 
-make depend
+make depend UNIX_TOOLS=1 CC=gcc
 
 
 # generate dependencies for Borland C++
@@ -110,7 +109,7 @@ echo "Generating BCC32 dependencies..."
 
 ./fix.sh bcc32 --quick
 
-make depend
+make depend UNIX_TOOLS=1 CC=gcc
 
 
 # generate dependencies for BeOS
@@ -118,7 +117,7 @@ echo "Generating BeOS dependencies..."
 
 ./fix.sh beos --quick
 
-make depend
+make depend UNIX_TOOLS=1 CC=gcc
 
 
 # generate dependencies for QNX
@@ -126,9 +125,7 @@ echo "Generating QNX dependencies..."
 
 ./fix.sh qnx --quick
 
-make depend
-
-unset UNIX_TOOLS
+make depend UNIX_TOOLS=1 CC=gcc
 
 
 # generate the DLL export definition files for Windows compilers
@@ -136,7 +133,7 @@ misc/fixdll.sh
 
 # running autoconf
 echo "Running autoconf to generate configure script..."
-autoconf
+autoconf || exit 1
 
 # touch stamp-h.in so the user doesn't need autoheader to compile
 touch stamp-h.in
