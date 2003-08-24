b7457948db16c15f89893fb66fced9020138a89b
Grzegorz Adam Hankiewicz <gradha@users.sourceforge.net>
2003-08-24 15:30:32 +0000

    Tobi Vollebregt spotted a bug with ustrerror(), the function was not returning a pointer to a static buffer.

--- src/unicode.c
+++ src/unicode.c
@@ -2234,7 +2234,7 @@ double ustrtod(AL_CONST char *s, char **endp)
 AL_CONST char *ustrerror(int err)
 {
    AL_CONST char *s = strerror(err);
-   char tmp[1024];
+   static char tmp[1024];
    return uconvert_ascii(s, tmp);
 }
 
