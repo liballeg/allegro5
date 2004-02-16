4f91ba8a97b33ab3c277d03ef49748fbcd21fcfc
Eric Botcazou <ebotcazou@users.sourceforge.net>
2004-02-16 04:43:40 +0000

    Omar Cornut improved the handling of KEY_COLON2 on Japanese keyboards.

--- src/misc/pckeys.c
+++ src/misc/pckeys.c
@@ -173,7 +173,7 @@ static unsigned short standard_key_ascii_table[KEY_MAX] =
    /* misc chars */  27, '`', '-', '=', 8, 9, '[', ']', 13, ';', '\'', '\\', '\\', ',', '.', '/', ' ',
    /* controls */    0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF,
    /* numpad */      '/', '*', '-', '+', '.', 13,
-   /* others */      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+   /* others */      0, 0, 0, 0, 0, 0, 0, 0, 0, '\'', 0, 0, 0, 0, 0,
    /* modifiers */   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
 
@@ -190,7 +190,7 @@ static unsigned short standard_key_capslock_table[KEY_MAX] =
    /* misc chars */  27, '`', '-', '=', 8, 9, '[', ']', 13, ';', '\'', '\\', '\\', ',', '.', '/', ' ',
    /* controls */    0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF,
    /* numpad */      '/', '*', '-', '+', '.', 13,
-   /* others */      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+   /* others */      0, 0, 0, 0, 0, 0, 0, 0, 0, '\'', 0, 0, 0, 0, 0,
    /* modifiers */   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
 
@@ -207,7 +207,7 @@ static unsigned short standard_key_shift_table[KEY_MAX] =
    /* misc chars */  27, '~', '_', '+', 8, 9, '{', '}', 13, ':', '"', '|', '|', '<', '>', '?', ' ',
    /* controls */    0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF,
    /* numpad */      '/', '*', '-', '+', '.', 13,
-   /* others */      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+   /* others */      0, 0, 0, 0, 0, 0, 0, 0, 0, '"', 0, 0, 0, 0, 0,
    /* modifiers */   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
 
