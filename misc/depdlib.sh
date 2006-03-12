06b4267a8bd8d63fa40d53c3cd6974dbfd3a27aa
Jon Rafkind <kazzmir@users.sourceforge.net>
2006-03-12 22:05:15 +0000

    Create plugins.h in tools/

--- misc/depdlib.sh
+++ misc/depdlib.sh
@@ -145,7 +145,7 @@ fi
 
 # Special rule to create the plugins.h file
 inc_files=`echo tools/plugins/*.inc | sed 's,tools/plugins/,$(srcdir)/tools/plugins/,g'`
-echo "\$(OBJDIR)/plugins.h: $inc_files"
-echo "	cat $inc_files > tools/plugins/plugins.h"
+echo "\$(plugins_h): $inc_files"
+echo "	cat $inc_files > \$(plugins_h)"
 echo ""
 
