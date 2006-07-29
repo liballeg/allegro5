7de144534e84ac53a9dcf6f9b2a684d39e3c5dd0
Peter Wang <tjaden@users.sourceforge.net>
2006-07-29 04:42:09 +0000

    Make the messages from SrcTests.cmake look more like the standard ones, and make them STATUS messages so they don't pop up windows in CMake GUI tools.

--- cmake/SrcTests.cmake
+++ cmake/SrcTests.cmake
@@ -1,5 +1,6 @@
 # THIS WILL COMPILE SOME CODE AND RETURN SUCCESS OR FAILURE AND SET THE VAR TO THAT
 MACRO(COMPILE_TEST VAR FLAGS LIBS SOURCE_TXT)
+        MESSAGE(STATUS "Performing Test ${VAR}")
 	FILE(WRITE ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp/tmp.c "${SOURCE_TXT}\n")
 	IF(NOT ${FLAGS})
 		SET(${FLAGS} " ")
@@ -14,10 +15,10 @@ MACRO(COMPILE_TEST VAR FLAGS LIBS SOURCE_TXT)
       		    "-DLINK_LIBRARIES:STRING=${LIBS}"
                     OUTPUT_VARIABLE OUTPUT)
                     IF(${VAR})
-                    	MESSAGE("-- Compilation test for ${VAR} successfull!")
+                    	MESSAGE(STATUS "Performing Test ${VAR} - Success")
                     	SET(${VAR} ${OUTPUT_VARIABLE})
                     ELSE(${VAR})
-                    	MESSAGE("-- Compilation test for ${VAR} failed output to ${CMAKE_BINARY_DIR}/CMakeError.log!")
+                    	MESSAGE("Performing Test ${VAR} - Failed")
                     	FILE(APPEND ${CMAKE_BINARY_DIR}/CMakeError.log
         		"Performing Compilation Test ${VAR} failed with the following output:\n"
         		"${OUTPUT}\n")
@@ -27,6 +28,7 @@ ENDMACRO(COMPILE_TEST VAR FLAGS LIBS SOURCE_TXT)
 
 # SAME AS THE ABOVE BUT WILL RUN THE COMPILED EXECUTABLE AND STORE IT'S RETURN VALUE IN VAR_1 and the COMPILE DATA IN VAR_2
 MACRO(RUN_TEST VAR_1 VAR_2 FLAGS LIBS SOURCE_TXT)
+        MESSAGE(STATUS "Performing Test ${VAR_1}")
 	FILE(WRITE ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp/tmp.c "${SOURCE_TXT}\n")
 	IF(NOT ${FLAGS})
 		SET(${FLAGS} " ")
@@ -41,10 +43,10 @@ MACRO(RUN_TEST VAR_1 VAR_2 FLAGS LIBS SOURCE_TXT)
                     "-DLINK_LIBRARIES:STRING=${LIBS}"
                     OUTPUT_VARIABLE OUTPUT)
                     IF(${VAR_1})
-                    	MESSAGE("-- Test for ${VAR_1} successfull!")
+                    	MESSAGE(STATUS "Performing Test ${VAR_1} - Success")
                     	SET(${VAR_1} ${OUTPUT_VARIABLE})
                     ELSE(${VAR_1})
-                    	MESSAGE("-- Run test for ${VAR_1} failed output to ${CMAKE_BINARY_DIR}/CMakeError.log!")
+                    	MESSAGE(STATUS "Performing Test ${VAR_1} - Failed")
                     	FILE(APPEND ${CMAKE_BINARY_DIR}/CMakeError.log
         		"Performing Compilation & Run Test ${VAR_1} failed with the following output:\n"
         		"${OUTPUT}\n")
@@ -54,17 +56,18 @@ ENDMACRO(RUN_TEST VAR_1 VAR_2 FLAGS LIBS SOURCE_TXT)
 
 # RUNS ASM CODE WITH as STORES SUCCESS OR NOT IN VAR
 MACRO(ASM_TEST VAR SOURCE_TXT)
+        MESSAGE(STATUS "Performing Test ${VAR}")
 	FILE(WRITE ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp/tmp.s "${SOURCE_TXT}\n")
 	EXECUTE_PROCESS(COMMAND "as" "tmp.s"
 		     WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp"
 		     RESULT_VARIABLE OUTPUT
 		     ERROR_VARIABLE  ERROR)
 		    IF(EXISTS ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp/a.out)
-                    	MESSAGE("-- Compilation test for ${VAR} successfull!")
+                    	MESSAGE(STATUS "Performing Test ${VAR} - Success")
                     	FILE(REMOVE ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp/a.out)
                     	SET(${VAR} 1)
                     ELSE(EXISTS ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp/a.out)
-                    	MESSAGE("-- Compilation test for ${VAR} failed output to ${CMAKE_BINARY_DIR}/CMakeError.log!")
+                    	MESSAGE(STATUS "Performing Test ${VAR} - Failed")
                     	FILE(APPEND ${CMAKE_BINARY_DIR}/CMakeError.log
         		"Performing Compilation Test ${VAR} failed with the following output:\n"
         		"${OUTPUT} ${ERROR}\n")
