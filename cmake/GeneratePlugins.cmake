11f7e0881af39332b0a2419ff2d0c499ae8fe1b1
Peter Wang <tjaden@users.sourceforge.net>
2006-11-24 08:10:01 +0000

    CMake build: fix some bugs with the grabber plugins support.

--- cmake/GeneratePlugins.cmake
+++ cmake/GeneratePlugins.cmake
@@ -1,15 +1,18 @@
 # Produce tools/plugins/plugins.h
 # These variables need to be passed in on the command line:
 #   plugins_h
-#   inc_files
+#   inc_files (space separated list of plugin .inc files)
 # 
 
 if("${plugins_h}" MATCHES "^$")
-	message(FATAL_ERROR "** Internal error: plugins_h variable not set **")
+    message(FATAL_ERROR "** Internal error: plugins_h variable not set **")
 endif("${plugins_h}" MATCHES "^$")
 
-FILE(WRITE ${plugins_h} "/* automatically generated */\n")
-FOREACH(inc ${inc_files})
-	FILE(READ ${inc} content)
-	FILE(APPEND "plugins/plugins.h" "${content}")
-ENDFOREACH(inc)
+separate_arguments(inc_files)
+file(WRITE ${plugins_h} "/* automatically generated */\n")
+foreach(inc ${inc_files})
+    # It's too easy to miss when something goes wrong without messages.
+    message("Appending " ${inc})
+    file(READ ${inc} content)
+    file(APPEND "plugins/plugins.h" "${content}")
+endforeach(inc)
