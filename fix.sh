de9e8c318196d225f1306abb54b63c5750723b87
Milan Mimica <mmimica@users.sourceforge.net>
2006-12-30 15:23:28 +0000

    Michal Molhanec and Milan Mimica simplified MSVC build process: - fix.bat is now used for all MSVC builds (even with cygwin) - "--msvcpaths" argument to fix.bat is default now. Use --nomsvcpaths to disable   automatic converting of MSVCDir to DOS 8.3 format. - build docs updated - other minor fixes

--- fix.sh
+++ fix.sh
@@ -9,7 +9,7 @@ proc_help()
    echo
    echo "Usage: $0 <platform> [--quick|--dtou|--utod|--utom|--mtou]"
    echo
-   echo "Where platform is one of: bcc32, beos, djgpp, mingw, msvc, qnx, unix"
+   echo "Where platform is one of: bcc32, beos, djgpp, mingw, qnx, unix"
    echo "mac, macosx, macosx-universal and watcom."
    echo "The --quick parameter turns off text file conversion, --dtou converts from"
    echo "DOS/Win32 format to Unix, --utod converts from Unix to DOS/Win32 format,"
@@ -142,7 +142,6 @@ case "$1" in
    "djgpp"   ) proc_fix "DOS (djgpp)"       "makefile.dj"  "ALLEGRO_DJGPP";;
    "mingw"   ) proc_fix "Windows (MinGW)"   "makefile.mgw" "ALLEGRO_MINGW32";;
    "mingw32" ) proc_fix "Windows (MinGW)"   "makefile.mgw" "ALLEGRO_MINGW32";;
-   "msvc"    ) proc_fix "Windows (MSVC)"    "makefile.vc"  "ALLEGRO_MSVC";;
    "qnx"     ) proc_fix "QNX"               "makefile.qnx" "ALLEGRO_QNX";;
    "unix"    ) proc_fix "Unix"              "none"         "ALLEGRO_UNIX";;
    "mac"     ) proc_fix "Mac"               "none"         "ALLEGRO_MPW";;
