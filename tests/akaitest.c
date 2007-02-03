2157e36e506c355482a22d84bfbea84daec4b50c
Peter Wang <tjaden@users.sourceforge.net>
2007-02-03 23:59:46 +0000

    torhu fixed a bug where an array was indexed with -1.
    [merge from 4.2 to 4.3]

--- tests/akaitest.c
+++ tests/akaitest.c
@@ -458,7 +458,7 @@ int piano_proc(int msg, DIALOG *d, int c)
 	    if (!blackkey[i%12]) {
 	       l = i*12;
 	       r = (i+1)*12;
-	       if (blackkey[(i-1)%12])
+	       if (i > 0 && blackkey[(i-1)%12])
 		  l -= 6;
 	       if (blackkey[(i+1)%12])
 		  r += 6;
