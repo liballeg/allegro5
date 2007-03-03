0039aab642466c04c8686196febdba70c4de94dc
Peter Wang <tjaden@users.sourceforge.net>
2007-03-03 01:16:07 +0000

    Change the way we escape a backslash inside backticks in asmdef.sh. It seems newer versions of bash have changed behaviour.

--- misc/asmdef.sh
+++ misc/asmdef.sh
@@ -20,9 +20,9 @@ fi
 while [ 1 = 1 ]; do
     c=`echo $list | cut -f 1 -d " "`:
     if [ $c = "0:" ]; then break; fi
-    n=`awk /$c/,/ascii/ $1 | grep "ascii" | cut -f 2 -d '"' | cut -f 1 -d '\'`
+    n=`awk /$c/,/ascii/ $1 | grep "ascii" | cut -f 2 -d '"' | cut -f 1 -d \\ `
     if [ -z "$n" ]; then
-      n=`awk /$c/,/string/ $1 | grep "string" | cut -f 2 -d '"' | cut -f 1 -d '\'`
+      n=`awk /$c/,/string/ $1 | grep "string" | cut -f 2 -d '"' | cut -f 1 -d \\ `
     fi
     if [ -z "$n" ]; then
       echo "$0: unable to parse assembly file $1"
