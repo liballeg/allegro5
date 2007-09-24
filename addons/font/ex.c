9cea518ac0924a232e12ceb0b112a9865857f25f
Trent Gamblin <trentg@users.sourceforge.net>
2007-09-24 11:53:35 +0000

    Added better font and example for the bitmap font addon.

--- addons/font/ex.c
+++ addons/font/ex.c
@@ -12,7 +12,17 @@ int main(void)
 
     a5font_init();
     A5FONT_FONT *f = a5font_load_font("bmpfont.tga", NULL);
-    a5font_textout(f, "text", 10, 10);
+    ALLEGRO_COLOR color;
+
+    /* Draw red text */
+    al_map_rgb(al_get_backbuffer(), &color, 255, 0, 0);
+    al_set_blender(ALLEGRO_ALPHA, ALLEGRO_INVERSE_ALPHA, &color);
+    a5font_textout(f, "red", 10, 10);
+
+    /* Draw green text */
+    al_map_rgb(al_get_backbuffer(), &color, 0, 255, 0);
+    al_set_blender(ALLEGRO_ALPHA, ALLEGRO_INVERSE_ALPHA, &color);
+    a5font_textout(f, "green", 10, 50);
 
     al_flip_display();
     al_rest(3000);
