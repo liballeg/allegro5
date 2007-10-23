963a9b4e2cf865111eb5b42eface08447108ae7c
Elias Pschernig <elias@users.sourceforge.net>
2007-10-23 20:08:32 +0000

    undid accidental commit of test code in exnew_bitmap

--- examples/exnew_bitmap.c
+++ examples/exnew_bitmap.c
@@ -7,11 +7,7 @@ int main(void)
     al_init();
     display = al_create_display(320, 200);
     bitmap = al_load_bitmap("mysha.pcx");
-    ALLEGRO_COLOR color;
-    al_map_rgb(&color, 255, 255, 255);
-    al_set_blender(ALLEGRO_ONE, ALLEGRO_ONE, &color);
-    al_draw_rotated_bitmap(bitmap, 160, 100, 160, 100, 45, 0);
-    al_draw_rotated_bitmap(bitmap, 160, 100, 160, 100, 45, 0);
+    al_draw_bitmap(bitmap, 0, 0, 0);
     al_flip_display();
     al_rest(1000);
     return 0;
