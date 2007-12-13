e54879397f5de2eff972fd7eb03f08e8b8074a28
Trent Gamblin <trentg@users.sourceforge.net>
2007-12-13 20:31:43 +0000

    Updated thanks._tx and changes._tx

--- examples/exnew_mouse.c
+++ examples/exnew_mouse.c
@@ -2,36 +2,36 @@
 
 int main(void)
 {
-	ALLEGRO_DISPLAY *display;
-	ALLEGRO_BITMAP *cursor;
-	ALLEGRO_MSESTATE msestate;
-	ALLEGRO_KBDSTATE kbdstate;
-	ALLEGRO_COLOR black;
+   ALLEGRO_DISPLAY *display;
+   ALLEGRO_BITMAP *cursor;
+   ALLEGRO_MSESTATE msestate;
+   ALLEGRO_KBDSTATE kbdstate;
+   ALLEGRO_COLOR black;
 
-	al_init();
+   al_init();
 
-	al_install_mouse();
-	al_install_keyboard();
+   al_install_mouse();
+   al_install_keyboard();
 
-	display = al_create_display(640, 480);
+   display = al_create_display(640, 480);
 
-	al_hide_mouse_cursor();
+   al_hide_mouse_cursor();
 
-	cursor = al_load_bitmap("cursor.tga");
+   cursor = al_load_bitmap("cursor.tga");
 
-	al_map_rgb(&black, 0, 0, 0);
+   al_map_rgb(&black, 0, 0, 0);
 
-	do {
-		al_get_mouse_state(&msestate);
-		al_get_keyboard_state(&kbdstate);
-		al_clear(&black);
-		al_draw_bitmap(cursor, msestate.x, msestate.y, 0);
-		al_flip_display();
-		al_rest(5);
-	} while (!al_key_down(&kbdstate, ALLEGRO_KEY_ESCAPE)
-		&& !msestate.buttons);
+   do {
+      al_get_mouse_state(&msestate);
+      al_get_keyboard_state(&kbdstate);
+      al_clear(&black);
+      al_draw_bitmap(cursor, msestate.x, msestate.y, 0);
+      al_flip_display();
+      al_rest(5);
+   } while (!al_key_down(&kbdstate, ALLEGRO_KEY_ESCAPE)
+      && !msestate.buttons);
 
-	return 0;
+   return 0;
 }
 END_OF_MAIN()
 
--- examples/exnew_mouse_events.c
+++ examples/exnew_mouse_events.c
@@ -2,60 +2,60 @@
 
 int main(void)
 {
-	ALLEGRO_DISPLAY *display;
-	ALLEGRO_BITMAP *cursor;
-	ALLEGRO_COLOR black;
-	ALLEGRO_EVENT_QUEUE *queue;
-	ALLEGRO_EVENT event;
-	int mx = 0;
-	int my = 0;
+   ALLEGRO_DISPLAY *display;
+   ALLEGRO_BITMAP *cursor;
+   ALLEGRO_COLOR black;
+   ALLEGRO_EVENT_QUEUE *queue;
+   ALLEGRO_EVENT event;
+   int mx = 0;
+   int my = 0;
 
-	al_init();
+   al_init();
 
-	al_install_mouse();
-	al_install_keyboard();
+   al_install_mouse();
+   al_install_keyboard();
 
-	display = al_create_display(640, 480);
+   display = al_create_display(640, 480);
 
-	al_hide_mouse_cursor();
+   al_hide_mouse_cursor();
 
-	cursor = al_load_bitmap("cursor.tga");
+   cursor = al_load_bitmap("cursor.tga");
 
-	al_map_rgb(&black, 0, 0, 0);
+   al_map_rgb(&black, 0, 0, 0);
 
-	queue = al_create_event_queue();
-	al_register_event_source(queue, (void *)al_get_mouse());
-	al_register_event_source(queue, (void *)al_get_keyboard());
+   queue = al_create_event_queue();
+   al_register_event_source(queue, (void *)al_get_mouse());
+   al_register_event_source(queue, (void *)al_get_keyboard());
 
-	do {
-		if (!al_event_queue_is_empty(queue)) {
-			while (al_get_next_event(queue, &event)) {
-				switch (event.type) {
-					case ALLEGRO_EVENT_MOUSE_AXES:
-						mx = event.mouse.x;
-						my = event.mouse.y;
-						break;
-					case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
-						goto done;
-						break;
-					case ALLEGRO_EVENT_KEY_DOWN:
-						if (event.keyboard.keycode ==
-								ALLEGRO_KEY_ESCAPE)
-							goto done;
-						break;
-				}
-			}
-		}
-		al_clear(&black);
-		al_draw_bitmap(cursor, mx, my, 0);
-		al_flip_display();
-		al_rest(5);
-	}
+   do {
+      if (!al_event_queue_is_empty(queue)) {
+         while (al_get_next_event(queue, &event)) {
+            switch (event.type) {
+               case ALLEGRO_EVENT_MOUSE_AXES:
+                  mx = event.mouse.x;
+                  my = event.mouse.y;
+                  break;
+               case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
+                  goto done;
+                  break;
+               case ALLEGRO_EVENT_KEY_DOWN:
+                  if (event.keyboard.keycode ==
+                        ALLEGRO_KEY_ESCAPE)
+                     goto done;
+                  break;
+            }
+         }
+      }
+      al_clear(&black);
+      al_draw_bitmap(cursor, mx, my, 0);
+      al_flip_display();
+      al_rest(5);
+   }
         while(1);
 done:
-	al_destroy_event_queue(queue);
+   al_destroy_event_queue(queue);
 
-	return 0;
+   return 0;
 }
 END_OF_MAIN()
 
