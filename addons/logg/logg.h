daa860fcc191ba3d8869403f9737f9eec9ef1c5e
Milan Mimica <mmimica@users.sourceforge.net>
2007-12-31 22:15:21 +0000

    Missed logg.h

---
+++ addons/logg/logg.h
@@ -0,0 +1,36 @@
+#ifndef LOGG_H
+#define LOGG_H
+
+#include <allegro.h>
+#include <vorbis/vorbisfile.h>
+
+#define OGG_PAGES_TO_BUFFER 2
+
+typedef struct {
+	char *buf[OGG_PAGES_TO_BUFFER];
+	int current_page;
+	int playing_page;
+	AUDIOSTREAM* audio_stream;
+	OggVorbis_File ovf;
+	int bits;
+	int stereo;
+	int freq;
+	int len;
+	char* filename;
+	int loop;
+	int volume;
+	int pan;
+} LOGG_Stream;
+
+extern SAMPLE* logg_load(const char* filename);
+extern int logg_get_buffer_size();
+extern void logg_set_buffer_size(int size);
+extern LOGG_Stream* logg_get_stream(const char* filename,
+		int volume, int pan, int loop);
+extern int logg_update_stream(LOGG_Stream* s);
+extern void logg_destroy_stream(LOGG_Stream* s);
+extern void logg_stop_stream(LOGG_Stream* s);
+extern int logg_restart_stream(LOGG_Stream* s);
+
+#endif
+
