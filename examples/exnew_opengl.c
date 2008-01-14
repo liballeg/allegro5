c59c259f4e8b107b5a7b2719fae225bb2e3c72e9
Elias Pschernig <elias@users.sourceforge.net>
2008-01-14 20:47:03 +0000

    Added exnew_opengl example. Will only with with the XGLX driver for now.

---
+++ examples/exnew_opengl.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <allegro5/allegro5.h>
+#include <allegro5/a5_opengl.h>
+
+/* Simple example showing how to use an extension. It draws a yellow triangle
+ * on red background onto a texture, then draws a quad with that texture.
+ */
+
+GLuint tex, fbo;
+bool no_fbo = 0;
+
+void draw_opengl(void)
+{
+   double ms = al_current_time() / 1000.0;
+
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+
+   /* Let's create a texture. It will only be visible if we cannot draw over
+    * it with the framebuffer extension.
+    */
+   if (!tex) {
+      unsigned char *pixels = malloc(256 * 256 * 4);
+      int x, y;
+      for (y = 0; y < 256; y++)
+      {
+         for (x = 0; x < 256; x++)
+         {
+            unsigned char r = x, g = y, b = 0, a = 255;
+            pixels[y * 256 * 4 + x * 4 + 0] = r;
+            pixels[y * 256 * 4 + x * 4 + 1] = g;
+            pixels[y * 256 * 4 + x * 4 + 2] = b;
+            pixels[y * 256 * 4 + x * 4 + 3] = a;
+         }
+      }
+      glGenTextures(1, &tex);
+      glBindTexture(GL_TEXTURE_2D, tex);
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 256, 256, 0, GL_RGBA,
+         GL_UNSIGNED_INT_8_8_8_8_REV, pixels);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+   }
+
+   /* Let's create a framebuffer object. */
+   if (!fbo && !no_fbo) {
+      /* Did Allegro discover the OpenGL extension for us? */
+      if (allegro_gl_extensions.ALLEGRO_GL_EXT_framebuffer_object) {
+         /* If yes, then it also filled in the function pointer. How nice. */
+         glGenFramebuffersEXT(1, &fbo);
+
+         glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
+         /* Attach the framebuffer object to our texture. */
+         glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT,
+            GL_TEXTURE_2D, tex, 0);
+         if (glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT) !=
+            GL_FRAMEBUFFER_COMPLETE_EXT) {
+            no_fbo = 1;
+         }
+         glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+      }
+      else {
+         /* We are screwed, the extension is not available. */
+         no_fbo = 1;
+      }
+   }
+
+  /* Draw a yellow triangle on red background to the framebuffer objcet. */
+   if (fbo && !no_fbo)
+   {
+      glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
+
+      glPushAttrib(GL_VIEWPORT_BIT | GL_TRANSFORM_BIT);
+      glViewport(0, 0, 256, 256);
+      glClearColor(1, 0, 0, 1);
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      glMatrixMode(GL_PROJECTION);
+      glPushMatrix();
+
+      glLoadIdentity();
+      glOrtho(0, 256, 256, 0, -1, 1);
+
+      glDisable(GL_TEXTURE_2D);
+      glColor3f(1, 1, 0);
+      glTranslatef(128, 128 + sin(ms * AL_PI * 2 * 2) * 20, 0);
+      glBegin(GL_TRIANGLES);
+      glVertex2f(0, -100);
+      glVertex2f(100, 0);
+      glVertex2f(-100, 0);
+      glEnd();
+
+      glPopMatrix();
+      glPopAttrib();
+
+      glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+   }
+
+   /* Draw a quad with our texture. */
+   glClearColor(0, 0, 1, 1);
+   glClear(GL_COLOR_BUFFER_BIT);
+
+   glLoadIdentity();
+   glTranslatef(320, 240, 0);
+   glRotatef(ms * 360 / 4, 0, 0, 1);
+   glColor3f(1, 1, 1);
+   glEnable(GL_TEXTURE_2D);
+   glBindTexture(GL_TEXTURE_2D, tex);
+   glBegin(GL_QUADS);
+   glTexCoord2f(0, 0); glVertex2f(-100, -100);
+   glTexCoord2f(1, 0); glVertex2f(+100, -100);
+   glTexCoord2f(1, 1); glVertex2f(+100, +100);
+   glTexCoord2f(0, 1); glVertex2f(-100, +100);
+   glEnd();
+}
+
+int main(void)
+{
+   ALLEGRO_DISPLAY *display;
+   ALLEGRO_EVENT_QUEUE *queue;
+   ALLEGRO_EVENT event;
+   int frames = 0;
+   double start;
+
+   al_init();
+
+   al_install_keyboard();
+   display = al_create_display(640, 480);
+
+   queue = al_create_event_queue();
+   al_register_event_source(queue, (ALLEGRO_EVENT_SOURCE *)al_get_keyboard());
+   al_register_event_source(queue, (ALLEGRO_EVENT_SOURCE *)display);
+
+   start = al_current_time() / 1000.0;
+   do {
+      /* Check for ESC key or close button event and quit in either case. */
+      if (!al_event_queue_is_empty(queue)) {
+         while (al_get_next_event(queue, &event)) {
+            switch (event.type) {
+               case ALLEGRO_EVENT_DISPLAY_CLOSE:
+                  goto done;
+                  break;
+               case ALLEGRO_EVENT_KEY_DOWN:
+                  if (event.keyboard.keycode ==
+                        ALLEGRO_KEY_ESCAPE)
+                     goto done;
+                  break;
+            }
+         }
+      }
+      draw_opengl();
+      al_flip_display();
+      frames++;
+   }
+   while(1);
+done:
+   printf("%.1f FPS\n", frames / (al_current_time() / 1000.0 - start));
+   al_destroy_event_queue(queue);
+
+   return 0;
+}
+END_OF_MAIN()
+
---
+++ include/allegro5/a5_opengl.h
@@ -0,0 +1,21 @@
+#ifndef A5_OPENGL_H
+#define A5_OPENGL_H
+
+
+#ifdef __cplusplus
+   extern "C" {
+#endif
+
+
+#include "allegro5/opengl/gl_ext.h"
+
+
+#ifdef __cplusplus
+   }
+#endif
+
+#endif /* ifndef A5_OPENGL_H */
+
+
+
+
