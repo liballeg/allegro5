eaca1f00527c7af117019ac123f80d9c5fd8cd83
Trent Gamblin <trentg@users.sourceforge.net>
2008-02-16 01:05:25 +0000

    Ryan Dickie made timers roll over properly in Windows

--- src/win/wtime.c
+++ src/win/wtime.c
@@ -23,35 +23,53 @@
    #include <mmsystem.h>
 #endif
 
-
-
-static DWORD _al_win_initial_time_low_res;
-
 #define LARGE_INTEGER_TO_INT64(li) (((int64_t)li.HighPart << 32) | \
 	(int64_t)li.LowPart)
 
-static int64_t _al_win_initial_time_high_res;
 static int64_t high_res_timer_freq;
+static int64_t _al_win_prev_time;
+static double _al_win_total_time;
 
 static double (*real_current_time_func)(void);
 
+static _AL_MUTEX time_mutex = _AL_MUTEX_UNINITED;
 
 static double low_res_current_time(void)
 {
-   return (double) (timeGetTime() - _al_win_initial_time_low_res) / 1000;
+    _al_mutex_lock(&time_mutex);
+   
+   int64_t cur_time = (int64_t) timeGetTime();
+   double ellapsed_time = (double) (cur_time - _al_win_prev_time) / 1000;
+
+   if (cur_time < _al_win_prev_time) {
+       ellapsed_time += 4294967.295;
+   }
+
+   _al_win_total_time += ellapsed_time;
+   _al_win_prev_time = cur_time;
+
+   _al_mutex_unlock(&time_mutex);
+
+   return _al_win_total_time;
 }
 
 
 static double high_res_current_time(void)
 {
-	LARGE_INTEGER count;
+   _al_mutex_lock(&time_mutex);
 
-	QueryPerformanceCounter(&count);
+   LARGE_INTEGER count;
+   QueryPerformanceCounter(&count);
 
-	int64_t c = LARGE_INTEGER_TO_INT64(count);
-	c -= _al_win_initial_time_high_res;
+   int64_t cur_time = LARGE_INTEGER_TO_INT64(count);
+   double ellapsed_time = (double)(cur_time - _al_win_prev_time) / (double)high_res_timer_freq;
 
-	return (double)c / high_res_timer_freq;
+   _al_win_total_time += ellapsed_time;
+   _al_win_prev_time = cur_time;
+
+   _al_mutex_unlock(&time_mutex);
+
+   return _al_win_total_time;
 }
 
 
@@ -64,20 +82,20 @@ double al_current_time(void)
 void _al_win_init_time(void)
 {
    LARGE_INTEGER tmp_freq;
-
+   _al_win_total_time = 0;
    QueryPerformanceFrequency(&tmp_freq);
 
    high_res_timer_freq = LARGE_INTEGER_TO_INT64(tmp_freq);
 
    if (high_res_timer_freq == 0) {
       real_current_time_func = low_res_current_time;
-      _al_win_initial_time_low_res = timeGetTime();
+      _al_win_prev_time = (int64_t) timeGetTime();
    }
    else {
       LARGE_INTEGER count;
       real_current_time_func = high_res_current_time;
       QueryPerformanceCounter(&count);
-      _al_win_initial_time_high_res = LARGE_INTEGER_TO_INT64(count);
+      _al_win_prev_time = LARGE_INTEGER_TO_INT64(count);
    }
 }
 
