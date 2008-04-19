a286ebd6e131212fb9dd74bac4351b030a436dcd
Ryan Dickie <goalie_ca@users.sourceforge.net>
2008-04-19 22:48:54 +0000

    acodec now uses case insensitive comparison for file extensions to determine which codec to use.

--- addons/acodec/acodec.c
+++ addons/acodec/acodec.c
@@ -25,17 +25,17 @@ ALLEGRO_SAMPLE* al_load_sample(const char* filename)
    //languages
    ext++; //get past the '.' character
    #if defined(ALLEGRO_CFG_ACODEC_VORBIS)
-      if (strcmp("ogg",ext) == 0)
+      if (stricmp("ogg",ext) == 0)
          return al_load_sample_oggvorbis(filename);
    #endif
    
    #if defined(ALLEGRO_CFG_ACODEC_FLAC)
-      if (strcmp("flac",ext) == 0)
+      if (stricmp("flac",ext) == 0)
          return al_load_sample_flac(filename);
    #endif
 
    #if defined(ALLEGRO_CFG_ACODEC_SNDFILE)
-      if (strcmp("wav",ext) == 0 || strcmp("aiff",ext) == 0)
+      if (stricmp("wav",ext) == 0 || strcmp("aiff",ext) == 0)
          return al_load_sample_sndfile(filename);
    #endif
  
