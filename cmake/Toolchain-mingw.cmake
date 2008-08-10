a167612d043ad1f904dd04089281158dc651445b
Peter Wang <tjaden@users.sourceforge.net>
2008-08-10 02:39:44 +0000

    Add URL for mingw cross compiler package hosted at SDL site. Update paths for current version of the package.

--- cmake/Toolchain-mingw.cmake
+++ cmake/Toolchain-mingw.cmake
@@ -4,7 +4,11 @@
 #   cmake -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchain-mingw.cmake .
 #
 # You will need at least CMake 2.6.0.
+#
 # Adjust the following paths to suit your environment.
+# The following assumes you have unpacked the package from
+#   http://www.libsdl.org/extras/win32/cross/
+# into /usr/local.
 #
 # This file was based on http://www.cmake.org/Wiki/CmakeMingw
 
@@ -12,11 +16,11 @@
 set(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-set(CMAKE_C_COMPILER /opt/cross-tools/bin/i386-mingw32msvc-gcc)
-set(CMAKE_CXX_COMPILER /opt/cross-tools/bin/i386-mingw32msvc-g++)
+set(CMAKE_C_COMPILER /usr/local/cross-tools/bin/i386-mingw32-gcc)
+set(CMAKE_CXX_COMPILER /usr/local/cross-tools/bin/i386-mingw32-g++)
 
 # here is the target environment located
-set(CMAKE_FIND_ROOT_PATH /opt/cross-tools)
+set(CMAKE_FIND_ROOT_PATH /usr/local/cross-tools)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
