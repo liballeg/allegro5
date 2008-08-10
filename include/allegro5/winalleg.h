b070d55b9f2761e1cba67d191afb112d44056551
Trent Gamblin <trentg@users.sourceforge.net>
2008-08-10 18:54:30 +0000

    Include d3d stuff in scan_depend

--- include/allegro5/winalleg.h
+++ include/allegro5/winalleg.h
@@ -112,7 +112,8 @@ AL_FUNC(void, stretch_blit_from_hdc, (HDC hdc, BITMAP *bitmap, int src_x, int sr
 
 /* D3D stuff */
 
-#if defined ALLEGRO_CFG_D3D
+
+#if defined(SCAN_DEPEND) || defined(ALLEGRO_CFG_D3D)
 #include <d3d9.h>
 
 #ifdef __cplusplus
