e547875eaea2521979a758ad3f80950257760a99
Peter Wang <tjaden@users.sourceforge.net>
2008-08-18 14:10:00 +0000

    Simplified exnew_monitorinfo.c output. Switch to 3 space indents.

--- examples/exnew_monitorinfo.c
+++ examples/exnew_monitorinfo.c
@@ -3,31 +3,24 @@
 
 int main(void)
 {
-	int num_adapters;
-	int i;
+   ALLEGRO_MONITOR_INFO info;
+   int num_adapters;
+   int i, j;
 
-	al_init();
+   al_init();
 
-	num_adapters = al_get_num_video_adapters();
+   num_adapters = al_get_num_video_adapters();
 
-	printf("%d adapters found...\n", num_adapters);
+   printf("%d adapters found...\n", num_adapters);
 
-	for (i = 0; i < num_adapters; i++) {
-		ALLEGRO_MONITOR_INFO info;
-		al_get_monitor_info(i, &info);
-		if (i != 0) {
-			int j;
-			for (j = 0; j < 79; j++) {
-				printf("-");
-			}
-			printf("\n");
-		}
-		printf("Adapter %d\n", i);
-		printf("%d,%d\n", info.x1, info.y1);
-		printf("\n\n\n\n\t\t%d,%d\n", info.x2, info.y2);
-	}
+   for (i = 0; i < num_adapters; i++) {
+      al_get_monitor_info(i, &info);
+      printf("Adapter %d: ", i);
+      printf("(%d, %d) - (%d, %d)\n", info.x1, info.y1, info.x2, info.y2);
+   }
 
-	return 0;
+   return 0;
 }
 END_OF_MAIN()
 
+/* vim: set sts=3 sw=3 et: */
