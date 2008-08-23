8a44de1333d736a80a2e25ebc2d8a4ff86b37573
Milan Mimica <mmimica@users.sourceforge.net>
2008-08-23 10:32:52 +0000

    Make iio shared link work on MSVC.

--- addons/iio/allegro5/a5_iio.h
+++ addons/iio/allegro5/a5_iio.h
@@ -1,6 +1,29 @@
 #ifndef A5_IIO_H
 #define A5_IIO_H
 
+#if (defined ALLEGRO_MINGW32) || (defined ALLEGRO_MSVC) || (defined ALLEGRO_BCC32)
+   #ifndef ALLEGRO_STATICLINK
+      #ifdef A5_IIO_SRC
+         #define _A5_IIO_DLL __declspec(dllexport)
+      #else
+         #define _A5_IIO_DLL __declspec(dllimport)
+      #endif
+   #else
+      #define _A5_IIO_DLL
+   #endif
+#endif
+
+#if defined ALLEGRO_MSVC
+   #define A5_IIO_FUNC(type, name, args)      _A5_IIO_DLL type __cdecl name args
+#elif defined ALLEGRO_MINGW32
+   #define A5_IIO_FUNC(type, name, args)      extern type name args
+#elif defined ALLEGRO_BCC32
+   #define A5_IIO_FUNC(type, name, args)      extern _A5_IIO_DLL type name args
+#else
+   #define A5_IIO_FUNC      AL_FUNC
+#endif
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -8,9 +31,9 @@ extern "C" {
 typedef ALLEGRO_BITMAP *(*IIO_LOADER_FUNCTION)(AL_CONST char *filename);
 
 
-AL_FUNC(bool, iio_init, (void));
-AL_FUNC(bool, iio_add_loader, (AL_CONST char *ext, IIO_LOADER_FUNCTION function));
-AL_FUNC(ALLEGRO_BITMAP *, iio_load, (AL_CONST char *filename));
+A5_IIO_FUNC(bool, iio_init, (void));
+A5_IIO_FUNC(bool, iio_add_loader, (AL_CONST char *ext, IIO_LOADER_FUNCTION function));
+A5_IIO_FUNC(ALLEGRO_BITMAP *, iio_load, (AL_CONST char *filename));
 
 #ifdef __cplusplus
 }
