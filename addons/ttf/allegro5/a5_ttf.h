23f642974bf6a070a15da66611f44d9e4cee8f07
Milan Mimica <mmimica@users.sourceforge.net>
2008-08-28 20:47:57 +0000

    Fix ttf and font addon shared linking on windows.

--- addons/ttf/allegro5/a5_ttf.h
+++ addons/ttf/allegro5/a5_ttf.h
@@ -3,4 +3,26 @@
 
 #define ALLEGRO_TTF_NO_KERNING 1
 
-ALLEGRO_FONT *al_ttf_load_font(char const *filename, int size, int flags);
+#if (defined ALLEGRO_MINGW32) || (defined ALLEGRO_MSVC) || (defined ALLEGRO_BCC32)
+   #ifndef ALLEGRO_STATICLINK
+      #ifdef A5_TTF_SRC
+         #define _A5_TTF_DLL __declspec(dllexport)
+      #else
+         #define _A5_TTF_DLL __declspec(dllimport)
+      #endif
+   #else
+      #define _A5_TTF_DLL
+   #endif
+#endif
+
+#if defined ALLEGRO_MSVC
+   #define A5_TTF_FUNC(type, name, args)      _A5_TTF_DLL type __cdecl name args
+#elif defined ALLEGRO_MINGW32
+   #define A5_TTF_FUNC(type, name, args)      extern type name args
+#elif defined ALLEGRO_BCC32
+   #define A5_TTF_FUNC(type, name, args)      extern _A5_TTF_DLL type name args
+#else
+   #define A5_TTF_FUNC      AL_FUNC
+#endif
+
+A5_TTF_FUNC(ALLEGRO_FONT *, al_ttf_load_font, (char const *filename, int size, int flags));
