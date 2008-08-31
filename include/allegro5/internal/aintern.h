dfe7bfeebaa0a6601a935e8c4fdbcfbd6d7b1b3d
Peter Wang <tjaden@users.sourceforge.net>
2008-08-31 01:56:23 +0000

    Delete obsolete function prototype.

--- include/allegro5/internal/aintern.h
+++ include/allegro5/internal/aintern.h
@@ -142,11 +142,6 @@ AL_FUNC(PACKFILE *, _pack_fdopen, (int fd, AL_CONST char *mode));
 AL_FUNC(int, _al_lzss_incomplete_state, (AL_CONST LZSS_UNPACK_DATA *dat));
 
 
-/* config stuff */
-void _reload_config(void);
-
-
-
 /* various libc stuff */
 AL_FUNC(void *, _al_sane_realloc, (void *ptr, size_t size));
 AL_FUNC(char *, _al_sane_strncpy, (char *dest, const char *src, size_t n));
