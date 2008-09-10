6099970eaaadfe8e547ca49179a74b5c0c2c745e
Milan Mimica <mmimica@users.sourceforge.net>
2008-09-10 20:26:14 +0000

    Fixed al_rest() in event processing loop in WGL.

--- src/win/wgl_disp.c
+++ src/win/wgl_disp.c
@@ -1203,7 +1203,7 @@ static void display_thread_proc(void *arg)
       al_lock_mutex(_al_win_input_mutex);
       result = MsgWaitForMultipleObjects(_win_input_events, _win_input_event_id, FALSE, 0, QS_ALLINPUT);
       al_unlock_mutex(_al_win_input_mutex);
-      al_rest(1);
+      al_rest(0.001);
       if (result < (DWORD) WAIT_OBJECT_0 + _win_input_events) {
          /* one of the registered events is in signaled state */
          (*_win_input_event_handler[result - WAIT_OBJECT_0])();
