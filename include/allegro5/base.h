df81a583a7d49561fa4482401a2dd70005b6785d
Peter Wang <tjaden@users.sourceforge.net>
2008-09-11 09:09:11 +0000

    Bump version number to 4.9.5 (SVN).

--- include/allegro5/base.h
+++ include/allegro5/base.h
@@ -51,10 +51,10 @@ AL_BEGIN_EXTERN_C
 
 #define ALLEGRO_VERSION          4
 #define ALLEGRO_SUB_VERSION      9
-#define ALLEGRO_WIP_VERSION      4
-#define ALLEGRO_VERSION_STR      "4.9.4 (SVN)"
+#define ALLEGRO_WIP_VERSION      5
+#define ALLEGRO_VERSION_STR      "4.9.5 (SVN)"
 #define ALLEGRO_DATE_STR         "2008"
-#define ALLEGRO_DATE             20080427    /* yyyymmdd */
+#define ALLEGRO_DATE             20080911    /* yyyymmdd */
 
 /*******************************************/
 /************ Some global stuff ************/
