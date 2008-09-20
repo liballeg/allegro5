4a957c5f811d70cfda8840cc427ea34720ae1ecd
Thomas Fjellstrom <strangemoose@users.sourceforge.net>
2008-09-20 02:41:58 +0000

    add the two examples

---
+++ examples/ex_get_path.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "allegro5/allegro5.h"
+#include "allegro5/fshook.h"
+
+int main(int argc, char **argv)
+{
+	char buffer[1024];
+	al_init();
+
+	al_get_path(AL_PROGRAM_PATH, buffer, 1024);
+	printf("AL_PROGRAM_PATH: %s\n", buffer);
+
+	al_get_path(AL_TEMP_PATH, buffer, 1024);
+	printf("AL_TEMP_PATH: %s\n", buffer);
+
+	al_get_path(AL_SYSTEM_DATA_PATH, buffer, 1024);
+	printf("AL_SYSTEM_DATA_PATH: %s\n", buffer);
+
+	al_get_path(AL_USER_DATA_PATH, buffer, 1024);
+	printf("AL_USER_DATA_PATH: %s\n", buffer);
+
+	al_get_path(AL_USER_HOME_PATH, buffer, 1024);
+	printf("AL_USER_HOME_PATH: %s\n", buffer);
+
+	return 0;
+}
