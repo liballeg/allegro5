59bb7d8198e3d692a1eedb85db61d6bc6c35ba69
Peter Wang <tjaden@users.sourceforge.net>
2008-09-21 01:42:40 +0000

    icodec: Respect bitmap pitch when loading images.
    I couldn't figure out how to do the same when saving images.
    Disable saving in ex_icodec.
    
    Use TRACE instead of fprintf(stderr, ...) for logging.

--- addons/icodec/icodec.cpp
+++ addons/icodec/icodec.cpp
@@ -15,13 +15,15 @@
 
 #include <exception>
 
+#include "allegro5/icodec.h"
+
 extern "C" {
-   #include "allegro5/icodec.h"
 
    /* image magick++ reads/writes based on string format */
    /* FIXME: for some inexplicable reason these are opposite endianess
     * image magick problem or allegro5 weirdness? */
-   const char* _pixel_format_string(int fmt) {
+   static const char* _pixel_format_string(int fmt)
+   {
       switch (fmt) {
          case ALLEGRO_PIXEL_FORMAT_ARGB_8888:
             return "BGRA";
@@ -37,7 +39,8 @@ extern "C" {
       }
    }
 
-   ALLEGRO_BITMAP* al_load_image(const char* path) {
+   ALLEGRO_BITMAP* al_load_image(const char* path)
+   {
       try {
          Magick::Image in(path);
          unsigned int width = in.columns();
@@ -47,20 +50,25 @@ extern "C" {
          al_lock_bitmap(bmp, &lr, ALLEGRO_LOCK_WRITEONLY);
          const char* sfmt = _pixel_format_string(lr.format);
          if (!sfmt) {
-            fprintf(stderr, "Unsupported pixel format. Currently 24bpp and 32bpp only\n");
+            TRACE("Unsupported pixel format. Currently 24bpp and 32bpp only\n");
             return NULL;
          }
-         fprintf(stderr, "Pixel format %s\n",sfmt);
-         in.write(0,0,width,height, sfmt, Magick::CharPixel, lr.data);
+         TRACE("Pixel format %s\n",sfmt);
+         char *p = (char *) lr.data;
+         for (unsigned int y = 0; y < height; y++) {
+            in.write(0, y, width, 1, sfmt, Magick::CharPixel, p);
+            p += lr.pitch;
+         }
          al_unlock_bitmap(bmp);
          return bmp;
       } catch(const std::exception& err ) {
-         fprintf(stderr, "icodec load error: %s\n", err.what());
+         TRACE("icodec load error: %s\n", err.what());
       } 
       return NULL;
    }
-   
-   int al_save_image(ALLEGRO_BITMAP* bmp, const char* path) {
+
+   int al_save_image(ALLEGRO_BITMAP* bmp, const char* path)
+   {
       try {
          unsigned int width = al_get_bitmap_width(bmp);
          unsigned int height = al_get_bitmap_height(bmp);
@@ -69,17 +77,19 @@ extern "C" {
          al_lock_bitmap(bmp, &lr, ALLEGRO_LOCK_READONLY);
          sfmt = _pixel_format_string(lr.format);
          if (!sfmt) {
-            fprintf(stderr, "Unsupported pixel format. Currently 24bpp and 32bpp only\n");
+            TRACE("Unsupported pixel format. Currently 24bpp and 32bpp only\n");
             return 0;
          }
-         fprintf(stderr, "Pixel format %s\n",sfmt);
+         TRACE("Pixel format %s\n",sfmt);
+	 /* XXX this doesn't respect the pitch, hence doesn't work */
          Magick::Image out(width,height, sfmt, Magick::CharPixel, lr.data);
          al_unlock_bitmap(bmp);
          out.write(path);
          return 0;
       } catch( const std::exception& err ) {
-         fprintf(stderr, "icodec write error: %s\n", err.what());
+         TRACE("icodec write error: %s\n", err.what());
          return 1;
       } 
    }
+
 } /* extern C */
--- examples/ex_icodec.c
+++ examples/ex_icodec.c
@@ -1,27 +1,31 @@
 /*
- * Ryan Dickie
- * This example was derived from the acodec one
- * It reads image files and displays them one by one
+ * Example program by Ryan Dickie.
+ *
+ * This example reads image files using the icodec addon (ImageMagick) and
+ * displays them one by one.
  */
 
 #include <stdio.h>
 #include "allegro5/allegro5.h"
 #include "allegro5/icodec.h"
 
-#define WIDTH 640
-#define HEIGHT 480
+#define WIDTH	640
+#define HEIGHT	480
 
 int main(int argc, char **argv)
 {
    int i;
    ALLEGRO_DISPLAY *display;
 
-   if(argc < 2) {
+   if (argc < 2) {
       fprintf(stderr, "Usage: %s {image_files}\n", argv[0]);
       return 1;
    }
 
-   al_init();
+   if (!al_init()) {
+      TRACE("Could not init Allegro.\n");
+      return 1;
+   }
 
    display = al_create_display(WIDTH, HEIGHT);
    if (!display) {
@@ -29,26 +33,31 @@ int main(int argc, char **argv)
       return 1;
    }
 
-   al_show_mouse_cursor();
-   for (i = 1; i < argc; ++i)
-   {
-      unsigned int width, height;
-      const char* filename = argv[i]; 
-      ALLEGRO_BITMAP* bmp = al_load_image(filename);
+   for (i = 1; i < argc; i++) {
+      const char *filename = argv[i];
+      ALLEGRO_BITMAP *bmp;
+      int width, height;
+      
+      bmp = al_load_image(filename);
       if (!bmp) {
-         fprintf(stderr, "Could not load ALLEGRO_BITMAP from '%s'!\n", filename);
-         continue;
+	 fprintf(stderr, "Could not load '%s'!\n", filename);
+	 continue;
       }
-      fprintf(stderr, "Drawing scaled bitmap %s\n",filename);
+
       width = al_get_bitmap_width(bmp);
       height = al_get_bitmap_height(bmp);
-      al_draw_scaled_bitmap(bmp, 0,0, width, height, 0,0,WIDTH,HEIGHT,0);
+      al_draw_scaled_bitmap(bmp, 0, 0, width, height, 0, 0, WIDTH, HEIGHT, 0);
       al_flip_display();
       al_rest(3.0);
-      al_save_image(bmp, "output.jpg");
+
+      /* XXX icodec saving is currently broken */
+      /* al_save_image(bmp, "output.jpg"); */
+
       al_destroy_bitmap(bmp);
    }
 
    return 0;
 }
 END_OF_MAIN()
+
+/* vim: set sts=3 sw=3 et: */
