e586801eab4e752c04f56234b7122e27f6131159
Milan Mimica <mmimica@users.sourceforge.net>
2008-10-12 10:39:31 +0000

    Added implementations for opendir(), closedir(), readdir() and missing struct declarations from dirent.h, since those are not part of the standard lib and MSVC doesn't provide them. The code was taken from dirent.c from MinGW Runtime sources (version 3.15.1) which implements them using WinAPI. The file is under Public Domain license.

--- src/fshook_stdio.c
+++ src/fshook_stdio.c
@@ -17,10 +17,18 @@
 
 
 #include <stdio.h>
+
 #ifdef _MSC_VER
    #define _POSIX_
+   #include <limits.h>
+   #undef _POSIX_
+
+   #include <tchar.h>
+   #include <stdlib.h>
+   #include <io.h>
+   #define  SUFFIX _T("*")
+   #define	SLASH	 _T("\\")
 #endif
-#include <limits.h>
 
 #include "allegro5/allegro5.h"
 #include "allegro5/debug.h"
@@ -63,6 +71,255 @@
       #include <time.h>
    #endif
 #endif
+
+
+/*
+ * MSVC is missing the whole dirent.h so we implement the bits we need here.
+ * The following block is coppied from dirent.c from MinGW Runtime sources
+ * version 3.15.1 with minor modifications.
+ */
+#ifdef ALLEGRO_MSVC
+
+#define	S_ISDIR(m)	(((m) & S_IFMT) == S_IFDIR)
+
+struct dirent
+{
+   long d_ino;                /* Always zero. */
+   unsigned short	d_reclen;   /* Always zero. */
+   unsigned short	d_namlen;   /* Length of name in d_name. */
+   char d_name[FILENAME_MAX]; /* File name. */
+};
+
+/*
+ * This is an internal data structure. Good programmers will not use it
+ * except as an argument to one of the functions below.
+ * dd_stat field is now int (was short in older versions).
+ */
+typedef struct
+{
+	/* disk transfer area for this dir */
+	struct _finddata_t dd_dta;
+
+	/* dirent struct to return from dir (NOTE: this makes this thread
+	 * safe as long as only one thread uses a particular DIR struct at
+	 * a time) */
+	struct dirent dd_dir;
+
+	/* _findnext handle */
+	long dd_handle;
+
+	/*
+    * Status of search:
+	 *   0 = not started yet (next entry to read is first entry)
+	 *  -1 = off the end
+	 *   positive = 0 based index of next entry
+	 */
+	int dd_stat;
+
+	/* given path for dir with search pattern (struct is extended) */
+	char dd_name[1];
+} DIR;
+
+/* Helper for opendir().  */
+static unsigned __inline _tGetFileAttributes(const _TCHAR * tPath)
+{
+#ifdef _UNICODE
+  /* GetFileAttributesW does not work on W9x, so convert to ANSI */
+  if (_osver & 0x8000)
+    {
+      char aPath [MAX_PATH];
+      WideCharToMultiByte(CP_ACP, 0, tPath, -1, aPath, MAX_PATH, NULL,
+			   NULL);
+      return GetFileAttributesA(aPath);
+    }
+  return GetFileAttributesW(tPath);
+#else
+  return GetFileAttributesA(tPath);
+#endif
+}
+
+/*
+ * opendir
+ *
+ * Returns a pointer to a DIR structure appropriately filled in to begin
+ * searching a directory.
+ */
+DIR *opendir(const _TCHAR *szPath)
+{
+   DIR *nd;
+   unsigned int rc;
+   _TCHAR szFullPath[MAX_PATH];
+
+   errno = 0;
+
+   if (!szPath) {
+      errno = EFAULT;
+      return (DIR *) 0;
+   }
+
+   if (szPath[0] == _T('\0')) {
+      errno = ENOTDIR;
+      return (DIR*) 0;
+   }
+
+   /* Attempt to determine if the given path really is a directory. */
+   rc = _tGetFileAttributes(szPath);
+   if (rc == (unsigned int)-1) {
+      /* call GetLastError for more error info */
+      errno = ENOENT;
+      return (DIR*) 0;
+   }
+   if (!(rc & FILE_ATTRIBUTE_DIRECTORY)) {
+      /* Error, entry exists but not a directory. */
+      errno = ENOTDIR;
+      return (DIR*) 0;
+   }
+
+   /* Make an absolute pathname.  */
+   _tfullpath(szFullPath, szPath, MAX_PATH);
+
+   /* Allocate enough space to store DIR structure and the complete
+   * directory path given. */
+   nd = (DIR *) malloc(sizeof (DIR) + (_tcslen(szFullPath) + _tcslen(SLASH)
+      + _tcslen(SUFFIX) + 1) * sizeof(_TCHAR));
+
+   if (!nd) {
+      /* Error, out of memory. */
+      errno = ENOMEM;
+      return (DIR*) 0;
+   }
+
+   /* Create the search expression. */
+   _tcscpy(nd->dd_name, szFullPath);
+
+   /* Add on a slash if the path does not end with one. */
+   if (nd->dd_name[0] != _T('\0')
+      && _tcsrchr(nd->dd_name, _T('/')) !=
+         nd->dd_name + _tcslen(nd->dd_name) - 1
+      && _tcsrchr(nd->dd_name, _T('\\')) !=
+         nd->dd_name + _tcslen(nd->dd_name) - 1)
+   {
+      _tcscat(nd->dd_name, SLASH);
+   }
+
+   /* Add on the search pattern */
+   _tcscat(nd->dd_name, SUFFIX);
+
+   /* Initialize handle to -1 so that a premature closedir doesn't try
+   * to call _findclose on it. */
+   nd->dd_handle = -1;
+
+   /* Initialize the status. */
+   nd->dd_stat = 0;
+
+   /* Initialize the dirent structure. ino and reclen are invalid under
+   * Win32, and name simply points at the appropriate part of the
+   * findfirst_t structure. */
+   nd->dd_dir.d_ino = 0;
+   nd->dd_dir.d_reclen = 0;
+   nd->dd_dir.d_namlen = 0;
+   memset(nd->dd_dir.d_name, 0, FILENAME_MAX);
+
+   return nd;
+}
+
+
+/*
+ * readdir
+ *
+ * Return a pointer to a dirent structure filled with the information on the
+ * next entry in the directory.
+ */
+struct dirent* readdir(DIR* dirp)
+{
+   errno = 0;
+
+   /* Check for valid DIR struct. */
+   if (!dirp) {
+     errno = EFAULT;
+     return (struct dirent*) 0;
+   }
+
+   if (dirp->dd_stat < 0) {
+      /* We have already returned all files in the directory
+       * (or the structure has an invalid dd_stat). */
+      return (struct dirent*) 0;
+   }
+   else if (dirp->dd_stat == 0) {
+      /* We haven't started the search yet. */
+      /* Start the search */
+      dirp->dd_handle = _tfindfirst(dirp->dd_name, &(dirp->dd_dta));
+
+      if (dirp->dd_handle == -1) {
+         /* Whoops! Seems there are no files in that
+          * directory. */
+         dirp->dd_stat = -1;
+      }
+      else {
+         dirp->dd_stat = 1;
+      }
+   }
+   else {
+      /* Get the next search entry. */
+      if (_tfindnext(dirp->dd_handle, &(dirp->dd_dta))) {
+         /* We are off the end or otherwise error.	
+            _findnext sets errno to ENOENT if no more file
+            Undo this. */ 
+         DWORD winerr = GetLastError();
+         if (winerr == ERROR_NO_MORE_FILES)
+            errno = 0;	
+         _findclose(dirp->dd_handle);
+         dirp->dd_handle = -1;
+         dirp->dd_stat = -1;
+      }
+      else {
+         /* Update the status to indicate the correct
+          * number. */
+         dirp->dd_stat++;
+      }
+   }
+
+   if (dirp->dd_stat > 0) {
+      /* Successfully got an entry. Everything about the file is
+       * already appropriately filled in except the length of the
+       * file name. */
+      dirp->dd_dir.d_namlen = _tcslen(dirp->dd_dta.name);
+      _tcscpy(dirp->dd_dir.d_name, dirp->dd_dta.name);
+      return &dirp->dd_dir;
+   }
+
+   return (struct dirent*) 0;
+}
+
+
+/*
+ * closedir
+ *
+ * Frees up resources allocated by opendir.
+ */
+int closedir(DIR* dirp)
+{
+  int rc;
+
+  errno = 0;
+  rc = 0;
+
+  if (!dirp) {
+     errno = EFAULT;
+     return -1;
+  }
+
+  if (dirp->dd_handle != -1) {
+     rc = _findclose(dirp->dd_handle);
+  }
+
+  /* Delete the dir structure. */
+  free(dirp);
+
+  return rc;
+}
+
+#endif /* ALLEGRO_MSVC */
 
 
 #define PREFIX_I "al-fs-stdio INFO: "
