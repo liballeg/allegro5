c9377c069b4e3cbcf35ce3efca5be3cc1fa5bdca
Peter Wang <tjaden@users.sourceforge.net>
2008-10-29 10:20:38 +0000

    Fix formatting in path API examples.

--- examples/ex_get_path.c
+++ examples/ex_get_path.c
@@ -3,24 +3,28 @@
 
 int main(int argc, char **argv)
 {
-	char buffer[1024];
-	al_init();
+   char buffer[1024];
 
-	al_get_path(AL_PROGRAM_PATH, buffer, 1024);
-	printf("AL_PROGRAM_PATH: %s\n", buffer);
+   al_init();
 
-	al_get_path(AL_TEMP_PATH, buffer, 1024);
-	printf("AL_TEMP_PATH: %s\n", buffer);
+   al_get_path(AL_PROGRAM_PATH, buffer, 1024);
+   printf("AL_PROGRAM_PATH: %s\n", buffer);
 
-	al_get_path(AL_SYSTEM_DATA_PATH, buffer, 1024);
-	printf("AL_SYSTEM_DATA_PATH: %s\n", buffer);
+   al_get_path(AL_TEMP_PATH, buffer, 1024);
+   printf("AL_TEMP_PATH: %s\n", buffer);
 
-	al_get_path(AL_USER_DATA_PATH, buffer, 1024);
-	printf("AL_USER_DATA_PATH: %s\n", buffer);
+   al_get_path(AL_SYSTEM_DATA_PATH, buffer, 1024);
+   printf("AL_SYSTEM_DATA_PATH: %s\n", buffer);
 
-	al_get_path(AL_USER_HOME_PATH, buffer, 1024);
-	printf("AL_USER_HOME_PATH: %s\n", buffer);
+   al_get_path(AL_USER_DATA_PATH, buffer, 1024);
+   printf("AL_USER_DATA_PATH: %s\n", buffer);
 
-	return 0;
+   al_get_path(AL_USER_HOME_PATH, buffer, 1024);
+   printf("AL_USER_HOME_PATH: %s\n", buffer);
+
+   return 0;
 }
+
 END_OF_MAIN()
+
+/* vim: set sts=3 sw=3 et: */
--- examples/ex_path.c
+++ examples/ex_path.c
@@ -10,25 +10,27 @@ int main(int argc, char **argv)
    al_init();
    set_uformat(U_ASCII);
 
-   if(argc < 2) {
+   if (argc < 2) {
       ALLEGRO_PATH *exe = al_path_create(argv[0]);
-      if(exe) {
-         printf("useage1: %s <path>\n", al_path_get_filename(exe));
+      if (exe) {
+         printf("usage1: %s <path>\n", al_path_get_filename(exe));
          al_path_free(exe);
       }
       else {
-         printf("useage2: %s <path>\n", argv[0]);
+         printf("usage2: %s <path>\n", argv[0]);
       }
 
-      exit(-1);
+      return 0;
    }
 
    dyn = al_path_create(argv[1]);
-   if(!dyn) {
-      printf("failed to create path structure for '%s'\n", argv[1]);
+   if (!dyn) {
+      printf("Failed to create path structure for '%s'.\n", argv[1]);
    }
    else {
-      printf("dyn: drive=\"%s\", file=\"%s\"\n", al_path_get_drive(dyn), al_path_get_filename(dyn));
+      printf("dyn: drive=\"%s\", file=\"%s\"\n",
+	  al_path_get_drive(dyn),
+	  al_path_get_filename(dyn));
       al_path_free(dyn);
    }
 
@@ -41,24 +43,33 @@ int main(int argc, char **argv)
    }
 
    tostring = al_path_create(argv[1]);
-   if(!tostring) {
-      printf("failed to create path structure for tostring test\n");
-   } else {
+   if (!tostring) {
+      printf("Failed to create path structure for tostring test\n");
+   }
+   else {
       char tostring_string[1024];
-      int32_t i = 0;
-      printf("tostring: '%s'\n", al_path_to_string(tostring, tostring_string, 1024, '/'));
+      int i;
+
+      al_path_to_string(tostring, tostring_string, 1024, '/');
+      printf("tostring: '%s'\n", tostring_string);
       printf("tostring: drive:'%s'", al_path_get_drive(tostring));
       printf(" dirs:");
-      for(i=0; i<al_path_num_components(tostring); i++) {
-         if(i > 0) printf(",");
+      for (i = 0; i < al_path_num_components(tostring); i++) {
+         if (i > 0)
+            printf(",");
          printf(" '%s'", al_path_index(tostring, i));
       }
       printf(" filename:'%s'\n", al_path_get_filename(tostring));
       al_path_free(tostring);
    }
 
-   /* FIXME: test out more of the al_path_ functions, ie: insert, remove, concat, cannonocalize, absolute, relative */
+   /* FIXME: test out more of the al_path_ functions, ie: insert, remove,
+    * concat, canonicalize, absolute, relative
+    */
 
    return 0;
 }
+
 END_OF_MAIN()
+
+/* vim: set sts=3 sw=3 et: */
