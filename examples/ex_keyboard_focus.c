2223541f7697c925d955980d8c6f2eaaa28df381
Peter Wang <tjaden@users.sourceforge.net>
2008-11-06 08:10:38 +0000

    Add an example to test the ALLEGRO_KEYBOARD_STATE `display' field.

---
+++ examples/ex_keyboard_focus.c
@@ -0,0 +1,72 @@
+/*
+ *    Example program for the Allegro library.
+ *
+ *    This program tests if the ALLEGRO_KEYBOARD_STATE `display' field
+ *    is set correctly to the focused display.
+ */
+
+#include "allegro5/allegro5.h"
+
+static ALLEGRO_DISPLAY *display1;
+static ALLEGRO_DISPLAY *display2;
+
+static void redraw(ALLEGRO_COLOR color1, ALLEGRO_COLOR color2)
+{
+   al_set_current_display(display1);
+   al_clear(color1);
+   al_flip_display();
+
+   al_set_current_display(display2);
+   al_clear(color2);
+   al_flip_display();
+}
+
+int main(void)
+{
+   ALLEGRO_COLOR black;
+   ALLEGRO_COLOR red;
+   ALLEGRO_KEYBOARD_STATE kbdstate;
+
+   if (!al_init()) {
+      return 1;
+   }
+
+   if (!al_install_keyboard()) {
+      return 1;
+   }
+
+   display1 = al_create_display(300, 300);
+   display2 = al_create_display(300, 300);
+   if (!display1 || !display2) {
+      al_destroy_display(display1);
+      al_destroy_display(display2);
+      return 1;
+   }
+
+   black = al_map_rgb(0, 0, 0);
+   red = al_map_rgb(255, 0, 0);
+
+   while (1) {
+      al_get_keyboard_state(&kbdstate);
+      if (al_key_down(&kbdstate, ALLEGRO_KEY_ESCAPE)) {
+         break;
+      }
+
+      if (kbdstate.display == display1) {
+         redraw(red, black);
+      }
+      else if (kbdstate.display == display2) {
+         redraw(black, red);
+      }
+      else {
+         redraw(black, black);
+      }
+
+      al_rest(0.1);
+   }
+
+   return 0;
+}
+END_OF_MAIN()
+
+/* vim: set sts=3 sw=3 et: */
