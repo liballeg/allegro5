352b9b595a5b0ca428cededdb98af0fa7a417044
Elias Pschernig <elias@users.sourceforge.net>
2009-02-06 13:30:12 +0000

    Moved broken scons files out of SVN, to not confuse users who try the 4.9.8 release.

--- scons/globals.py
+++
@@ -1,7 +0,0 @@
-# vi: syntax=python
-
-# Global variables, mostly for keeping track of things that happen multiple times
-# when they should only happen once, like configuring builds.
-# Do not abuse this file!
-
-unixConfig = None
