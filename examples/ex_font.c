0049b9bd2eee0cbf729cefd748d47747795bbf8f
Elias Pschernig <elias@users.sourceforge.net>
2009-02-24 20:15:45 +0000

    Made ex_font wait for an Escape key press instead of exiting after 3 seconds. (That takes more code than expected :P)

--- examples/ex_font.c
+++ examples/ex_font.c
@@ -2,6 +2,40 @@
 #include "allegro5/a5_font.h"
 #include "allegro5/a5_iio.h"
 
+static void wait_for_esc(void)
+{
+   ALLEGRO_EVENT_QUEUE *queue;
+   ALLEGRO_BITMAP *screen_clone;
+   al_install_keyboard();
+   queue = al_create_event_queue();
+   al_register_event_source(queue, (void *)al_get_keyboard());
+   al_register_event_source(queue, (void *)al_get_current_display());
+   screen_clone = al_clone_bitmap(al_get_target_bitmap());
+   while (1) {
+      ALLEGRO_EVENT event;
+      al_wait_for_event(queue, &event);
+      if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+         break;
+      else if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
+         if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
+            break;
+      }
+      else if (event.type == ALLEGRO_EVENT_DISPLAY_EXPOSE) {
+         int x = event.display.x;
+         int y = event.display.y;
+         int w = event.display.width;
+         int h = event.display.height;
+         al_set_blender(ALLEGRO_ALPHA, ALLEGRO_INVERSE_ALPHA,
+            al_map_rgb_f(1, 1, 1));
+         al_draw_bitmap_region(screen_clone, x, y - 2, w, h + 4,
+            x, y - 2, 0);
+         al_update_display_region(x, y, w, h);
+      }
+   }
+   al_destroy_bitmap(screen_clone);
+   al_destroy_event_queue(queue);
+}
+
 int main(void)
 {
     ALLEGRO_DISPLAY *display;
@@ -16,6 +50,8 @@ int main(void)
     al_iio_init();
     al_font_init();
 
+    al_set_new_display_option(ALLEGRO_DOUBLEBUFFERED, false, ALLEGRO_SUGGEST);
+    al_set_new_display_flags(ALLEGRO_GENERATE_EXPOSE_EVENTS);
     display = al_create_display(320, 200);
     if (!display) {
         TRACE("Failed to create display\n");
@@ -60,10 +96,11 @@ int main(void)
     /* Draw a unicode symbol */
     al_set_blender(ALLEGRO_ALPHA, ALLEGRO_INVERSE_ALPHA,
       al_map_rgb(0, 0, 255));
-    al_font_textout(a4f, 10, 90, "Mysha's 0.01€", -1);
+    al_font_textout(a4f, 10, 90, "Mysha's 0.02€", -1);
 
     al_flip_display();
-    al_rest(3);
+
+    wait_for_esc();
 
     al_destroy_bitmap(bitmap);
     al_destroy_bitmap(font_bitmap);
