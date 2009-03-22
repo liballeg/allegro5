8712332b49a8a8ac5ff722f5a74b26858ea89db5
Peter Wang <tjaden@users.sourceforge.net>
2009-03-22 03:20:58 +0000

    Delete tricky float->int function.
    It's only helpful on x86 processors without SSE.  Trent reported seeing
    some garbage due to its rounding behaviour, so it's not worth keeping.

--- include/allegro5/internal/aintern_float.h
+++ include/allegro5/internal/aintern_float.h
@@ -1,45 +1,15 @@
 #ifndef __al_included_aintern_float_h
 #define __al_included_aintern_float_h
 
-
-/*
- * Fast float->int conversion.
- *
- * I found this version at
- * http://stackoverflow.com/questions/78619/what-is-the-fastest-way-to-convert-float-to-int-on-x86
- *
- * It says:
- * by Vlad Kaipetsky.
- * Portable assuming FP24 set to nearest rounding mode
- * Efficient on x86 platform
- *
- * Note that it does not truncate like (int)f.
- *
- * The SSE instruction CVTTSS2SI is faster, and the compiler should
- * automatically use it if you allow it to.  Hence this trick is only useful
- * when compatibility with Pentium 2's and below is required.
- */
-#if defined(ALLEGRO_I386) && !defined(ALLEGRO_CFG_ALLOW_SSE)
-
-   union _al_UFloatInt {
-      int32_t i;
-      float f;
-   };
-
-   AL_INLINE(int32_t, _al_fast_float_to_int, (float fval),
-   {
-      /* ASSERT(fabs(fval) <= 0x003fffff); */ /* only 23 bit values handled */
-      union _al_UFloatInt *fi = (union _al_UFloatInt *)&fval;
-      fi->f += (3 << 22);
-      return ((fi->i) & 0x007fffff) - 0x00400000;
-   })
-
-#else
-
+   /* This file used to contain a tricky function that sped up float->int
+    * conversions on x86 machines when the SSE instruction CVTTSS2SI wasn't
+    * available (or when SSE wasn't enabled in the compiler).
+    *
+    * However, it performed rounding instead of truncating like (int)f, which
+    * did cause problems.  If an alternative is found we could define this
+    * macro once again.
+    */
    #define _al_fast_float_to_int(f)  ((int)(f))
-
-#endif
-
 
 #endif   /* __al_included_aintern_float_h */
 
