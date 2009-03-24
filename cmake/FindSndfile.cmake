ca49cef24964edb634cc932f055636214a8d09b1
Peter Wang <tjaden@users.sourceforge.net>
2009-03-24 02:28:11 +0000

    Search for libsndfile-1.dll as well as that is the name in the official Windows binary package.

--- cmake/FindSndfile.cmake
+++ cmake/FindSndfile.cmake
@@ -12,7 +12,7 @@ endif(SNDFILE_INCLUDE_DIR)
 
 find_path(SNDFILE_INCLUDE_DIR sndfile.h)
 
-find_library(SNDFILE_LIBRARY NAMES sndfile)
+find_library(SNDFILE_LIBRARY NAMES sndfile sndfile-1)
 
 # Handle the QUIETLY and REQUIRED arguments and set SNDFILE_FOUND to TRUE if
 # all listed variables are TRUE.
