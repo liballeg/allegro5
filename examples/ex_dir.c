e0d98b23c30930af8221f4c75e5bd5d1d3f5cca0
Peter Wang <tjaden@users.sourceforge.net>
2009-03-28 07:39:29 +0000

    Fix memory leaks in ex_dir.c.  Add END_OF_MAIN.

--- examples/ex_dir.c
+++ examples/ex_dir.c
@@ -27,17 +27,21 @@ void print_file(ALLEGRO_FS_ENTRY *entry)
       now - mtime,
       now - atime,
       size);
+   al_path_free(path);
 }
 
 void print_entry(ALLEGRO_FS_ENTRY *entry)
 {
    if (al_is_directory(entry)) {
       ALLEGRO_FS_ENTRY *next;
+
       al_open_entry(entry, "r");
       while (1) {
          next = al_readdir(entry);
-         if (!next) break;
+         if (!next)
+            break;
          print_file(next);
+         al_destroy_entry(next);
       }
    }
    else {
@@ -57,12 +61,17 @@ int main(int argc, char **argv)
    if (argc == 1) {
       ALLEGRO_FS_ENTRY *entry = al_create_entry(".");
       print_entry(entry);
+      al_destroy_entry(entry);
       return 0;
    }
-   
+
    for (i = 1; i < argc; i++) {
       ALLEGRO_FS_ENTRY *entry = al_create_entry(argv[i]);
       print_entry(entry);
+      al_destroy_entry(entry);
    }
    return 0;
 }
+END_OF_MAIN()
+
+/* vim: set sts=3 sw=3 et: */
