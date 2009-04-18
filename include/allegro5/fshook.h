aa16ab626ae05d907190638646431ff5c1b67f41
Peter Wang <tjaden@users.sourceforge.net>
2009-04-18 02:48:10 +0000

    Delete file sortings/filtering flags as they are unused.

--- include/allegro5/fshook.h
+++ include/allegro5/fshook.h
@@ -44,49 +44,6 @@ struct ALLEGRO_FS_ENTRY {
 };
 #endif /* ALLEGRO_LIB_BUILD */
 
-/* Enum: ALLEGRO_FS_FILTER
- */
-typedef enum ALLEGRO_FS_FILTER {
-   ALLEGRO_FS_FILTER_DRIVES         = 0x001,
-   ALLEGRO_FS_FILTER_DIRS           = 0x002,
-   ALLEGRO_FS_FILTER_FILES          = 0x004,
-   ALLEGRO_FS_FILTER_NOSYMLINKS     = 0x008,
-   ALLEGRO_FS_FILTER_TYPEMASK       = 0x00F,
-
-   ALLEGRO_FS_FILTER_ALLENTRIES     = ALLEGRO_FS_FILTER_DIRS | ALLEGRO_FS_FILTER_FILES | ALLEGRO_FS_FILTER_DRIVES,
-
-   ALLEGRO_FS_FILTER_READABLE       = 0x010,
-   ALLEGRO_FS_FILTER_WRITABLE       = 0x020,
-   ALLEGRO_FS_FILTER_EXECUTABLE     = 0x040,
-   ALLEGRO_FS_FILTER_PERMISSIONMASK = 0x070,
-
-   ALLEGRO_FS_FILTER_HIDDEN         = 0x100,
-   ALLEGRO_FS_FILTER_SYSTEM         = 0x200,
-   ALLEGRO_FS_FILTER_ACCESSMASK     = 0x300,
-
-   ALLEGRO_FS_FILTER_ALLDIRS        = 0x400,
-   ALLEGRO_FS_FILTER_CASESENSITIVE  = 0x800,
-   ALLEGRO_FS_FILTER_NODOTDOT       = 0x1000,
-
-   ALLEGRO_FS_FILTER_NOFILTER       = -1,
-} ALLEGRO_FS_FILTER;
-
-/* Enum: ALLEGRO_FS_SORT
- */
-typedef enum ALLEGRO_FS_SORT {
-   ALLEGRO_FS_SORT_NAME          = 0x00,
-   ALLEGRO_FS_SORT_TIME          = 0x01,
-   ALLEGRO_FS_SORT_SIZE          = 0x02,
-   ALLEGRO_FS_SORT_UNSORTED      = 0x03,
-   ALLEGRO_FS_SORT_SORTBYMASK    = 0x03,
-
-   ALLEGRO_FS_SORT_DIRSFIRST     = 0x04,
-   ALLEGRO_FS_SORT_REVERSED      = 0x08,
-   ALLEGRO_FS_SORT_CASESENSITIVE = 0x10,
-   ALLEGRO_FS_SORT_DIRSLAST      = 0x20,
-   ALLEGRO_FS_SORT_TYPE          = 0x80,
-} ALLEGRO_FS_SORT;
-
 /* Enum: ALLEGRO_FS_MKTEMP_REMOVE
  */
 enum {
@@ -118,35 +75,6 @@ enum {
 #ifndef EOF
    #define EOF    (-1)
 #endif
-
-/*
-   Different Methods of handling file stat info (ie: AL_STAT):
-
-   1. AL_STAT *al_fs_fstat(char *file) && void al_fs_stat_free(stathandle);
-      Pros: Can handle AL_STAT as a opaque type, hiding the struct members, and struct size away
-            to avoid possible ABI breakages.
-      Cons: The user has to free the handle, and use some annoyingly long named functions to access it.
-
-   2. int32_t al_fs_fstat(char *file, AL_STAT *st);
-      Pros: no handle to free, just pass in a AL_STAT on the stack.
-      Cons: struct needs to be fully uservisible,
-               may cause ABI breakages if items are added or removed from the struct
-            again needs some annoyingly long and ugly (named) functions to access the info.
-
-   3. Have no AL_STAT handle at all, cache the stat info in the ALLEGRO_FS_ENTRY handle.
-      Pros: no annoying AL_STAT handle or ABI breakages to worry about.
-            annoying access methods can be shortened.
-      Cons: Needs a ALLEGRO_FS_ENTRY handle to access stat info ***
-
-   ***) Possible Solution: allow an ALLEGRO_FS_ENTRY handle to be created, but not open the file
-
-   <Thomas> 3 is my preference. I'm going to implement it, and worry about arguments for or against later.
-*/
-
-/* TODO: Have stat info cached in ALLEGRO_FS_ENTRY handles. */
-/*  add al_fs_get_handle()/al_fs_open_handle()/_close_handle()
- *  alows one to grab a handle to get file info without opening the file
- */
 
 AL_FUNC(ALLEGRO_FS_ENTRY*, al_create_entry, (const char *path));
 AL_FUNC(void, al_destroy_entry, (ALLEGRO_FS_ENTRY *handle));
@@ -226,17 +154,6 @@ AL_FUNC(size_t, al_fwrite32be, (ALLEGRO_FS_ENTRY *f, int32_t l));
 
 AL_FUNC(char*, al_fgets, (ALLEGRO_FS_ENTRY *f, char *p, size_t max));
 AL_FUNC(int,   al_fputs, (ALLEGRO_FS_ENTRY *f, const char *p));
-
-/* Find stuff */
-
-/*
-typedef int (*AL_FILTER_PROC)(const char *, void *);
-typedef int (*AL_SORT_PROC)(const char *, const char *, void *);
-
-AL_FUNC(char **, al_fs_find_ex, (char *path, AL_FILTER_PROC filter, void *, AL_SORT_PROC sort, void *));
-AL_FUNC(char **, al_fs_find, (char *path, ALLEGRO_FS_FILTER filter, ALLEGRO_FS_SORT sort));
-AL_FUNC(void,  al_fs_free_list, (char **));
-*/
 
 AL_FUNC(char *, al_find_resource, (const char *base, const char *resource, uint32_t fm, char *buffer, size_t len));
 
