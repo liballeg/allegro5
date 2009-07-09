5094f5ea38b8806c276a19ff0759ff1d2e6a598b
Paul Suntsov <siegelord@users.sourceforge.net>
2009-07-09 01:57:41 +0000

    Some OpenGL bitmap routines were not checking whether the target bitmap was locked

--- src/opengl/ogl_bitmap.c
+++ src/opengl/ogl_bitmap.c
@@ -225,7 +225,7 @@ static void ogl_draw_bitmap(ALLEGRO_BITMAP *bitmap, float x, float y,
    ALLEGRO_BITMAP_OGL *ogl_target = (ALLEGRO_BITMAP_OGL *)target;
    ALLEGRO_DISPLAY *disp = (void *)al_get_current_display();
    if (disp->ogl_extras->opengl_target != ogl_target ||
-      !setup_blending(disp)) {
+      !setup_blending(disp) || target->locked) {
       _al_draw_bitmap_memory(bitmap, x, y, flags);
       return;
    }
@@ -344,7 +344,7 @@ static void ogl_draw_rotated_bitmap(ALLEGRO_BITMAP *bitmap, float cx, float cy,
    ALLEGRO_BITMAP_OGL *ogl_target = (ALLEGRO_BITMAP_OGL *)target;
    ALLEGRO_DISPLAY *disp = (void *)al_get_current_display();
    if (disp->ogl_extras->opengl_target != ogl_target ||
-      !setup_blending(disp)) {
+      !setup_blending(disp) || target->locked) {
       _al_draw_rotated_bitmap_memory(bitmap, cx, cy, dx, dy, angle, flags);
       return;
    }
@@ -371,7 +371,7 @@ static void ogl_draw_rotated_scaled_bitmap(ALLEGRO_BITMAP *bitmap,
    ALLEGRO_BITMAP_OGL *ogl_target = (ALLEGRO_BITMAP_OGL *)target;
    ALLEGRO_DISPLAY *disp = (void *)al_get_current_display();
    if (disp->ogl_extras->opengl_target != ogl_target ||
-      !setup_blending(disp)) {
+      !setup_blending(disp) || target->locked) {
       _al_draw_rotated_scaled_bitmap_memory(bitmap, cx, cy, dx, dy,
                                             xscale, yscale, angle, flags);
       return;
