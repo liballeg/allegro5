6b5890efb043477e45e87b8d2f59c5a1c36091b0
Peter Wang <tjaden@users.sourceforge.net>
2009-08-13 01:55:48 +0000

    Fix warnings.

--- include/allegro5/allegro_opengl.h
+++ include/allegro5/allegro_opengl.h
@@ -109,9 +109,13 @@ AL_FUNC(float,                 al_get_opengl_version,            (void));
 AL_FUNC(int,                   al_is_opengl_extension_supported, (AL_CONST char *extension));
 AL_FUNC(void*,                 al_get_opengl_proc_address,       (AL_CONST char *name));
 AL_FUNC(ALLEGRO_OGL_EXT_LIST*, al_get_opengl_extension_list,     (void));
-AL_FUNC(GLuint, al_get_opengl_texture, (ALLEGRO_BITMAP *bitmap));
-AL_FUNC(void, al_remove_opengl_fbo, (ALLEGRO_BITMAP *bitmap));
-AL_FUNC(GLuint, al_get_opengl_fbo, (ALLEGRO_BITMAP *bitmap));
+AL_FUNC(GLuint,                al_get_opengl_texture,            (ALLEGRO_BITMAP *bitmap));
+AL_FUNC(void,                  al_remove_opengl_fbo,             (ALLEGRO_BITMAP *bitmap));
+AL_FUNC(GLuint,                al_get_opengl_fbo,                (ALLEGRO_BITMAP *bitmap));
+AL_FUNC(void,                  al_get_opengl_texture_size,       (ALLEGRO_BITMAP *bitmap,
+                                                                  int *w, int *h));
+AL_FUNC(void,                  al_get_opengl_texture_position,   (ALLEGRO_BITMAP *bitmap,
+                                                                  int *u, int *v));
 
 #ifdef __cplusplus
    }
