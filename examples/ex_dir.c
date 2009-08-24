6a571a1a2c984463d13b5f0dfb61827a61d24985
Peter Wang <tjaden@users.sourceforge.net>
2009-08-24 23:14:09 +0000

    Make ex_dir traverse directories recursively.
    Show the `data' directory by default, otherwise lots of long CMake
    filenames will be in the output.

--- examples/ex_dir.c
+++ examples/ex_dir.c
@@ -18,7 +18,7 @@ static void print_file(ALLEGRO_FS_ENTRY *entry)
    else
       name = al_get_path_filename(path);
    printf("%-32s %s%s%s%s%s%s %10lu %10lu %10lu %13lu\n", 
-      name,
+      al_path_cstr(path, '/'),
       mode & ALLEGRO_FILEMODE_READ ? "r" : ".",
       mode & ALLEGRO_FILEMODE_WRITE ? "w" : ".",
       mode & ALLEGRO_FILEMODE_EXECUTE ? "e" : ".",
@@ -36,13 +36,28 @@ static void print_entry(ALLEGRO_FS_ENTRY *entry)
 {
    if (al_is_directory(entry)) {
       ALLEGRO_FS_ENTRY *next;
+      ALLEGRO_PATH *path;
+      const char *name;
 
       al_opendir(entry);
       while (1) {
          next = al_readdir(entry);
          if (!next)
             break;
-         print_file(next);
+
+         path = al_get_entry_name(next);
+         name = al_get_path_component(path, -1);
+         /* XXX this is annoying */
+         if (al_is_directory(next) &&
+               0 != strcmp(name, ".") &&
+               0 != strcmp(name, "..")) {
+            print_entry(next);
+         }
+         else {
+            print_file(next);
+         }
+         al_free_path(path);
+
          al_destroy_entry(next);
       }
       al_closedir(entry);
@@ -60,9 +75,15 @@ int main(int argc, char **argv)
    
    printf("%-32s %-6s %10s %10s %10s %13s\n",
       "name", "flags", "ctime", "mtime", "atime", "size");
+   printf("-------------------------------- "
+          "------ "
+          "---------- "
+          "---------- "
+          "---------- "
+          "-------------\n");
 
    if (argc == 1) {
-      ALLEGRO_FS_ENTRY *entry = al_create_entry(".");
+      ALLEGRO_FS_ENTRY *entry = al_create_entry("data");
       print_entry(entry);
       al_destroy_entry(entry);
       return 0;
