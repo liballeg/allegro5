ce65ee2f7116ced094b4a93cbb975ffaf256b3a3
Peter Wang <tjaden@users.sourceforge.net>
2009-10-11 01:51:15 +0000

    Search for ogg/ogg.h and add its directory to the include directories.

--- cmake/FindVorbis.cmake
+++ cmake/FindVorbis.cmake
@@ -10,6 +10,7 @@ if(NOT GP2XWIZ)
         # Already in cache, be silent
         set(VORBIS_FIND_QUIETLY TRUE)
     endif(VORBIS_INCLUDE_DIR)
+    find_path(OGG_INCLUDE_DIR ogg/ogg.h)
     find_path(VORBIS_INCLUDE_DIR vorbis/vorbisfile.h)
     find_library(OGG_LIBRARY NAMES ogg)
     find_library(VORBIS_LIBRARY NAMES vorbis)
@@ -18,7 +19,8 @@ if(NOT GP2XWIZ)
     # to TRUE if all listed variables are TRUE.
     include(FindPackageHandleStandardArgs)
     find_package_handle_standard_args(VORBIS DEFAULT_MSG
-        VORBIS_INCLUDE_DIR OGG_LIBRARY VORBIS_LIBRARY VORBISFILE_LIBRARY)
+        OGG_INCLUDE_DIR VORBIS_INCLUDE_DIR
+        OGG_LIBRARY VORBIS_LIBRARY VORBISFILE_LIBRARY)
 else(NOT GP2XWIZ)
     find_path(VORBIS_INCLUDE_DIR tremor/ivorbisfile.h)
     find_library(VORBIS_LIBRARY NAMES vorbis_dec)
@@ -37,6 +39,6 @@ else(VORBIS_FOUND)
   set(VORBIS_LIBRARIES)
 endif(VORBIS_FOUND)
 
-mark_as_advanced(VORBIS_INCLUDE_DIR)
+mark_as_advanced(OGG_INCLUDE_DIR VORBIS_INCLUDE_DIR)
 mark_as_advanced(OGG_LIBRARY VORBIS_LIBRARY VORBISFILE_LIBRARY)
 
