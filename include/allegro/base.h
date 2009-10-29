69b791dcc52f01c502be361d05788b7cc5670ce3
Peter Wang <tjaden@users.sourceforge.net>
2009-10-29 11:34:19 +0000

    Bump version number to 4.2.3.

--- include/allegro/base.h
+++ include/allegro/base.h
@@ -46,10 +46,10 @@
 
 #define ALLEGRO_VERSION          4
 #define ALLEGRO_SUB_VERSION      2
-#define ALLEGRO_WIP_VERSION      4
-#define ALLEGRO_VERSION_STR      "4.2.4 (SVN)"
+#define ALLEGRO_WIP_VERSION      3
+#define ALLEGRO_VERSION_STR      "4.2.3"
 #define ALLEGRO_DATE_STR         "2009"
-#define ALLEGRO_DATE             20091004    /* yyyymmdd */
+#define ALLEGRO_DATE             20091029    /* yyyymmdd */
 
 /*******************************************/
 /************ Some global stuff ************/
