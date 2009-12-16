394a9c5dfb7c8b30ce1ce5e960c5d36a2207e58f
Peter Wang <tjaden@users.sourceforge.net>
2009-12-16 04:54:00 +0000

    exsample, exsample, extimer: Run in windowed mode.

--- examples/exsample.c
+++ examples/exsample.c
@@ -45,12 +45,16 @@ int main(int argc, char *argv[])
       return 1;
    }
 
-   if (set_gfx_mode(GFX_AUTODETECT, 320, 200, 0, 0) != 0) {
+   if (set_gfx_mode(GFX_AUTODETECT_WINDOWED, 320, 200, 0, 0) != 0) {
       if (set_gfx_mode(GFX_SAFE, 320, 200, 0, 0) != 0) {
 	 set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
 	 allegro_message("Unable to set any graphic mode\n%s\n", allegro_error);
 	 return 1;
       }
+   }
+
+   if (set_display_switch_mode(SWITCH_BACKGROUND) != 0) {
+      set_display_switch_mode(SWITCH_BACKAMNESIA);
    }
 
    set_palette(desktop_palette);
