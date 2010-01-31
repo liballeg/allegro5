f557979109a78071954da0afd1c15bafb9c5e424
Peter Wang <tjaden@users.sourceforge.net>
2010-01-31 05:19:09 +0000

    Delete some outdated stuff in aintern.h.

--- include/allegro5/internal/aintern.h
+++ include/allegro5/internal/aintern.h
@@ -36,25 +36,6 @@ AL_FUNC(void, _al_rotate_scale_flip_coordinates, (al_fixed w, al_fixed h,
    al_fixed scale_x, al_fixed scale_y, int h_flip, int v_flip,
    al_fixed xs[4], al_fixed ys[4]));
 
-#if (defined allegro_windows)
-
-   al_func(int, _al_win_open, (const char *filename, int mode, int perm));
-   al_func(int, _al_win_remove, (const char *filename));
-
-
-   #define _al_open(filename, mode, perm)   _al_win_open(filename, mode, perm)
-   #define _al_remove(filename)             _al_win_remove(filename)
-
-#else
-
-   #define _al_open(filename, mode, perm)   open(filename, mode, perm)
-   #define _al_remove(filename)             unlink(filename)
-
-#endif
-
-/* length in bytes of the cpu_vendor string */
-#define _AL_CPU_VENDOR_SIZE 32
-
 
 AL_FUNCPTR(int, _al_trace_handler, (AL_CONST char *msg));
 
@@ -65,45 +46,8 @@ AL_FUNC(void, _al_remove_exit_func, (AL_METHOD(void, func, (void))));
 AL_FUNC(void, _al_run_exit_funcs, (void));
 
 
-/* helper structure for talking to Unicode strings */
-typedef struct UTYPE_INFO
-{
-   int id;
-   AL_METHOD(int, u_getc, (AL_CONST char *s));
-   AL_METHOD(int, u_getx, (char **s));
-   AL_METHOD(int, u_setc, (char *s, int c));
-   AL_METHOD(int, u_width, (AL_CONST char *s));
-   AL_METHOD(int, u_cwidth, (int c));
-   AL_METHOD(int, u_isok, (int c));
-   int u_width_max;
-} UTYPE_INFO;
-
 /* message stuff */
 #define ALLEGRO_MESSAGE_SIZE  4096
-
-#if 0
-/* wrappers for implementing disk I/O on different platforms */
-AL_FUNC(int, _al_file_isok, (AL_CONST char *filename));
-AL_FUNC(uint64_t, _al_file_size_ex, (AL_CONST char *filename));
-AL_FUNC(time_t, _al_file_time, (AL_CONST char *filename));
-AL_FUNC(int, _al_drive_exists, (int drive));
-AL_FUNC(int, _al_getdrive, (void));
-AL_FUNC(void, _al_getdcwd, (int drive, char *buf, int size));
-
-AL_FUNC(void, _al_detect_filename_encoding, (void));
-
-/* obsolete; only exists for binary compatibility with 4.2.0 */
-AL_FUNC(long, _al_file_size, (AL_CONST char *filename));
-
-
-/* packfile stuff */
-AL_VAR(int, _packfile_filesize);
-AL_VAR(int, _packfile_datasize);
-AL_VAR(int, _packfile_type);
-AL_FUNC(PACKFILE *, _pack_fdopen, (int fd, AL_CONST char *mode));
-
-AL_FUNC(int, _al_lzss_incomplete_state, (AL_CONST LZSS_UNPACK_DATA *dat));
-#endif
 
 /* various libc stuff */
 AL_FUNC(void *, _al_sane_realloc, (void *ptr, size_t size));
