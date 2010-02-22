bb34e88ef8d42f2ff436d73b725ec54d6da4c339
Peter Wang <tjaden@users.sourceforge.net>
2010-02-22 12:00:28 +0000

    Add toolchain configuration for MinGW environment built by http://mingw-cross-env.nongnu.org/
    [copied from 4.9]

--- cmake/Toolchain-mingw.cmake
+++ cmake/Toolchain-mingw.cmake
@@ -10,12 +10,6 @@
 # You will need at least CMake 2.6.0.
 #
 # Adjust the following paths to suit your environment.
-# The following assumes you have unpacked the package from
-#   http://www.libsdl.org/extras/win32/cross/
-# into /usr/local.
-#
-# You might want to set MINGDIR to prevent make install making a mess
-# in your normal directories.
 #
 # This file was based on http://www.cmake.org/Wiki/CmakeMingw
 
@@ -33,13 +27,19 @@ if(EXISTS /usr/i586-mingw32msvc)
     # First look in standard location as used by Debian/Ubuntu/etc.
     set(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
     set(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
-    set(CMAKE_RC_COMPILER i586-mingw32-windres)
     set(CMAKE_FIND_ROOT_PATH /usr/i586-mingw32msvc)
+elseif(EXISTS /opt/mingw)
+    # Otherwise you can get a MinGW environment using the script at
+    # <http://mingw-cross-env.nongnu.org>.  It downloads and builds MinGW and
+    # most of the dependencies for you.  This is a suitable location.
+    set(CMAKE_C_COMPILER /opt/mingw/usr/bin/i686-pc-mingw32-gcc)
+    set(CMAKE_CXX_COMPILER /opt/mingw/usr/bin/i686-pc-mingw32-g++)
+    set(CMAKE_FIND_ROOT_PATH /opt/mingw/usr/i686-pc-mingw32)
 else()
     # Else fill in local path which the user will likely adjust.
+    # This is the location assumed by <http://www.libsdl.org/extras/win32/cross/>
     set(CMAKE_C_COMPILER /usr/local/cross-tools/bin/i386-mingw32-gcc)
     set(CMAKE_CXX_COMPILER /usr/local/cross-tools/bin/i386-mingw32-g++)
-    set(CMAKE_RC_COMPILER /usr/local/cross-tools/bin/i386-mingw32-windres)
     set(CMAKE_FIND_ROOT_PATH /usr/local/cross-tools)
 endif()
 
@@ -58,3 +58,4 @@ set(ENV{PKG_CONFIG_LIBDIR} ${CMAKE_FIND_ROOT_PATH}/lib/pkgconfig)
 set(ENV{PKG_CONFIG_PATH} ${CMAKE_FIND_ROOT_PATH}/lib/pkgconfig)
 
 set(INSTALL_PREFIX ${CMAKE_FIND_ROOT_PATH})
+set(ENV{MINGDIR} ${CMAKE_FIND_ROOT_PATH})
