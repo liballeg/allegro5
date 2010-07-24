0d2c8399a37ffdb9476a726159ca8ce4ebe9c6f9
Peter Wang <tjaden@users.sourceforge.net>
2010-07-24 13:35:41 +0000

    David Capello problems in Windows when you use Alt-Tab.  Sometimes the Alt modifier is kept pressed when you focus the Allegro window.

--- src/win/wkeybd.c
+++ src/win/wkeybd.c
@@ -424,26 +424,12 @@ static void key_dinput_handle(void)
 int key_dinput_acquire(void)
 {
    HRESULT hr;
-   int mask, state;
-   unsigned char key_state[256];
+   BYTE keystate[256];
 
    if (key_dinput_device) {
-      mask = KB_SCROLOCK_FLAG | KB_NUMLOCK_FLAG | KB_CAPSLOCK_FLAG;
-      state = 0;
-
       /* Read the current Windows keyboard state */
-      GetKeyboardState(key_state);
-
-      if (key_state[VK_SCROLL] & 1)
-         state |= KB_SCROLOCK_FLAG;
-
-      if (key_state[VK_NUMLOCK] & 1)
-         state |= KB_NUMLOCK_FLAG;
-
-      if (key_state[VK_CAPITAL] & 1)
-         state |= KB_CAPSLOCK_FLAG;
-
-      _key_shifts = (_key_shifts & ~mask) | (state & mask);
+      GetKeyboardState(keystate);
+      update_shifts(keystate);
 
       hr = IDirectInputDevice_Acquire(key_dinput_device);
 
