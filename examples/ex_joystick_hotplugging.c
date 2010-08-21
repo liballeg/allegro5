6beba61e183636385415f67d7569ac8ad2f6e614
Trent Gamblin <trentg@users.sourceforge.net>
2010-08-21 15:37:00 +0000

    Added ex_joystick_hotplugging

---
+++ examples/ex_joystick_hotplugging.c
@@ -0,0 +1,71 @@
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+
+int main(void)
+{
+   int num_joysticks;
+   ALLEGRO_EVENT_QUEUE *queue;
+   int i;
+   ALLEGRO_JOYSTICK *curr_joy;
+
+   al_init();
+   al_install_joystick();
+   al_install_keyboard();
+   al_init_primitives_addon();
+
+   ALLEGRO_DISPLAY *display = al_create_display(640, 480);
+
+   queue = al_create_event_queue();
+   al_register_event_source(queue, al_get_keyboard_event_source());
+   al_register_event_source(queue, al_get_joystick_event_source());
+   num_joysticks = al_get_num_joysticks();
+   if (num_joysticks > 0) {
+      curr_joy = al_get_joystick(0);
+   }
+
+   while (1) {
+      ALLEGRO_EVENT event;
+      al_wait_for_event(queue, &event);
+      if (event.type == ALLEGRO_EVENT_KEY_DOWN && event.keyboard.keycode ==
+            ALLEGRO_KEY_ESCAPE) {
+         break;
+      }
+      else if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
+         int n = event.keyboard.unichar - '0';
+         if (n >= 0 && n < num_joysticks)
+            curr_joy = al_get_joystick(n);
+      }
+      else if (event.type == ALLEGRO_EVENT_JOYSTICK_CONFIGURATION) {
+         num_joysticks = al_get_num_joysticks();
+      }
+      else if (event.type == ALLEGRO_EVENT_JOYSTICK_AXIS) {
+         printf("axis event from %p\n", event.joystick.id);
+      }
+      else if (event.type == ALLEGRO_EVENT_JOYSTICK_BUTTON_DOWN) {
+      	printf("button down event %d from %p\n", event.joystick.button, event.joystick.id);
+      }
+
+      al_clear_to_color(al_map_rgb(0, 0, 0));
+
+      if (num_joysticks > 0) {
+         int x = 100;
+         int y = 100;
+         ALLEGRO_JOYSTICK_STATE joystate;
+         al_get_joystick_state(curr_joy, &joystate);
+         al_draw_filled_circle(
+            x+joystate.stick[0].axis[0]*20,
+            y+joystate.stick[0].axis[1]*20,
+            20, al_map_rgb(255, 255, 255)
+         );
+	 for (i = 0; i < al_get_joystick_num_buttons(curr_joy); i++) {
+	 	if (joystate.button[i]) {
+			al_draw_filled_circle(
+				i*20+10, 400, 9, al_map_rgb(255, 255, 255)
+			);
+		}
+	 }
+      }
+
+      al_flip_display();
+   }
+}
