3d7b4637f836683a5c8cf041f782440ae6b01578
Elias Pschernig <elias@users.sourceforge.net>
2010-09-10 16:48:26 +0000

    Added al_draw_prim test to ex_blend_bench.

--- examples/ex_blend_bench.c
+++ examples/ex_blend_bench.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
 #include <time.h>
 
 #include "common.c"
@@ -21,14 +22,88 @@
 #define TEST_TIME 5.0
 
 enum Mode {
+   ALL,
    PLAIN_BLIT,
    SCALED_BLIT,
-   ROTATE_BLIT
+   ROTATE_BLIT,
+   DRAW_PRIM,
 };
+
+static char const *names[] = {
+   "", "Plain blit", "Scaled blit", "Rotated blit", "Draw primitive"
+};
+
+ALLEGRO_DISPLAY *display;
+
+ALLEGRO_VERTEX_ELEMENT elems[] = {
+   {ALLEGRO_PRIM_POSITION, ALLEGRO_PRIM_FLOAT_3, offsetof(ALLEGRO_VERTEX, x)},
+   {ALLEGRO_PRIM_TEX_COORD_PIXEL, ALLEGRO_PRIM_FLOAT_2, offsetof(ALLEGRO_VERTEX, u)},
+   {ALLEGRO_PRIM_COLOR_ATTR, 0, offsetof(ALLEGRO_VERTEX, color)},
+   {0, 0, 0}
+};
+ALLEGRO_VERTEX_DECL* decl;
+
+static void replacement_al_draw_scaled_rotated_bitmap(ALLEGRO_BITMAP *bitmap,
+   float cx, float cy, float dx, float dy, float xscale, float yscale,
+   float angle, int flags)
+{
+   ALLEGRO_VERTEX v[4];
+   ALLEGRO_COLOR c = al_map_rgb_f(1, 1, 1);
+   int w = al_get_bitmap_width(bitmap);
+   int h = al_get_bitmap_height(bitmap);
+   ALLEGRO_TRANSFORM trans;
+   al_identity_transform(&trans);
+   al_translate_transform(&trans, -cx, -cy);
+   al_scale_transform(&trans, xscale, yscale);
+   al_rotate_transform(&trans, angle);
+   al_translate_transform(&trans, dx, dy);
+   v[0].x = 0;
+   v[0].y = 0;
+   v[0].z = 0;
+   v[0].u = 0;
+   v[0].v = 0;
+   v[0].color = c;
+   v[1].x = w;
+   v[1].y = 0;
+   v[1].z = 0;
+   v[1].u = w;
+   v[1].v = 0;
+   v[1].color = c;
+   v[3].x = w;
+   v[3].y = h;
+   v[3].z = 0;
+   v[3].u = w;
+   v[3].v = h;
+   v[3].color = c;
+   v[2].x = 0;
+   v[2].y = h;
+   v[2].z = 0;
+   v[2].u = 0;
+   v[2].v = h;
+   v[2].color = c;
+   
+   al_use_transform(&trans);
+   al_draw_prim(v, decl, bitmap, 0, 4, ALLEGRO_PRIM_TRIANGLE_STRIP);
+}
+
+static void setup(enum Mode mode)
+{
+   if (mode == DRAW_PRIM) {
+      decl = al_create_vertex_decl(elems, sizeof(ALLEGRO_VERTEX));
+   }
+}
+
+static void tear_down(enum Mode mode)
+{
+   if (mode == DRAW_PRIM) {
+      al_destroy_vertex_decl(decl);
+   }
+}
 
 static void step(enum Mode mode, ALLEGRO_BITMAP *b2)
 {
    switch (mode) {
+      case ALL: break;
       case PLAIN_BLIT:
          al_draw_bitmap(b2, 0, 0, 0);
          break;
@@ -38,6 +113,10 @@ static void step(enum Mode mode, ALLEGRO_BITMAP *b2)
       case ROTATE_BLIT:
          al_draw_scaled_rotated_bitmap(b2, 10, 10, 10, 10, 2.0, 2.0,
             ALLEGRO_PI/30, 0);
+         break;
+      case DRAW_PRIM:
+         replacement_al_draw_scaled_rotated_bitmap(b2, 10, 10, 10, 10,
+            2.0, 2.0, ALLEGRO_PI/30, 0);
          break;
    }
 }
@@ -51,57 +130,29 @@ static double current_clock(void)
    return (double)c / CLOCKS_PER_SEC;
 }
 
-int main(int argc, const char *argv[])
+static bool do_test(enum Mode mode)
 {
-   enum Mode mode = PLAIN_BLIT;
    ALLEGRO_STATE state;
-   ALLEGRO_DISPLAY *display;
    ALLEGRO_BITMAP *b1;
    ALLEGRO_BITMAP *b2;
    int REPEAT;
    double t0, t1;
    int i;
 
-   if (argc > 1) {
-      i = strtol(argv[1], NULL, 10);
-      switch (i) {
-         case 1:
-            mode = SCALED_BLIT;
-            break;
-         case 2:
-            mode = ROTATE_BLIT;
-            break;
-         default:
-            mode = PLAIN_BLIT;
-            break;
-      }
-   }
-
-   if (!al_init())
-      return 1;
-
-   open_log();
-
-   al_init_image_addon();
-
-   display = al_create_display(640, 480);
-   if (!display) {
-      abort_example("Error creating display\n");
-      return 1;
-   }
+   setup(mode);
 
    al_set_new_bitmap_flags(ALLEGRO_MEMORY_BITMAP);
 
    b1 = al_load_bitmap("data/mysha.pcx");
    if (!b1) {
       abort_example("Error loading data/mysha.pcx\n");
-      return 1;
+      return false;
    }
 
    b2 = al_load_bitmap("data/allegro.pcx");
    if (!b2) {
       abort_example("Error loading data/mysha.pcx\n");
-      return 1;
+      return false;
    }
 
    al_set_target_bitmap(b1);
@@ -116,6 +167,7 @@ int main(int argc, const char *argv[])
    al_flip_display();
    al_restore_state(&state);
 
+   log_printf("Benchmark: %s\n", names[mode]);
    log_printf("Please wait...\n");
 
    /* Do warmup run and estimate required runs for real test. */
@@ -133,8 +185,63 @@ int main(int argc, const char *argv[])
    }
    t1 = current_clock();
 
-   log_printf("Time = %g s, %d steps, FPS = %g\n",
-      t1 - t0, REPEAT, REPEAT / (t1 - t0));
+   log_printf("Time = %g s, %d steps\n",
+      t1 - t0, REPEAT);
+   log_printf("%s: %g FPS\n", names[mode], REPEAT / (t1 - t0));
+   
+   al_destroy_bitmap(b1);
+   al_destroy_bitmap(b2);
+      
+   tear_down(mode);
+   
+   return true;
+}
+
+int main(int argc, const char *argv[])
+{
+   enum Mode mode = ALL;
+   int i;
+
+   if (argc > 1) {
+      i = strtol(argv[1], NULL, 10);
+      switch (i) {
+         case 0:
+            mode = PLAIN_BLIT;
+            break;
+         case 1:
+            mode = SCALED_BLIT;
+            break;
+         case 2:
+            mode = ROTATE_BLIT;
+            break;
+         case 3:
+            mode = DRAW_PRIM;
+            break;
+      }
+   }
+
+   if (!al_init())
+      return 1;
+
+   open_log();
+
+   al_init_image_addon();
+   al_init_primitives_addon();
+
+   display = al_create_display(640, 480);
+   if (!display) {
+      abort_example("Error creating display\n");
+      return 1;
+   }
+   
+   if (mode == ALL) {
+      for (mode = PLAIN_BLIT; mode <= DRAW_PRIM; mode++) {
+         do_test(mode);
+      }
+   }
+   else {
+      do_test(mode);
+   }
 
    al_destroy_display(display);
 
