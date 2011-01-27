d23452205a26c74ffb6fd67b6c392e6abf9e1cbd
Trent Gamblin <trentg@users.sourceforge.net>
2011-01-27 21:28:19 +0000

    Updated iphone standard paths to match the recent discussions on [AD].
    cherry-pick https://alleg.svn.sourceforge.net/svnroot/alleg/allegro/branches/5.1@14244
    
    From: Trent Gamblin <trent@example.org>

--- src/iphone/iphone_path.m
+++ src/iphone/iphone_path.m
@@ -1,31 +1,50 @@
 #import <Foundation/Foundation.h>
 #include <allegro5/internal/aintern_iphone.h>
+#include <mach-o/dyld.h>
 
 ALLEGRO_PATH *_al_iphone_get_path(int id)
 {
-    char str[PATH_MAX];
-    NSString *string;
-    NSBundle *mainBundle;
-    switch (id) {
-        case ALLEGRO_EXENAME_PATH:
-        case ALLEGRO_PROGRAM_PATH:
-        case ALLEGRO_USER_HOME_PATH:
-            strcpy(str, getenv("HOME"));
-            return al_create_path_for_directory(str);
+   char str[PATH_MAX];
+   NSString *string;
+   NSArray *array;
+   NSBundle *mainBundle;
+  
+   switch (id) {
+      case ALLEGRO_USER_HOME_PATH:
+         string = NSHomeDirectory();
+         break;
         case ALLEGRO_TEMP_PATH:
-            string = NSTemporaryDirectory();
-            sprintf(str, "%s", [string UTF8String]);
-            return al_create_path_for_directory(str);
+         string = NSTemporaryDirectory();
+         break;
+        case ALLEGRO_PROGRAM_PATH:
+            mainBundle = [NSBundle mainBundle];
+            string = [mainBundle bundlePath];
+         break;
         case ALLEGRO_SYSTEM_DATA_PATH:
         case ALLEGRO_USER_DATA_PATH:
             mainBundle = [NSBundle mainBundle];
             string = [mainBundle resourcePath];
-            sprintf(str, "%s", [string UTF8String]);
-            return al_create_path_for_directory(str);
+         break;
         case ALLEGRO_USER_SETTINGS_PATH:
         case ALLEGRO_SYSTEM_SETTINGS_PATH:
-            sprintf(str, "%s/Library/Preferences", getenv("HOME"));
-            return al_create_path_for_directory(str);
-    }
-    return NULL;
+         array = NSSearchPathForDirectoriesInDomains(
+            NSDocumentDirectory,
+            NSUserDomainMask,
+            TRUE
+         );
+         string = (NSString *)[array objectAtIndex:0];
+         break;
+      case ALLEGRO_EXENAME_PATH: {
+            uint32_t size = sizeof(str);
+            if (_NSGetExecutablePath(str, &size) != 0) {
+            return NULL;
+         }
+         return al_create_path(str);
+      }
+      default:
+         return NULL;
+   }
+
+   sprintf(str, "%s", [string UTF8String]);
+   return al_create_path_for_directory(str);
 }
