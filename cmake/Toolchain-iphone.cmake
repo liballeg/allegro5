96a6d456b0fb03c3f97b321a279cf93c9704db7e
Elias Pschernig <elias@users.sourceforge.net>
2011-04-24 00:22:12 +0000

    use latest ios SDK by default instead of 3.2

--- cmake/Toolchain-iphone.cmake
+++ cmake/Toolchain-iphone.cmake
@@ -1,6 +1,4 @@
 SET (IPHONE 1)
 
-SET (SDKVER "3.2")
-SET (DEVROOT "/Developer/Platforms/iPhoneOS.platform/Developer")
-SET (SDKROOT "${DEVROOT}/SDKs/iPhoneOS${SDKVER}.sdk")
+SET (SDKROOT "/Developer/Platforms/iPhoneOS.platform/DeviceSupport/Latest/Symbols")
 SET (CMAKE_OSX_SYSROOT "${SDKROOT}")
