5bc5d5a72f4ac22e80299eecaac2abe694477036
Trent Gamblin <trentg@users.sourceforge.net>
2011-05-13 13:56:28 +0000

    Add missing file

---
+++ include/allegro5/platform/ald3d.h
@@ -0,0 +1,27 @@
+/*         ______   ___    ___
+ *        /\  _  \ /\_ \  /\_ \
+ *        \ \ \L\ \\//\ \ \//\ \      __     __   _ __   ___
+ *         \ \  __ \ \ \ \  \ \ \   /'__`\ /'_ `\/\`'__\/ __`\
+ *          \ \ \/\ \ \_\ \_ \_\ \_/\  __//\ \L\ \ \ \//\ \L\ \
+ *           \ \_\ \_\/\____\/\____\ \____\ \____ \ \_\\ \____/
+ *            \/_/\/_/\/____/\/____/\/____/\/___L\ \/_/ \/___/
+ *                                           /\____/
+ *                                           \_/__/
+ *
+ *      Header file for Direct3D specific API.
+ *
+ *      By Milan Mimica.
+ *
+ */
+
+#ifndef __al_included_allegro5_allegro_direct3d_disp_h
+#define __al_included_allegro5_allegro_direct3d_disp_h
+
+#include "allegro5/platform/alplatf.h"
+
+/* Display creation flag. */
+#define ALLEGRO_DIRECT3D     ALLEGRO_DIRECT3D_INTERNAL
+
+#endif
+
+/* vim: set ts=8 sts=3 sw=3 et: */
