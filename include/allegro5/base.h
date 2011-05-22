4c1304606093e935fc80bdbf480fe1a778d2fed1
Peter Wang <tjaden@users.sourceforge.net>
2011-05-22 02:30:49 +0000

    Bump version to 5.0.3.
    From: Peter Wang <novalazy@gmail.com>

--- include/allegro5/base.h
+++ include/allegro5/base.h
@@ -55,7 +55,7 @@
 
 #define ALLEGRO_VERSION          5
 #define ALLEGRO_SUB_VERSION      0
-#define ALLEGRO_WIP_VERSION      2
+#define ALLEGRO_WIP_VERSION      3
 
 /* Not sure we need it, but since ALLEGRO_VERSION_STR contains it:
  * 0 = SVN
@@ -65,11 +65,11 @@
  * Note x.y.z (= x.y.z.0) has release number 1, and x.y.z.1 has release
  * number 2, just to confuse you.
  */
-#define ALLEGRO_RELEASE_NUMBER   2
+#define ALLEGRO_RELEASE_NUMBER   1
 
-#define ALLEGRO_VERSION_STR      "5.0.2"
+#define ALLEGRO_VERSION_STR      "5.0.3"
 #define ALLEGRO_DATE_STR         "2011"
-#define ALLEGRO_DATE             20110404    /* yyyymmdd */
+#define ALLEGRO_DATE             20110522    /* yyyymmdd */
 #define ALLEGRO_VERSION_INT \
     ((ALLEGRO_VERSION << 24) | (ALLEGRO_SUB_VERSION << 16) | \
     (ALLEGRO_WIP_VERSION << 8) | ALLEGRO_RELEASE_NUMBER)
