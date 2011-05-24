5505b44cefdb4f0c4ed54580220befff7cb49d1e
Michał Cichoń <thedmd@users.sourceforge.net>
2011-05-24 08:10:54 +0000

    Add ability to build jpgalleg as DLL.

--- addons/jpgalleg/include/jpgalleg.h
+++ addons/jpgalleg/include/jpgalleg.h
@@ -22,6 +22,26 @@
 
 #include <allegro.h>
 
+
+#if (defined JPGALLEG_DYNAMIC) && (defined ALLEGRO_WINDOWS)
+    #ifdef JPGALLEG_SRC_BUILD
+        #define _AJPG_DLL __declspec(dllexport)
+    #else
+        #define _AJPG_DLL __declspec(dllimport)
+    #endif /* ALLEGRO_GL_SRC_BUILD */
+#else
+    #define _AJPG_DLL
+#endif /* (defined JPGALLEG_DYNAMIC) && (defined ALLEGRO_WINDOWS) */
+
+#define AJPG_VAR(type, name) extern _AJPG_DLL type name
+
+#if (defined JPGALLEG_DYNAMIC) && (defined ALLEGRO_WINDOWS)
+    #define AJPG_FUNC(type, name, args) extern _AJPG_DLL type __cdecl name args
+#else
+    #define AJPG_FUNC(type, name, args) extern type name args
+#endif /* (defined JPGALLEG_DYNAMIC) && (defined ALLEGRO_WINDOWS) */
+
+
 /* Library version constant and string */
 #define JPGALLEG_VERSION 0x0206
 #define JPGALLEG_VERSION_STRING			"JPGalleg 2.6, by Angelo Mottola, 2000-2006"
@@ -63,20 +83,20 @@ extern "C" {
 #endif
 
 
-extern int jpgalleg_init(void);
+AJPG_FUNC(int, jpgalleg_init, (void));
 
-extern BITMAP *load_jpg(AL_CONST char *filename, RGB *palette);
-extern BITMAP *load_jpg_ex(AL_CONST char *filename, RGB *palette, void (*callback)(int progress));
-extern BITMAP *load_memory_jpg(void *buffer, int size, RGB *palette);
-extern BITMAP *load_memory_jpg_ex(void *buffer, int size, RGB *palette, void (*callback)(int progress));
+AJPG_FUNC(BITMAP *, load_jpg, (AL_CONST char *filename, RGB *palette));
+AJPG_FUNC(BITMAP *, load_jpg_ex, (AL_CONST char *filename, RGB *palette, void (*callback)(int progress)));
+AJPG_FUNC(BITMAP *, load_memory_jpg, (void *buffer, int size, RGB *palette));
+AJPG_FUNC(BITMAP *, load_memory_jpg_ex, (void *buffer, int size, RGB *palette, void (*callback)(int progress)));
 
-extern int save_jpg(AL_CONST char *filename, BITMAP *image, AL_CONST RGB *palette);
-extern int save_jpg_ex(AL_CONST char *filename, BITMAP *image, AL_CONST RGB *palette, int quality, int flags, void (*callback)(int progress));
-extern int save_memory_jpg(void *buffer, int *size, BITMAP *image, AL_CONST RGB *palette);
-extern int save_memory_jpg_ex(void *buffer, int *size, BITMAP *image, AL_CONST RGB *palette, int quality, int flags, void (*callback)(int progress));
+AJPG_FUNC(int, save_jpg, (AL_CONST char *filename, BITMAP *image, AL_CONST RGB *palette));
+AJPG_FUNC(int, save_jpg_ex, (AL_CONST char *filename, BITMAP *image, AL_CONST RGB *palette, int quality, int flags, void (*callback)(int progress)));
+AJPG_FUNC(int, save_memory_jpg, (void *buffer, int *size, BITMAP *image, AL_CONST RGB *palette));
+AJPG_FUNC(int, save_memory_jpg_ex, (void *buffer, int *size, BITMAP *image, AL_CONST RGB *palette, int quality, int flags, void (*callback)(int progress)));
 
-extern int jpgalleg_error;
-extern const char *jpgalleg_error_string(void);
+AJPG_VAR(int, jpgalleg_error);
+AJPG_FUNC(const char *, jpgalleg_error_string, (void));
 
 
 #ifdef __cplusplus
