b9baa7a528520a826e912b4493b16c49fb1be5e1
Peter Wang <tjaden@users.sourceforge.net>
2011-08-14 04:20:39 +0000

    Fix missing arguments to al_calloc in the GP2X port.
    cherry-pick https://alleg.svn.sourceforge.net/svnroot/alleg/allegro/branches/5.1@14952
    
    From: Peter Wang <tjaden@example.org>

--- src/gp2xwiz/wiz_display_opengl.c
+++ src/gp2xwiz/wiz_display_opengl.c
@@ -53,9 +53,9 @@ static ALLEGRO_DISPLAY *gp2xwiz_create_display_ogl(int w, int h)
    if (set_gfx_mode)
       return NULL;
 
-   ALLEGRO_DISPLAY_GP2XWIZ_OGL *d = al_calloc(sizeof *d);
+   ALLEGRO_DISPLAY_GP2XWIZ_OGL *d = al_calloc(1, sizeof *d);
    ALLEGRO_DISPLAY *display = (void*)d;
-   ALLEGRO_OGL_EXTRAS *ogl = al_calloc(sizeof *ogl);
+   ALLEGRO_OGL_EXTRAS *ogl = al_calloc(1, sizeof *ogl);
    EGLint numConfigs;
 
    display->ogl_extras = ogl;
@@ -236,7 +236,7 @@ ALLEGRO_DISPLAY_INTERFACE *_al_display_gp2xwiz_opengl_driver(void)
    if (gp2xwiz_vt)
       return gp2xwiz_vt;
 
-   gp2xwiz_vt = al_calloc(sizeof *gp2xwiz_vt);
+   gp2xwiz_vt = al_calloc(1, sizeof *gp2xwiz_vt);
 
    gp2xwiz_vt->create_display = gp2xwiz_create_display_ogl;
    gp2xwiz_vt->destroy_display = gp2xwiz_destroy_display_ogl;
