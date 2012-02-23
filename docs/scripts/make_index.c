419ca1aa2157c984b065aa4cb1c9c391514ed992
Jon Rafkind <kazzmir@users.sourceforge.net>
2012-02-23 12:53:05 +0000

    sort index
    cherry-pick https://alleg.svn.sourceforge.net/svnroot/alleg/allegro/branches/5.1@15177
    
    From: Jon Rafkind <kazzmir@example.org>

--- docs/scripts/make_index.c
+++ docs/scripts/make_index.c
@@ -13,22 +13,55 @@
  * support.
  */
 
+#include <string.h>
+#include <stdlib.h>
 #include "dawk.h"
+#include "aatree.h"
+
+static char *xstrdup(const char *s)
+{
+   size_t n = strlen(s);
+   char *p = malloc(n + 1);
+   memcpy(p, s, n);
+   p[n] = '\0';
+   return p;
+}
+
+
+static void print_it(const char * value){
+   /* We need an extra newline to make pandoc know this is the only thing in the link */
+   d_printf("[%s][%s]\n\n", value, value);
+}
+
+static void pre_order_traversal(Aatree * node, void (*doit)(const char *)){
+   if (node->left != &aa_nil){
+      pre_order_traversal(node->left, doit);
+   }
+
+   doit(node->value);
+
+   if (node->right != &aa_nil){
+      pre_order_traversal(node->right, doit);
+   }
+}
 
 int main(int argc, char *argv[])
 {
    dstr line;
+   Aatree * root = &aa_nil;
 
    d_init(argc, argv);
 
    while ((d_getline(line))) {
       if (d_match(line, "^\\[([^\\]]*)")) {
          const char *ref = d_submatch(1);
-         d_printf("[%s][%s]\n", ref, ref);
-      } else {
-         d_printf("\n");
+         root = aa_insert(root, xstrdup(ref), xstrdup(ref));
       }
    }
+
+   pre_order_traversal(root, print_it);
+
+   aa_destroy(root);
 
    return 0;
 }
