b3815494540cc1afd272f3a89e7c0b0d564f043f
Peter Wang <tjaden@users.sourceforge.net>
2012-02-23 12:12:35 +0000

    ex_get_path: Test al_set_exe_name.

--- examples/ex_get_path.c
+++ examples/ex_get_path.c
@@ -16,6 +16,8 @@ static void show_path(int id, const char *label)
 
 int main(void)
 {
+   int pass;
+
    /* defaults to blank */
    al_set_org_name("liballeg.org");
 
@@ -27,13 +29,27 @@ int main(void)
    }
    open_log();
 
-   show_path(ALLEGRO_RESOURCES_PATH, "RESOURCES_PATH");
-   show_path(ALLEGRO_TEMP_PATH, "TEMP_PATH");
-   show_path(ALLEGRO_USER_DATA_PATH, "USER_DATA_PATH");
-   show_path(ALLEGRO_USER_SETTINGS_PATH, "USER_SETTINGS_PATH");
-   show_path(ALLEGRO_USER_HOME_PATH, "USER_HOME_PATH");
-   show_path(ALLEGRO_USER_DOCUMENTS_PATH, "USER_DOCUMENTS_PATH");
-   show_path(ALLEGRO_EXENAME_PATH, "EXENAME_PATH");
+   for (pass = 1; pass <= 3; pass++) {
+      if (pass == 1) {
+         log_printf("With default exe name:\n");
+      }
+      else if (pass == 2) {
+         log_printf("\nOverriding exe name to blahblah\n");
+         al_set_exe_name("blahblah");
+      }
+      else if (pass == 3) {
+         log_printf("\nOverriding exe name to /tmp/blahblah.exe:\n");
+         al_set_exe_name("/tmp/blahblah.exe");
+      }
+
+      show_path(ALLEGRO_RESOURCES_PATH, "RESOURCES_PATH");
+      show_path(ALLEGRO_TEMP_PATH, "TEMP_PATH");
+      show_path(ALLEGRO_USER_DATA_PATH, "USER_DATA_PATH");
+      show_path(ALLEGRO_USER_SETTINGS_PATH, "USER_SETTINGS_PATH");
+      show_path(ALLEGRO_USER_HOME_PATH, "USER_HOME_PATH");
+      show_path(ALLEGRO_USER_DOCUMENTS_PATH, "USER_DOCUMENTS_PATH");
+      show_path(ALLEGRO_EXENAME_PATH, "EXENAME_PATH");
+   }
 
    close_log(true);
    return 0;
