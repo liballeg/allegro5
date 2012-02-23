2a4e4e0c9ffc18be3cfc13eb02c0d3eeecfc33a6
Peter Wang <tjaden@users.sourceforge.net>
2012-02-23 12:56:11 +0000

    Factor out finding Ogg library.
    cherry-pick https://alleg.svn.sourceforge.net/svnroot/alleg/allegro/branches/5.1@15200
    
    From: Peter Wang <novalazy@gmail.com>

--- cmake/FindVorbis.cmake
+++ cmake/FindVorbis.cmake
@@ -5,34 +5,35 @@
 #  VORBIS_LIBRARIES   - List of libraries when using vorbis(file).
 #  VORBIS_FOUND       - True if vorbis found.
 
+if(VORBIS_INCLUDE_DIR)
+    # Already in cache, be silent
+    set(VORBIS_FIND_QUIETLY TRUE)
+endif(VORBIS_INCLUDE_DIR)
+
 if(NOT GP2XWIZ)
-    if(VORBIS_INCLUDE_DIR)
-        # Already in cache, be silent
-        set(VORBIS_FIND_QUIETLY TRUE)
-    endif(VORBIS_INCLUDE_DIR)
-    find_path(OGG_INCLUDE_DIR ogg/ogg.h)
-    find_path(VORBIS_INCLUDE_DIR vorbis/vorbisfile.h)
-    # MSVC built ogg/vorbis may be named ogg_static and vorbis_static
-    # The provided project files name the library with the lib prefix.
-    find_library(OGG_LIBRARY
-        NAMES ogg ogg_static libogg libogg_static)
-    find_library(VORBIS_LIBRARY
-        NAMES vorbis vorbis_static libvorbis libvorbis_static)
-    find_library(VORBISFILE_LIBRARY
-        NAMES vorbisfile vorbisfile_static libvorbisfile libvorbisfile_static)
-    # Handle the QUIETLY and REQUIRED arguments and set VORBIS_FOUND
-    # to TRUE if all listed variables are TRUE.
-    include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(VORBIS DEFAULT_MSG
-        OGG_INCLUDE_DIR VORBIS_INCLUDE_DIR
-        OGG_LIBRARY VORBIS_LIBRARY VORBISFILE_LIBRARY)
+    find_package(Ogg)
+    if(OGG_FOUND)
+	find_path(VORBIS_INCLUDE_DIR vorbis/vorbisfile.h)
+	# MSVC built vorbis may be named vorbis_static
+	# The provided project files name the library with the lib prefix.
+	find_library(VORBIS_LIBRARY
+	    NAMES vorbis vorbis_static libvorbis libvorbis_static)
+	find_library(VORBISFILE_LIBRARY
+	    NAMES vorbisfile vorbisfile_static libvorbisfile libvorbisfile_static)
+	# Handle the QUIETLY and REQUIRED arguments and set VORBIS_FOUND
+	# to TRUE if all listed variables are TRUE.
+	include(FindPackageHandleStandardArgs)
+	find_package_handle_standard_args(VORBIS DEFAULT_MSG
+	    VORBIS_INCLUDE_DIR
+	    VORBIS_LIBRARY VORBISFILE_LIBRARY)
+    endif(OGG_FOUND)
 else(NOT GP2XWIZ)
     find_path(VORBIS_INCLUDE_DIR tremor/ivorbisfile.h)
     find_library(VORBIS_LIBRARY NAMES vorbis_dec)
     find_package_handle_standard_args(VORBIS DEFAULT_MSG
         VORBIS_INCLUDE_DIR VORBIS_LIBRARY)
 endif(NOT GP2XWIZ)
-    
+
 if(VORBIS_FOUND)
   if(NOT GP2XWIZ)
      set(VORBIS_LIBRARIES ${VORBISFILE_LIBRARY} ${VORBIS_LIBRARY}
@@ -44,6 +45,6 @@ else(VORBIS_FOUND)
   set(VORBIS_LIBRARIES)
 endif(VORBIS_FOUND)
 
-mark_as_advanced(OGG_INCLUDE_DIR VORBIS_INCLUDE_DIR)
-mark_as_advanced(OGG_LIBRARY VORBIS_LIBRARY VORBISFILE_LIBRARY)
+mark_as_advanced(VORBIS_INCLUDE_DIR)
+mark_as_advanced(VORBIS_LIBRARY VORBISFILE_LIBRARY)
 
