a4639be0b0876553b446d27eb4a52d361ae9eaf2
Peter Wang <tjaden@users.sourceforge.net>
2012-03-22 13:04:48 +0000

    Delete redundant code.

--- addons/image/android.c
+++ addons/image/android.c
@@ -10,11 +10,7 @@ ALLEGRO_BITMAP *_al_load_android_bitmap_f(ALLEGRO_FILE *fp, int flags)
 
 ALLEGRO_BITMAP *_al_load_android_bitmap(const char *filename, int flags)
 {
-   ALLEGRO_BITMAP *bmp = NULL;
-
    // NOTE: reads from assets. asset handling must be done better.
    return _al_android_load_image(filename, flags);
-   
-   return bmp;
 }
 
