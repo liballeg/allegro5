1fc3d649fa55681c7e3e9860f2a9945fd6a61212
Jon Rafkind <kazzmir@users.sourceforge.net>
2012-04-30 02:09:37 +0000

    add some opensl api calls mostly from the examples in the documentation

--- addons/audio/opensl.c
+++ addons/audio/opensl.c
@@ -1,17 +1,64 @@
 /* OpenSL: The Standard for Embedded Audio Acceleration
  * http://www.khronos.org/opensles/
+ * http://www.khronos.org/registry/sles/specs/OpenSL_ES_Specification_1.1.pdf
  */
+
+/* 4/29/2012: dummy implementation */
 
 #include "allegro5/allegro.h"
 #include "allegro5/internal/aintern_audio.h"
 
+#include <SLES/OpenSLES.h>
+
+/* Not sure if this one is needed, yet */
+#include <SLES/OpenSLES_Android.h>
+
 ALLEGRO_DEBUG_CHANNEL("opensl")
 
-/* 4/29/2012: dummy implementation */
+static const char * opensl_get_error_string(SLresult result)
+{
+    switch (result){
+        case SL_RESULT_PRECONDITIONS_VIOLATED: return "Preconditions violated";
+        case SL_RESULT_PARAMETER_INVALID: return "Invalid parameter";
+        case SL_RESULT_MEMORY_FAILURE: return "Memory failure";
+        case SL_RESULT_RESOURCE_ERROR: return "Resource error";
+        case SL_RESULT_RESOURCE_LOST: return "Resource lost";
+        case SL_RESULT_IO_ERROR: return "IO error";
+        case SL_RESULT_BUFFER_INSUFFICIENT: return "Insufficient buffer";
+        case SL_RESULT_CONTENT_CORRUPTED: return "Content corrupted";
+        case SL_RESULT_CONTENT_UNSUPPORTED: return "Content unsupported";
+        case SL_RESULT_CONTENT_NOT_FOUND: return "Content not found";
+        case SL_RESULT_PERMISSION_DENIED: return "Permission denied";
+        case SL_RESULT_FEATURE_UNSUPPORTED: return "Feature unsupported";
+        case SL_RESULT_INTERNAL_ERROR: return "Internal error";
+        case SL_RESULT_UNKNOWN_ERROR: return "Unknown error";
+        case SL_RESULT_OPERATION_ABORTED: return "Operation aborted";
+        case SL_RESULT_CONTROL_LOST: return "Control lost";
+    }
+    return "Unknown OpenSL error";
+}
 
+/* TODO: not really working yet */
 static int _opensl_open(void)
 {
-    /* TODO */
+    SLresult result;
+    SLObjectItf opensl;
+    SLEngineOption options[] = {
+        (SLuint32) SL_ENGINEOPTION_THREADSAFE,
+        (SLuint32) SL_BOOLEAN_TRUE,
+        /*
+        (SLuint32) SL_ENGINEOPTION_MAJORVERSION, (SLuint32) 1,
+        (SLuint32) SL_ENGINEOPTION_MINORVERSION, (SLuint32) 1,
+        */
+    };
+
+    result = slCreateEngine(&opensl, 1, options, 0, NULL, NULL);
+    if (result != SL_RESULT_SUCCESS){
+        ALLEGRO_ERROR("Could not open audio device: %s\n",
+                      opensl_get_error_string(result));
+    }
+    (*opensl)->Destroy(opensl);
+
     return 1;
 }
 
