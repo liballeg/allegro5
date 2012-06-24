7518ca26f499d063c63266ae225cc4b56cbe6460
Peter Wang <tjaden@users.sourceforge.net>
2012-06-24 06:04:00 +0000

    bmp: Reject negative width instead of crashing.
    cherry-pick https://alleg.svn.sourceforge.net/svnroot/alleg/allegro/branches/5.1@15628
    
    From: Peter Wang <tjaden@example.org>

--- addons/image/bmp.c
+++ addons/image/bmp.c
@@ -697,6 +697,11 @@ ALLEGRO_BITMAP *_al_load_bmp_f(ALLEGRO_FILE *f)
       return NULL;
    }
 
+   if ((int)infoheader.biWidth < 0) {
+      ALLEGRO_WARN("negative width: %ld\n", infoheader.biWidth);
+      return NULL;
+   }
+
    if (infoheader.biBitCount == 24)
       bpp = 24;
    else if (infoheader.biBitCount == 16)
