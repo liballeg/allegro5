c9efee24f788601281c07b3deb6ebc0019bddaea
Peter Wang <tjaden@users.sourceforge.net>
2012-07-22 10:40:41 +1000

    Fix compilation failure without installed allegro_primitives.h.
    allegro_primitives.h should be included before aintern_prim_opengl.h
    so I just deleted the include.

--- addons/primitives/allegro5/internal/aintern_prim_opengl.h
+++ addons/primitives/allegro5/internal/aintern_prim_opengl.h
@@ -1,8 +1,6 @@
 #ifndef __al_included_allegro5_aintern_prim_opengl_h
 #define __al_included_allegro5_aintern_prim_opengl_h
 
-#include <allegro5/allegro_primitives.h>
-
 int _al_draw_prim_opengl(ALLEGRO_BITMAP* target, ALLEGRO_BITMAP* texture, const void* vtxs, const ALLEGRO_VERTEX_DECL* decl, int start, int end, int type);
 int _al_draw_prim_indexed_opengl(ALLEGRO_BITMAP *target, ALLEGRO_BITMAP* texture, const void* vtxs, const ALLEGRO_VERTEX_DECL* decl, const int* indices, int num_vtx, int type);
 void _al_create_vertex_buffer_opengl(ALLEGRO_VERTEX_BUFFER* buf, const void* initial_data, size_t num_vertices, int usage_hints);
