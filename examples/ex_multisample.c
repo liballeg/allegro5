de6838a13acdc5a2022e828f8002e7d585886b8a
Elias Pschernig <elias@users.sourceforge.net>
2012-09-29 15:24:36 +0200

    Added some moving bitmaps to ex_multisample.
    Now it should demonstrate the effect of multi-sampling on smooth sub-pixel movement of bitmaps as well as the relation to texture filtering. Also made it display the amount of multi-sampling in the title window and move the windows next to each other.

--- examples/ex_multisample.c
+++ examples/ex_multisample.c
@@ -1,16 +1,109 @@
+/* This example demonstrates the effect of multi-sampling on primitives and
+ * bitmaps.
+ *
+ * 
+ * In the window without multi-sampling, the edge of the colored lines will not
+ * be anti-aliased - each pixel is either filled completely with the primitive
+ * color ot not at all.
+ *
+ * The same is true for bitmaps. They will always be drawn at the nearest
+ * full-pixel position. However this is only true for the bitmap outline -
+ * when texture filtering is enabled the texture itself will honor the sub-pixel
+ * position.
+ *
+ * Therefore in the case with no multi-sampling but texture-filtering the
+ * outer black edge will stutter in one-pixel steps while the inside edge will
+ * be filtered and appear smooth.
+ *
+ * 
+ * In the multi-sampled version the colored lines will be anti-aliased.
+ *
+ * Same with the bitmaps. This means the bitmap outlines will always move in
+ * smooth sub-pixel steps. However if texture filtering is turned of the
+ * texels still are rounded to the next integer position.
+ *
+ * Therefore in the case where multi-sampling is enabled but texture-filtering
+ * is not the outer bitmap edges will move smoothly but the inner will not.
+ */
+
 #include <stdio.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_color.h>
+#include <allegro5/allegro_font.h>
 #include <allegro5/allegro_primitives.h>
+#include <math.h>
 
 #include "common.c"
 
-static void draw(void)
+static ALLEGRO_FONT *font, *font_ms;
+static ALLEGRO_BITMAP *bitmap_normal;
+static ALLEGRO_BITMAP *bitmap_filter;
+static ALLEGRO_BITMAP *bitmap_normal_ms;
+static ALLEGRO_BITMAP *bitmap_filter_ms;
+static float bitmap_x[8], bitmap_y[8];
+static float bitmap_t;
+
+
+static ALLEGRO_BITMAP *create_bitmap(void)
 {
-   al_clear_to_color(al_map_rgb_f(1, 1, 1));
-   al_draw_line(10, 10, 190, 190, al_map_rgb_f(0, 0, 0), 0);
-   al_draw_line(100, 10, 100, 190, al_map_rgb_f(0, 0, 0), 0);
-   al_draw_line(10, 100, 190, 100, al_map_rgb_f(0, 0, 0), 0);
-   al_draw_line(190, 10, 10, 190, al_map_rgb_f(0, 0, 0), 0);
+   static ALLEGRO_BITMAP *bitmap;
+   ALLEGRO_LOCKED_REGION *locked;
+   int x, y, p;
+   unsigned char *rgba;
+   int checkers_size = 8;
+   static int bitmap_size = 24;
+   bitmap = al_create_bitmap(bitmap_size, bitmap_size);
+   locked = al_lock_bitmap(bitmap, ALLEGRO_PIXEL_FORMAT_ABGR_8888, 0);
+   rgba = locked->data;
+   p = locked->pitch;
+   for (y = 0; y < bitmap_size; y++) {
+      for (x = 0; x < bitmap_size; x++) {
+         int c = (((x / checkers_size) + (y / checkers_size)) & 1) * 255;
+         rgba[y * p + x * 4 + 0] = 0;
+         rgba[y * p + x * 4 + 1] = 0;
+         rgba[y * p + x * 4 + 2] = 0;
+         rgba[y * p + x * 4 + 3] = c;
+      }
+   }
+   al_unlock_bitmap(bitmap);
+   return bitmap;
+}
+
+
+
+static void bitmap_move(void)
+{
+   int i;
+   bitmap_t++;
+   for (i = 0; i < 8; i++) {
+      float a = 2 * ALLEGRO_PI * i / 16;
+      float s = sin((bitmap_t + i * 40) / 180 * ALLEGRO_PI);
+      s *= 90;
+      bitmap_x[i] = 100 + s * cos(a);
+      bitmap_y[i] = 100 + s * sin(a);
+   }
+}
+
+
+static void draw(ALLEGRO_BITMAP *bitmap, int y, char const *text)
+{
+   int i;
+   ALLEGRO_COLOR color = al_color_name("black");
+   al_draw_text(font_ms, color, 0, y, 0, text);
+
+   for (i = 0; i < 16; i++) {
+      float a = 2 * ALLEGRO_PI * i / 16;
+      ALLEGRO_COLOR c = al_color_hsv(i * 360 / 16, 1, 1);
+      al_draw_line(150 + cos(a) * 10, y + 100 + sin(a) * 10,
+         150 + cos(a) * 90, y + 100 + sin(a) * 90, c, 3);
+   }
+
+   for (i = 0; i < 8; i++) {
+      float a = 2 * ALLEGRO_PI * i / 16;
+      int s = al_get_bitmap_width(bitmap);
+      al_draw_rotated_bitmap(bitmap, s / 2, s / 2,
+         50 + bitmap_x[i], y + bitmap_y[i], a, 0);
+   }
 }
 
 int main(void)
@@ -18,8 +111,11 @@ int main(void)
    ALLEGRO_DISPLAY *display, *ms_display;
    ALLEGRO_EVENT_QUEUE *queue;
    ALLEGRO_TIMER *timer;
+   ALLEGRO_BITMAP *memory;
+   char title[1024];
    bool quit = false;
    bool redraw = true;
+   int wx, wy;
 
    if (!al_init()) {
       printf("Could not init Allegro.\n");
@@ -29,20 +125,53 @@ int main(void)
 
    al_install_keyboard();
 
+   al_set_new_bitmap_flags(ALLEGRO_MEMORY_BITMAP);
+   memory = create_bitmap();
+
+   /* Create the normal display. */
+   al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 0, ALLEGRO_REQUIRE);
+   al_set_new_display_option(ALLEGRO_SAMPLES, 0, ALLEGRO_SUGGEST);
+   display = al_create_display(300, 450);
+   al_set_window_title(display, "Normal");
+
+   /* Create bitmaps for the normal display. */
+   al_set_new_bitmap_flags(ALLEGRO_MIN_LINEAR | ALLEGRO_MAG_LINEAR);
+   bitmap_filter = al_clone_bitmap(memory);
+   al_set_new_bitmap_flags(0);
+   bitmap_normal = al_clone_bitmap(memory);
+
+   font = al_create_builtin_font();
+
+   al_get_window_position(display, &wx, &wy);
+   if (wx < 160)
+      wx = 160;
+
+   /* Create the multi-sampling display. */
    al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 1, ALLEGRO_REQUIRE);
    al_set_new_display_option(ALLEGRO_SAMPLES, 4, ALLEGRO_SUGGEST);
-   ms_display = al_create_display(200, 200);
+   ms_display = al_create_display(300, 450);
    if (!ms_display) {
       printf("Multisampling not available.\n");
       return 1;
    }
-   al_set_window_title(ms_display, "Multisampling");
+   sprintf(title, "Multisampling (%dx)", al_get_display_option(
+      ms_display, ALLEGRO_SAMPLES));
+   al_set_window_title(ms_display, title);
 
-   al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 0, ALLEGRO_REQUIRE);
-   al_set_new_display_option(ALLEGRO_SAMPLES, 0, ALLEGRO_SUGGEST);
-   display = al_create_display(200, 200);
-   al_set_window_title(display, "Normal");
-   
+   /* Create bitmaps for the multi-sampling display. */
+   al_set_new_bitmap_flags(ALLEGRO_MIN_LINEAR | ALLEGRO_MAG_LINEAR);
+   bitmap_filter_ms = al_clone_bitmap(memory);
+   al_set_new_bitmap_flags(0);
+   bitmap_normal_ms = al_clone_bitmap(memory);
+
+   font_ms = al_create_builtin_font();
+
+   /* Move the windows next to each other, because some window manager
+    * would put them on top of each other otherwise.
+    */
+   al_set_window_position(display, wx - 160, wy);
+   al_set_window_position(ms_display, wx + 160, wy);
+
    timer = al_create_timer(1.0 / 30.0);
 
    queue = al_create_event_queue();
@@ -50,6 +179,8 @@ int main(void)
    al_register_event_source(queue, al_get_display_event_source(display));
    al_register_event_source(queue, al_get_display_event_source(ms_display));
    al_register_event_source(queue, al_get_timer_event_source(timer));
+
+   create_bitmap();
    
    al_start_timer(timer);
    while (!quit) {
@@ -66,17 +197,30 @@ int main(void)
             break;
          
          case ALLEGRO_EVENT_TIMER:
+            bitmap_move();
             redraw = true;
             break;
       }
       
       if (redraw && al_is_event_queue_empty(queue)) {
+         /* Draw the multi-sampled version into the first window. */
          al_set_target_backbuffer(ms_display);
-         draw();
+
+         al_clear_to_color(al_map_rgb_f(1, 1, 1));
+
+         draw(bitmap_filter_ms, 0, "filtered, multi-sample");
+         draw(bitmap_normal_ms, 250, "no filter, multi-sample");
+
          al_flip_display();
-         
+
+         /* Draw the normal version into the second window. */
          al_set_target_backbuffer(display);
-         draw();
+
+         al_clear_to_color(al_map_rgb_f(1, 1, 1));
+
+         draw(bitmap_filter, 0, "filtered");
+         draw(bitmap_normal, 250, "no filter");
+         
          al_flip_display();
          
          redraw = false;
