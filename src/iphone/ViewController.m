009558a674a5feb7ab7682cb34f377be13069aa1
Trent Gamblin <trentg@users.sourceforge.net>
2012-10-05 17:53:59 -0600

    Allow compilation of iOS port on SDK < 6 again (untested on SDK 6)

--- src/iphone/ViewController.m
+++ src/iphone/ViewController.m
@@ -2,6 +2,8 @@
 #import "EAGLView.h"
 #import <allegro5/allegro_iphone_objc.h>
 
+#include "allegroAppDelegate.h"
+
 ALLEGRO_DEBUG_CHANNEL("iphone");
 
 @implementation ViewController
@@ -31,14 +33,64 @@ ALLEGRO_DEBUG_CHANNEL("iphone");
    return YES;
 }
 
-- (NSUInteger)supportedInterfaceOrientations {
-   return UIInterfaceOrientationMaskLandscapeLeft | UIInterfaceOrientationMaskLandscapeRight;
+// Taken from Apple docs
+#if !(__IPHONE_OS_VERSION_MAX_REQUIRED > __IPHONE_5_1)
+typedef enum {
+   UIInterfaceOrientationMaskPortrait = (1 << UIInterfaceOrientationPortrait),
+   UIInterfaceOrientationMaskLandscapeLeft = (1 << UIInterfaceOrientationLandscapeLeft),
+   UIInterfaceOrientationMaskLandscapeRight = (1 << UIInterfaceOrientationLandscapeRight),
+   UIInterfaceOrientationMaskPortraitUpsideDown = (1 << UIInterfaceOrientationPortraitUpsideDown),
+   UIInterfaceOrientationMaskLandscape =
+   (UIInterfaceOrientationMaskLandscapeLeft | UIInterfaceOrientationMaskLandscapeRight),
+   UIInterfaceOrientationMaskAll =
+   (UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskLandscapeLeft |
+   UIInterfaceOrientationMaskLandscapeRight | UIInterfaceOrientationMaskPortraitUpsideDown),
+   UIInterfaceOrientationMaskAllButUpsideDown =
+   (UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskLandscapeLeft |
+   UIInterfaceOrientationMaskLandscapeRight),
+} UIInterfaceOrientationMask;
+#endif
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+   if (!display) return UIInterfaceOrientationMaskAll;
+   ALLEGRO_DISPLAY_IPHONE *d = (ALLEGRO_DISPLAY_IPHONE *)display;
+   if (d->extra->adapter != 0) return UIInterfaceOrientationMaskAll;
+   ALLEGRO_EXTRA_DISPLAY_SETTINGS *options = &display->extra_settings;
+   int supported = options->settings[ALLEGRO_SUPPORTED_ORIENTATIONS];
+   int mask = 0;
+
+   switch (supported) {
+      case ALLEGRO_DISPLAY_ORIENTATION_ALL:
+         mask = UIInterfaceOrientationMaskAll;
+         break;
+      case ALLEGRO_DISPLAY_ORIENTATION_PORTRAIT:
+         mask = UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskPortraitUpsideDown;
+         break;
+      case ALLEGRO_DISPLAY_ORIENTATION_LANDSCAPE:
+         mask = UIInterfaceOrientationMaskLandscapeLeft | UIInterfaceOrientationMaskLandscapeRight;
+         break;
+      case ALLEGRO_DISPLAY_ORIENTATION_0_DEGREES:
+         mask = UIInterfaceOrientationMaskPortrait;
+         break;
+      case ALLEGRO_DISPLAY_ORIENTATION_90_DEGREES:
+         mask = UIInterfaceOrientationMaskLandscapeRight;
+         break;
+      case ALLEGRO_DISPLAY_ORIENTATION_180_DEGREES:
+         mask = UIInterfaceOrientationMaskPortraitUpsideDown;
+         break;
+      case ALLEGRO_DISPLAY_ORIENTATION_270_DEGREES:
+         mask = UIInterfaceOrientationMaskLandscapeLeft;
+         break;
+   }
+
+   return mask;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
    if (display == NULL)
-      return NO;
+      return YES;
    EAGLView *view = (EAGLView *)al_iphone_get_view(display);
    if (view) {
       return [view orientation_supported:interfaceOrientation];
