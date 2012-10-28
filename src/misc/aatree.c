1f057bdcf9f6c38b74e6b3111fa3ef8e4085796f
Peter Wang <tjaden@users.sourceforge.net>
2012-10-28 12:42:48 +1100

    Implement AA tree delete operation.
    Not used yet but intended for removing config keys.

--- src/misc/aatree.c
+++ src/misc/aatree.c
@@ -19,6 +19,14 @@
 #include "allegro5/allegro.h"
 #include "allegro5/internal/aintern_aatree.h"
 
+struct DelInfo
+{
+   const void *key;
+   _al_cmp_t compare;
+   Aatree *last;
+   Aatree *deleted;
+};
+
 static Aatree nil = { 0, &nil, &nil, NULL, NULL };
 
 static Aatree *skew(Aatree *T)
@@ -101,6 +109,69 @@ void *_al_aa_search(const Aatree *T, const void *key, _al_cmp_t compare)
       T = (cmp < 0) ? T->left : T->right;
    }
    return NULL;
+}
+
+static Aatree *dodelete(struct DelInfo *info, Aatree *T, void **ret_value)
+{
+   if (T == &nil)
+      return T;
+
+   /* Search down the tree and set pointers last and deleted. */
+   info->last = T;
+   if (info->compare(info->key, T->key) < 0) {
+      T->left = dodelete(info, T->left, ret_value);
+   }
+   else {
+      info->deleted = T;
+      T->right = dodelete(info, T->right, ret_value);
+   }
+
+   /* At the bottom of the tree we remove the element if it is present. */
+   if (T == info->last &&
+      info->deleted != &nil &&
+      info->compare(info->key, info->deleted->key) == 0)
+   {
+      Aatree *right = T->right;
+      *ret_value = info->deleted->value;
+      info->deleted->key = T->key;
+      info->deleted->value = T->value;
+      info->deleted = &nil;
+      al_free(T);
+      return right;
+   }
+
+   /* On the way back, we rebalance. */
+   if (T->left->level < T->level - 1
+      || T->right->level < T->level - 1)
+   {
+      T->level--;
+      if (T->right->level > T->level) {
+         T->right->level = T->level;
+      }
+      T = skew(T);
+      T->right = skew(T->right);
+      T->right->right = skew(T->right->right);
+      T = split(T);
+      T->right = split(T->right);
+   }
+   return T;
+}
+
+Aatree *_al_aa_delete(Aatree *T, const void *key, _al_cmp_t compare,
+   void **ret_value)
+{
+   struct DelInfo info;
+   info.key = key;
+   info.compare = compare;
+   info.last = &nil;
+   info.deleted = &nil;
+
+   if (T) {
+      T = dodelete(&info, T, ret_value);
+      if (T == &nil)
+         T = NULL;
+   }
+   return T;
 }
 
 void _al_aa_free(Aatree *T)
