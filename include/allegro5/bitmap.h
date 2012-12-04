a9ab527efc6580ba2048422068e1e99bfcadf68c
Peter Wang <tjaden@users.sourceforge.net>
2012-12-04 22:56:29 +1100

    Separate blending mode function prototypes out of bitmap.h

--- include/allegro5/bitmap.h
+++ include/allegro5/bitmap.h
@@ -67,28 +67,6 @@ enum {
 };
 
 
-/*
- * Blending modes
- */
-enum ALLEGRO_BLEND_MODE {
-   ALLEGRO_ZERO = 0,
-   ALLEGRO_ONE = 1,
-   ALLEGRO_ALPHA = 2,
-   ALLEGRO_INVERSE_ALPHA = 3,
-   ALLEGRO_SRC_COLOR = 4,
-   ALLEGRO_DST_COLOR = 5,
-   ALLEGRO_INVERSE_SRC_COLOR = 6,
-   ALLEGRO_INVERSE_DST_COLOR = 7
-};
-
-enum ALLEGRO_BLEND_OPERATIONS {
-   ALLEGRO_ADD = 0,
-   ALLEGRO_SRC_MINUS_DEST = 1,
-   ALLEGRO_DEST_MINUS_SRC = 2,
-   ALLEGRO_NUM_BLEND_OPERATIONS
-};
-
-
 AL_FUNC(void, al_set_new_bitmap_format, (int format));
 AL_FUNC(void, al_set_new_bitmap_flags, (int flags));
 AL_FUNC(int, al_get_new_bitmap_format, (void));
@@ -141,14 +119,6 @@ AL_FUNC(ALLEGRO_BITMAP *, al_get_parent_bitmap, (ALLEGRO_BITMAP *bitmap));
 AL_FUNC(ALLEGRO_BITMAP *, al_clone_bitmap, (ALLEGRO_BITMAP *bitmap));
 AL_FUNC(void, al_convert_bitmap, (ALLEGRO_BITMAP *bitmap));
 AL_FUNC(void, al_convert_bitmaps, (void));
-
-/* Blending */
-AL_FUNC(void, al_set_blender, (int op, int source, int dest));
-AL_FUNC(void, al_get_blender, (int *op, int *source, int *dest));
-AL_FUNC(void, al_set_separate_blender, (int op, int source, int dest,
-   int alpha_op, int alpha_source, int alpha_dest));
-AL_FUNC(void, al_get_separate_blender, (int *op, int *source, int *dest,
-   int *alpha_op, int *alpha_src, int *alpha_dest));
 
 AL_FUNC(void, _al_put_pixel, (ALLEGRO_BITMAP *bitmap, int x, int y, ALLEGRO_COLOR color));
 
