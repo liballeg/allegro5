a8a57adcb098c2a59fd1a81ea6793c247904ca57
Peter Wang <tjaden@users.sourceforge.net>
2012-12-10 23:31:41 +1100

    ttf: Avoid redundant al_get_current_display calls.
    Also improve code style.

--- addons/ttf/ttf.c
+++ addons/ttf/ttf.c
@@ -443,11 +443,14 @@ static int render_glyph(ALLEGRO_FONT const *f,
    FT_Face face = data->face;
    ALLEGRO_TTF_GLYPH_DATA *glyph = get_glyph(data, ft_index);
    int advance = 0;
+   ALLEGRO_DISPLAY *display;
    ALLEGRO_TRANSFORM old_projection_transform;
 
    /* Workabout for bug 3484535 */
-   if (al_get_current_display() != NULL){
-      al_copy_transform(&old_projection_transform, al_get_projection_transform(al_get_current_display()));
+   display = al_get_current_display();
+   if (display) {
+      al_copy_transform(&old_projection_transform,
+         al_get_projection_transform(display));
    }
 
    /* We don't try to cache all glyphs in a pre-pass before drawing them.
@@ -459,8 +462,8 @@ static int render_glyph(ALLEGRO_FONT const *f,
    cache_glyph(data, face, ft_index, glyph, false);
 
    /* Workabout for bug 3484535 */
-   if (al_get_current_display() != NULL){
-      al_set_projection_transform(al_get_current_display(), &old_projection_transform);
+   if (display) {
+      al_set_projection_transform(display, &old_projection_transform);
    }
 
    advance += get_kerning(data, face, prev_ft_index, ft_index);
