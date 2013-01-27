c378bbb15fa0c3d80f203c7963960e09b7d9f585
Peter Wang <tjaden@users.sourceforge.net>
2013-01-28 09:49:00 +1100

    Fix late assertion in win_shutdown.
    Reported by Max Savenkov.
    
    (cherry picked from commit 0945a4ace670cbeffc0031804ff4ac0da896d7cc)

--- src/win/wsystem.c
+++ src/win/wsystem.c
@@ -183,8 +183,11 @@ static ALLEGRO_SYSTEM *win_initialize(int flags)
 
 static void win_shutdown(void)
 {
+   ALLEGRO_SYSTEM *s;
+   ASSERT(vt);
+
    /* Close all open displays. */
-   ALLEGRO_SYSTEM *s = al_get_system_driver();
+   s = al_get_system_driver();
    while (_al_vector_size(&s->displays) > 0) {
       ALLEGRO_DISPLAY **dptr = _al_vector_ref(&s->displays, 0);
       ALLEGRO_DISPLAY *d = *dptr;
@@ -204,7 +207,6 @@ static void win_shutdown(void)
       timeEndPeriod(1);
    }
 
-   ASSERT(vt);
    al_free(vt);
    vt = NULL;
 
