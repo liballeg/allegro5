9c8b71d5b796e4cf0e66f58e0f7d289ee0564b52
Trent Gamblin <trentg@users.sourceforge.net>
2013-02-11 23:11:13 -0700

    Allow command-tab to work in fullscreen window mode on OS X. Also makes native message boxes show up correctly.
    (cherry picked from commit dba94a9111713fcfc6fb98db4cb4d131ce26f4b5)

--- src/macosx/osxgl.m
+++ src/macosx/osxgl.m
@@ -1905,7 +1905,9 @@ static bool set_display_flag(ALLEGRO_DISPLAY *display, int flag, bool onoff)
 
       case ALLEGRO_FULLSCREEN_WINDOW:
          if (onoff) {
-            [[win contentView] enterFullScreenMode: [win screen] withOptions: nil];
+            NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
+	    [dict setObject:[NSNumber numberWithInt:NSApplicationPresentationHideDock | NSApplicationPresentationHideMenuBar] forKey:NSFullScreenModeApplicationPresentationOptions];
+            [[win contentView] enterFullScreenMode: [win screen] withOptions: dict];
             NSRect sc = [[win screen] frame];
             resize_display_win(display, sc.size.width, sc.size.height);
             display->flags |= ALLEGRO_FULLSCREEN_WINDOW;
