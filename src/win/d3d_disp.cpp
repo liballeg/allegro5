dd6ec5f345b5793528b45fbbc786433a7f916aa8
Trent Gamblin <trentg@users.sourceforge.net>
2013-02-19 18:51:55 -0700

    It was bothering me that the bitmap struct never got zeroed.

--- src/win/d3d_disp.cpp
+++ src/win/d3d_disp.cpp
@@ -2339,6 +2339,8 @@ ALLEGRO_BITMAP *_al_d3d_create_bitmap(ALLEGRO_DISPLAY *d,
    ASSERT(bitmap);
    (void)h;
 
+   memset(bitmap, 0, sizeof(*bitmap));
+
    format = al_get_new_bitmap_format();
    flags = al_get_new_bitmap_flags();
 
