18745f96c80eb3cbb9fc9e4f0b19a51ad11e3be0
Peter Wang <tjaden@users.sourceforge.net>
2013-03-01 23:39:13 +1100

    cmake: Avoid duplicate rules to copy example/data to build dir.
    This received warnings from the Ninja build tool, which can be used with
    recent versions of CMake.
    
    (cherry picked from commit ace9770daa18694f4fb9dfe7b626806fce03fd51)

--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -244,7 +244,7 @@ endfunction(add_our_executable)
 #
 # Files are only copied if they don't are inside a .svn folder so we
 # won't end up with read-only .svn folders in the build folder.
-function(copy_data_dir_to_build target name)
+function(copy_data_dir_to_build target name destination)
     if("${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_BINARY_DIR}")
         return()
     endif()
@@ -265,7 +265,7 @@ function(copy_data_dir_to_build target name)
 
     foreach(file ${files})
         add_custom_command(
-            OUTPUT ${file}
+            OUTPUT "${destination}/${file}"
             DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${file}"
             COMMAND "${CMAKE_COMMAND}" -E copy
                     "${CMAKE_CURRENT_SOURCE_DIR}/${file}" ${file}
