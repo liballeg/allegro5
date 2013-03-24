b7fffa2fd03c39baf68fb7b824f259959c93f6e1
Peter Wang <tjaden@users.sourceforge.net>
2013-03-24 15:04:38 +1100

    Bump version to 5.1.6 (WIP).

--- include/allegro5/base.h
+++ include/allegro5/base.h
@@ -65,11 +65,11 @@
  * Note x.y.z (= x.y.z.0) has release number 1, and x.y.z.1 has release
  * number 2, just to confuse you.
  */
-#define ALLEGRO_RELEASE_NUMBER   0
+#define ALLEGRO_RELEASE_NUMBER   1
 
-#define ALLEGRO_VERSION_STR      "5.1.6 (GIT)"
+#define ALLEGRO_VERSION_STR      "5.1.6 (WIP)"
 #define ALLEGRO_DATE_STR         "2013"
-#define ALLEGRO_DATE             20130113    /* yyyymmdd */
+#define ALLEGRO_DATE             20130324    /* yyyymmdd */
 #define ALLEGRO_VERSION_INT \
     ((ALLEGRO_VERSION << 24) | (ALLEGRO_SUB_VERSION << 16) | \
     (ALLEGRO_WIP_VERSION << 8) | ALLEGRO_RELEASE_NUMBER)
