9d0acce18c6409b8113846030c03247aa5c2d7dd
Peter Wang <tjaden@users.sourceforge.net>
2013-04-13 13:44:54 +1000

    Delete declaration of non-existent _al_bitmap_xglx_driver.

--- include/allegro5/platform/aintxglx.h
+++ include/allegro5/platform/aintxglx.h
@@ -3,6 +3,5 @@
 
 ALLEGRO_DISPLAY_INTERFACE *_al_display_xglx_driver(void);
 ALLEGRO_SYSTEM_INTERFACE *_al_system_xglx_driver(void);
-ALLEGRO_BITMAP_INTERFACE *_al_bitmap_xglx_driver(void);
 
 #endif
