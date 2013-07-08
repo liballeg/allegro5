ab7c5203c9921b184f473828084dcc857db06ec7
Trent Gamblin <trentg@users.sourceforge.net>
2013-07-07 23:45:38 -0600

    Simplify run.sh and modify it for the new library structure

--- android/example/run.sh
+++ android/example/run.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-rsync --progress --exclude='*.svn*' -r -t ~/projects/allegro-5.1/android-project/ ~/projects/alandroid-project/
-
-cp ~/build/android-toolchain/user/armeabi-v7a/lib/liballegro-debug.so jni/armeabi-v7a/
-cp ~/build/android-toolchain/user/armeabi-v7a/lib/liballegro_primitives-debug.so jni/armeabi-v7a/
-cp ~/build/android-toolchain/user/armeabi-v7a/lib/liballegro_image-debug.so jni/armeabi-v7a/
-
-export ANDROID_NDK_TOOLCHAIN_ROOT=~/build/android-toolchain
-V=1 ~/build/android-ndk/ndk-build clean
-V=1 ~/build/android-ndk/ndk-build
-
-ant clean
+echo build
+ndk-build
 ant debug
-
 echo uninstall
 adb -d uninstall org.liballeg.app
 echo install
 adb -d install bin/alandroid-project-debug.apk
 echo start
-adb -d shell 'am start -a android.intent.action.MAIN -n org.liballeg.app/.AllegroActivity'
-
+adb -d shell 'am start -a android.intent.action.MAIN -n org.liballeg.example/.ExampleActivity'
