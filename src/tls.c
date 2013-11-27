liballeg/allegro5

commit 7ae440bd51c4f3172fd0c1143f290686d3d632ea

Optimise al_restore_state if saved target display/bitmap are unchanged.

src/tls.c
@@ -696,13 +696,15 @@ void al_restore_state(ALLEGRO_STATE const *state)
    }
 
    if (flags & ALLEGRO_STATE_DISPLAY) {
-      _RESTORE(current_display);
-      _al_set_current_display_only(tls->current_display);
+      if (tls->current_display != stored->tls.current_display) {
+         _al_set_current_display_only(tls->current_display);
+      }
    }
 
    if (flags & ALLEGRO_STATE_TARGET_BITMAP) {
-      _RESTORE(target_bitmap);
-      al_set_target_bitmap(tls->target_bitmap);
+      if (tls->target_bitmap != stored->tls.target_bitmap) {
+         al_set_target_bitmap(tls->target_bitmap);
+      }
    }
 
    if (flags & ALLEGRO_STATE_BLENDER) {

commit a780e406a2b9f1aadb6ba671cd30036a0aa1975f

Fix al_restore_state after last optimisation.
Broken in commit 7ae440b
"Optimise al_restore_state if saved target display/bitmap are unchanged."

src/tls.c
@@ -697,13 +697,15 @@ void al_restore_state(ALLEGRO_STATE const *state)
 
    if (flags & ALLEGRO_STATE_DISPLAY) {
       if (tls->current_display != stored->tls.current_display) {
-         _al_set_current_display_only(tls->current_display);
+         _al_set_current_display_only(stored->tls.current_display);
+         ASSERT(tls->current_display == stored->tls.current_display);
       }
    }
 
    if (flags & ALLEGRO_STATE_TARGET_BITMAP) {
       if (tls->target_bitmap != stored->tls.target_bitmap) {
-         al_set_target_bitmap(tls->target_bitmap);
+         al_set_target_bitmap(stored->tls.target_bitmap);
+         ASSERT(tls->target_bitmap == stored->tls.target_bitmap);
       }
    }
 