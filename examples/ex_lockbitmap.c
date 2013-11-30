f213e4dfc6f6ddb3b9628567f77e9d0262de9e83
Peter Wang <tjaden@users.sourceforge.net>
2013-11-30 19:00:54 +1100

    ex_lockbitmap: Refactor.

--- examples/ex_lockbitmap.c
+++ examples/ex_lockbitmap.c
@@ -8,74 +8,18 @@
 
 #include "common.c"
 
-int main(int argc, char **argv)
+enum Mode
 {
-   ALLEGRO_DISPLAY *display;
-   ALLEGRO_BITMAP *bitmap;
+   MODE_VIDEO,
+   MODE_MEMORY,
+   MODE_BACKBUFFER
+};
+
+static void fill(ALLEGRO_BITMAP *bitmap, int lock_flags)
+{
    ALLEGRO_LOCKED_REGION *locked;
    uint8_t *ptr;
    int i, j;
-   ALLEGRO_EVENT_QUEUE *events;
-   ALLEGRO_EVENT event;
-   int mode = 0;
-   int lock_flags = ALLEGRO_LOCK_WRITEONLY;
-
-   (void)argc;
-   (void)argv;
-
-   if (!al_init()) {
-      abort_example("Could not init Allegro.\n");
-   }
-
-   al_install_keyboard();
-
-   open_log();
-
-   /* Create a window. */
-   display = al_create_display(3*256, 256);
-   if (!display) {
-      abort_example("Error creating display\n");
-   }
-
-   events = al_create_event_queue();
-   al_register_event_source(events, al_get_keyboard_event_source());
-
-   log_printf("Press space to change bitmap type\n");
-   log_printf("Press w to toggle WRITEONLY mode\n");
-
-restart:
-
-   /* Create the bitmap to lock, or use the display backbuffer. */
-   if (mode == 0) {
-      log_printf("Locking video bitmap");
-      al_clear_to_color(al_map_rgb(0, 0, 0));
-      al_set_new_bitmap_flags(ALLEGRO_VIDEO_BITMAP);
-      bitmap = al_create_bitmap(3*256, 256);
-   }
-   else if (mode == 1) {
-      log_printf("Locking memory bitmap");
-      al_clear_to_color(al_map_rgb(0, 0, 0));
-      al_set_new_bitmap_flags(ALLEGRO_MEMORY_BITMAP);
-      bitmap = al_create_bitmap(3*256, 256);
-   }
-   else {
-      log_printf("Locking display backbuffer");
-      bitmap = al_get_backbuffer(display);
-   }
-   if (!bitmap) {
-      abort_example("Error creating bitmap");
-   }
-
-   if (lock_flags & ALLEGRO_LOCK_WRITEONLY) {
-      log_printf(" in write-only mode\n");
-   }
-   else {
-      log_printf(" in read/write mode\n");
-   }
-
-   al_set_target_bitmap(bitmap);
-   al_clear_to_color(al_map_rgb_f(0.8, 0.8, 0.9));
-   al_set_target_backbuffer(display);
 
    /* Locking the bitmap means, we work directly with pixel data.  We can
     * choose the format we want to work with, which may imply conversions, or
@@ -86,6 +30,9 @@ restart:
     */
    locked = al_lock_bitmap_region(bitmap, 193, 65, 3*127, 127,
       ALLEGRO_PIXEL_FORMAT_RGB_565, lock_flags);
+   if (!locked)
+      return;
+
    for (j = 0; j < 127; j++) {
       ptr = (uint8_t *)locked->data + j * locked->pitch;
 
@@ -124,31 +71,122 @@ restart:
       }
    }
    al_unlock_bitmap(bitmap);
+}
 
-   if (mode < 2) {
+static void draw(ALLEGRO_DISPLAY *display, enum Mode mode, int lock_flags)
+{
+   ALLEGRO_BITMAP *bitmap;
+
+   /* Create the bitmap to lock, or use the display backbuffer. */
+   if (mode == MODE_VIDEO) {
+      log_printf("Locking video bitmap");
+      al_clear_to_color(al_map_rgb(0, 0, 0));
+      al_set_new_bitmap_flags(ALLEGRO_VIDEO_BITMAP);
+      bitmap = al_create_bitmap(3*256, 256);
+   }
+   else if (mode == MODE_MEMORY) {
+      log_printf("Locking memory bitmap");
+      al_clear_to_color(al_map_rgb(0, 0, 0));
+      al_set_new_bitmap_flags(ALLEGRO_MEMORY_BITMAP);
+      bitmap = al_create_bitmap(3*256, 256);
+   }
+   else {
+      log_printf("Locking display backbuffer");
+      bitmap = al_get_backbuffer(display);
+   }
+   if (!bitmap) {
+      abort_example("Error creating bitmap");
+   }
+
+   if (lock_flags & ALLEGRO_LOCK_WRITEONLY) {
+      log_printf(" in write-only mode\n");
+   }
+   else {
+      log_printf(" in read/write mode\n");
+   }
+
+   al_set_target_bitmap(bitmap);
+   al_clear_to_color(al_map_rgb_f(0.8, 0.8, 0.9));
+   al_set_target_backbuffer(display);
+
+   fill(bitmap, lock_flags);
+
+   if (mode != MODE_BACKBUFFER) {
       al_draw_bitmap(bitmap, 0, 0, 0);
       al_destroy_bitmap(bitmap);
       bitmap = NULL;
    }
 
    al_flip_display();
+}
 
-   while (1) {
+static enum Mode cycle_mode(enum Mode mode)
+{
+   switch (mode) {
+      case MODE_VIDEO:
+         return MODE_MEMORY;
+      case MODE_MEMORY:
+         return MODE_BACKBUFFER;
+      case MODE_BACKBUFFER:
+      default:
+         return MODE_VIDEO;
+   }
+}
+
+static int toggle_writeonly(int lock_flags)
+{
+   return lock_flags ^ ALLEGRO_LOCK_WRITEONLY;
+}
+
+int main(int argc, char **argv)
+{
+   ALLEGRO_DISPLAY *display;
+   ALLEGRO_EVENT_QUEUE *events;
+   ALLEGRO_EVENT event;
+   enum Mode mode = MODE_VIDEO;
+   int lock_flags = ALLEGRO_LOCK_WRITEONLY;
+   bool redraw = true;
+
+   (void)argc;
+   (void)argv;
+
+   if (!al_init()) {
+      abort_example("Could not init Allegro.\n");
+   }
+
+   al_install_keyboard();
+
+   open_log();
+
+   /* Create a window. */
+   display = al_create_display(3*256, 256);
+   if (!display) {
+      abort_example("Error creating display\n");
+   }
+
+   events = al_create_event_queue();
+   al_register_event_source(events, al_get_keyboard_event_source());
+
+   log_printf("Press space to change bitmap type\n");
+   log_printf("Press w to toggle WRITEONLY mode\n");
+
+   for (;;) {
+      if (redraw) {
+         draw(display, mode, lock_flags);
+         redraw = false;
+      }
+
       al_wait_for_event(events, &event);
-
       if (event.type == ALLEGRO_EVENT_KEY_CHAR) {
          if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
             break;
-
          if (event.keyboard.unichar == ' ') {
-            if (++mode > 2)
-               mode = 0;
-            goto restart;
+            mode = cycle_mode(mode);
+            redraw = true;
          }
-
-         if (event.keyboard.unichar == 'w' || event.keyboard.unichar == 'W') {
-            lock_flags ^= ALLEGRO_LOCK_WRITEONLY;
-            goto restart;
+         else if (event.keyboard.unichar == 'w' || event.keyboard.unichar == 'W') {
+            lock_flags = toggle_writeonly(lock_flags);
+            redraw = true;
          }
       }
    }
