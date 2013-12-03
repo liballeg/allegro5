liballeg/allegro5

commit 691ba74d0febbeb2350aacab243557257e376b31

Invert and replace ALLEGRO_CFG_NO_GLES2 by ALLEGRO_CFG_OPENGLES2.

CMakeLists.txt
@@ -532,14 +532,12 @@ if(WANT_OPENGL)
         set(ALLEGRO_CFG_OPENGLES 1)
         set(OPENGL_LIBRARIES ${OPENGLES_LIBRARIES})
         if(OPENGLES2_FOUND)
+            set(ALLEGRO_CFG_OPENGLES2 1)
             list(APPEND OPENGL_LIBRARIES ${OPENGLES2_LIBRARIES})
-        else()
-            set(ALLEGRO_CFG_NO_GLES2 1)
         endif()
     elseif(OPENGL_FOUND)
         set(SUPPORT_OPENGL 1)
         set(ALLEGRO_CFG_OPENGL 1)
-        set(ALLEGRO_CFG_NO_GLES2 1)
     endif()
 endif(WANT_OPENGL)
 

addons/primitives/prim_opengl.c
@@ -117,7 +117,7 @@ static void setup_state(const char* vtxs, const ALLEGRO_VERTEX_DECL* decl, ALLEG
    bool normalized;
 
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifndef ALLEGRO_CFG_NO_GLES2
+#ifdef ALLEGRO_CFG_OPENGLES2
       if(decl) {
          ALLEGRO_VERTEX_ELEMENT* e;
          int i;
@@ -287,7 +287,7 @@ static void setup_state(const char* vtxs, const ALLEGRO_VERTEX_DECL* decl, ALLEG
       }
 
       if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifndef ALLEGRO_CFG_NO_GLES2
+#ifdef ALLEGRO_CFG_OPENGLES2
          GLint handle;
 
          handle = display->ogl_extras->varlocs.tex_matrix_loc;
@@ -330,7 +330,7 @@ static void revert_state(ALLEGRO_BITMAP* texture)
 
    if(texture) {
       if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifndef ALLEGRO_CFG_NO_GLES2
+#ifdef ALLEGRO_CFG_OPENGLES2
          float identity[16] = {
             1, 0, 0, 0,
             0, 1, 0, 0,
@@ -357,7 +357,7 @@ static void revert_state(ALLEGRO_BITMAP* texture)
    }
 
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifndef ALLEGRO_CFG_NO_GLES2
+#ifdef ALLEGRO_CFG_OPENGLES2
       if (display->ogl_extras->varlocs.pos_loc >= 0)
          glDisableVertexAttribArray(display->ogl_extras->varlocs.pos_loc);
       if (display->ogl_extras->varlocs.color_loc >= 0)

cmake/Toolchain-raspberrypi.cmake
@@ -28,6 +28,7 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 SET(ALLEGRO_CFG_OPENGLES 1)
+SET(ALLEGRO_CFG_OPENGLES2 1)
 
 SET(ALLEGRO_EXCLUDE_GLX 1)
 

include/allegro5/platform/alplatf.h.cmake
@@ -19,7 +19,7 @@
 #cmakedefine ALLEGRO_CFG_SHADER_HLSL
 
 #cmakedefine ALLEGRO_CFG_OPENGLES
-#cmakedefine ALLEGRO_CFG_NO_GLES2
+#cmakedefine ALLEGRO_CFG_OPENGLES2
 #cmakedefine ALLEGRO_CFG_ANDROID_LEGACY
 
 /*---------------------------------------------------------------------------*/

src/android/android_display.c
@@ -419,7 +419,7 @@ static ALLEGRO_DISPLAY *android_create_display(int w, int h)
    ALLEGRO_DEBUG("begin");
 
    int flags = al_get_new_display_flags();
-#ifdef ALLEGRO_NO_GLES2
+#ifndef ALLEGRO_CFG_OPENGLES2
    if (flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
       return NULL;
    }

src/opengl/ogl_bitmap.c
@@ -179,7 +179,7 @@ char const *_al_gl_error_string(GLenum e)
       ERR(GL_STACK_OVERFLOW)
       ERR(GL_STACK_UNDERFLOW)
       ERR(GL_OUT_OF_MEMORY)
-#ifndef ALLEGRO_CFG_NO_GLES2
+#ifdef ALLEGRO_CFG_OPENGLES2
       ERR(GL_INVALID_FRAMEBUFFER_OPERATION)
 #endif
    }

src/opengl/ogl_draw.c
@@ -84,7 +84,7 @@ static void vert_ptr_on(ALLEGRO_DISPLAY *display, int n, GLint t, int stride, vo
 {
 /* Only use this shader stuff with GLES2+ or equivalent */
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifndef ALLEGRO_CFG_NO_GLES2
+#ifdef ALLEGRO_CFG_OPENGLES2
       if (display->ogl_extras->varlocs.pos_loc >= 0) {
          glVertexAttribPointer(display->ogl_extras->varlocs.pos_loc, n, t, false, stride, v);
          glEnableVertexAttribArray(display->ogl_extras->varlocs.pos_loc);
@@ -100,7 +100,7 @@ static void vert_ptr_on(ALLEGRO_DISPLAY *display, int n, GLint t, int stride, vo
 static void vert_ptr_off(ALLEGRO_DISPLAY *display)
 {
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifndef ALLEGRO_CFG_NO_GLES2
+#ifdef ALLEGRO_CFG_OPENGLES2
       if (display->ogl_extras->varlocs.pos_loc >= 0) {
          glDisableVertexAttribArray(display->ogl_extras->varlocs.pos_loc);
       }
@@ -114,7 +114,7 @@ static void vert_ptr_off(ALLEGRO_DISPLAY *display)
 static void color_ptr_on(ALLEGRO_DISPLAY *display, int n, GLint t, int stride, void *v)
 {
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifndef ALLEGRO_CFG_NO_GLES2
+#ifdef ALLEGRO_CFG_OPENGLES2
       if (display->ogl_extras->varlocs.color_loc >= 0) {
          glVertexAttribPointer(display->ogl_extras->varlocs.color_loc, n, t, false, stride, v);
          glEnableVertexAttribArray(display->ogl_extras->varlocs.color_loc);
@@ -130,7 +130,7 @@ static void color_ptr_on(ALLEGRO_DISPLAY *display, int n, GLint t, int stride, v
 static void color_ptr_off(ALLEGRO_DISPLAY *display)
 {
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifndef ALLEGRO_CFG_NO_GLES2
+#ifdef ALLEGRO_CFG_OPENGLES2
       if (display->ogl_extras->varlocs.color_loc >= 0) {
          glDisableVertexAttribArray(display->ogl_extras->varlocs.color_loc);
       }
@@ -144,7 +144,7 @@ static void color_ptr_off(ALLEGRO_DISPLAY *display)
 static void tex_ptr_on(ALLEGRO_DISPLAY *display, int n, GLint t, int stride, void *v)
 {
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifndef ALLEGRO_CFG_NO_GLES2
+#ifdef ALLEGRO_CFG_OPENGLES2
       if (display->ogl_extras->varlocs.texcoord_loc >= 0) {
          glVertexAttribPointer(display->ogl_extras->varlocs.texcoord_loc, n, t, false, stride, v);
          glEnableVertexAttribArray(display->ogl_extras->varlocs.texcoord_loc);
@@ -160,7 +160,7 @@ static void tex_ptr_on(ALLEGRO_DISPLAY *display, int n, GLint t, int stride, voi
 static void tex_ptr_off(ALLEGRO_DISPLAY *display)
 {
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifndef ALLEGRO_CFG_NO_GLES2
+#ifdef ALLEGRO_CFG_OPENGLES2
       if (display->ogl_extras->varlocs.texcoord_loc >= 0) {
          glDisableVertexAttribArray(display->ogl_extras->varlocs.texcoord_loc);
       }
@@ -330,7 +330,7 @@ static void ogl_flush_vertex_cache(ALLEGRO_DISPLAY *disp)
       return;
 
    if (disp->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifndef ALLEGRO_CFG_NO_GLES2
+#ifdef ALLEGRO_CFG_OPENGLES2
       if (disp->ogl_extras->varlocs.use_tex_loc >= 0) {
          glUniform1i(disp->ogl_extras->varlocs.use_tex_loc, 1);
       }
@@ -346,7 +346,7 @@ static void ogl_flush_vertex_cache(ALLEGRO_DISPLAY *disp)
    glGetIntegerv(GL_TEXTURE_BINDING_2D, (GLint*)&current_texture);
    if (current_texture != disp->cache_texture) {
       if (disp->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifndef ALLEGRO_CFG_NO_GLES2
+#ifdef ALLEGRO_CFG_OPENGLES2
          /* Use texture unit 0 */
          glActiveTexture(GL_TEXTURE0);
          if (disp->ogl_extras->varlocs.tex_loc >= 0)
@@ -446,7 +446,7 @@ static void ogl_flush_vertex_cache(ALLEGRO_DISPLAY *disp)
    disp->num_cache_vertices = 0;
 
    if (disp->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifndef ALLEGRO_CFG_NO_GLES2
+#ifdef ALLEGRO_CFG_OPENGLES2
       if (disp->ogl_extras->varlocs.use_tex_loc >= 0)
          glUniform1i(disp->ogl_extras->varlocs.use_tex_loc, 0);
 #endif

include/allegro5/allegro_opengl.h
@@ -63,7 +63,7 @@
 #include <GLES/gl.h>
 #include <GLES/glext.h>
 
-#ifndef ALLEGRO_CFG_NO_GLES2
+#ifdef ALLEGRO_CFG_OPENGLES2
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 #else

commit d0395e5e20223ee7aa02ed57bffdd415526aebdf

Restore OpenGL programmable pipeline support.
Programmable pipeline support with OpenGL (not OpenGLES) was broken in
commit 691ba74d0febbeb2350aacab243557257e376b31.

All the programmable pipeline support was hidden behind

    #ifndef ALLEGRO_CFG_NO_GLES2

which when trivially inverted became

    #ifdef ALLEGRO_CFG_OPENGLES2

but ALLEGRO_CFG_NO_GLES2 was also NOT defined for OpenGL proper.

CMakeLists.txt
@@ -537,11 +537,14 @@ if(WANT_OPENGL)
         set(OPENGL_LIBRARIES ${OPENGLES_LIBRARIES})
         if(OPENGLES2_FOUND)
             set(ALLEGRO_CFG_OPENGLES2 1)
+            set(ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE 1)
             list(APPEND OPENGL_LIBRARIES ${OPENGLES2_LIBRARIES})
         endif()
     elseif(OPENGL_FOUND)
         set(SUPPORT_OPENGL 1)
         set(ALLEGRO_CFG_OPENGL 1)
+        # We assume that the OpenGL library is not very old.
+        set(ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE 1)
     endif()
 endif(WANT_OPENGL)
 

addons/primitives/prim_opengl.c
@@ -117,7 +117,7 @@ static void setup_state(const char* vtxs, const ALLEGRO_VERTEX_DECL* decl, ALLEG
    bool normalized;
 
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifdef ALLEGRO_CFG_OPENGLES2
+#ifdef ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
       if(decl) {
          ALLEGRO_VERTEX_ELEMENT* e;
          int i;
@@ -287,7 +287,7 @@ static void setup_state(const char* vtxs, const ALLEGRO_VERTEX_DECL* decl, ALLEG
       }
 
       if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifdef ALLEGRO_CFG_OPENGLES2
+#ifdef ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
          GLint handle;
 
          handle = display->ogl_extras->varlocs.tex_matrix_loc;
@@ -330,7 +330,7 @@ static void revert_state(ALLEGRO_BITMAP* texture)
 
    if(texture) {
       if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifdef ALLEGRO_CFG_OPENGLES2
+#ifdef ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
          float identity[16] = {
             1, 0, 0, 0,
             0, 1, 0, 0,
@@ -357,7 +357,7 @@ static void revert_state(ALLEGRO_BITMAP* texture)
    }
 
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifdef ALLEGRO_CFG_OPENGLES2
+#ifdef ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
       if (display->ogl_extras->varlocs.pos_loc >= 0)
          glDisableVertexAttribArray(display->ogl_extras->varlocs.pos_loc);
       if (display->ogl_extras->varlocs.color_loc >= 0)

cmake/Toolchain-raspberrypi.cmake
@@ -29,6 +29,7 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 SET(ALLEGRO_CFG_OPENGLES 1)
 SET(ALLEGRO_CFG_OPENGLES2 1)
+SET(ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE 1)
 
 SET(ALLEGRO_EXCLUDE_GLX 1)
 

include/allegro5/platform/alplatf.h.cmake
@@ -2,9 +2,6 @@
 #cmakedefine ALLEGRO_MINGW32
 #cmakedefine ALLEGRO_UNIX
 #cmakedefine ALLEGRO_MSVC
-#cmakedefine ALLEGRO_CFG_D3D
-#cmakedefine ALLEGRO_CFG_D3D9EX
-#cmakedefine ALLEGRO_CFG_OPENGL
 #cmakedefine ALLEGRO_MACOSX
 #cmakedefine ALLEGRO_BCC32
 #cmakedefine ALLEGRO_IPHONE
@@ -15,11 +12,15 @@
 #cmakedefine ALLEGRO_CFG_PTHREADS_TLS
 #cmakedefine ALLEGRO_CFG_RELEASE_LOGGING
 
+#cmakedefine ALLEGRO_CFG_D3D
+#cmakedefine ALLEGRO_CFG_D3D9EX
+#cmakedefine ALLEGRO_CFG_OPENGL
+#cmakedefine ALLEGRO_CFG_OPENGLES
+#cmakedefine ALLEGRO_CFG_OPENGLES2
+#cmakedefine ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
 #cmakedefine ALLEGRO_CFG_SHADER_GLSL
 #cmakedefine ALLEGRO_CFG_SHADER_HLSL
 
-#cmakedefine ALLEGRO_CFG_OPENGLES
-#cmakedefine ALLEGRO_CFG_OPENGLES2
 #cmakedefine ALLEGRO_CFG_ANDROID_LEGACY
 
 /*---------------------------------------------------------------------------*/

src/android/android_display.c
@@ -419,7 +419,7 @@ static ALLEGRO_DISPLAY *android_create_display(int w, int h)
    ALLEGRO_DEBUG("begin");
 
    int flags = al_get_new_display_flags();
-#ifndef ALLEGRO_CFG_OPENGLES2
+#ifndef ALLEGRO_CFG_OPENGLES_PROGRAMMABLE_PIPELINE
    if (flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
       return NULL;
    }

src/opengl/ogl_bitmap.c
@@ -179,7 +179,7 @@ char const *_al_gl_error_string(GLenum e)
       ERR(GL_STACK_OVERFLOW)
       ERR(GL_STACK_UNDERFLOW)
       ERR(GL_OUT_OF_MEMORY)
-#ifdef ALLEGRO_CFG_OPENGLES2
+#ifdef ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
       ERR(GL_INVALID_FRAMEBUFFER_OPERATION)
 #endif
    }

src/opengl/ogl_draw.c
@@ -84,7 +84,7 @@ static void vert_ptr_on(ALLEGRO_DISPLAY *display, int n, GLint t, int stride, vo
 {
 /* Only use this shader stuff with GLES2+ or equivalent */
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifdef ALLEGRO_CFG_OPENGLES2
+#ifdef ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
       if (display->ogl_extras->varlocs.pos_loc >= 0) {
          glVertexAttribPointer(display->ogl_extras->varlocs.pos_loc, n, t, false, stride, v);
          glEnableVertexAttribArray(display->ogl_extras->varlocs.pos_loc);
@@ -100,7 +100,7 @@ static void vert_ptr_on(ALLEGRO_DISPLAY *display, int n, GLint t, int stride, vo
 static void vert_ptr_off(ALLEGRO_DISPLAY *display)
 {
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifdef ALLEGRO_CFG_OPENGLES2
+#ifdef ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
       if (display->ogl_extras->varlocs.pos_loc >= 0) {
          glDisableVertexAttribArray(display->ogl_extras->varlocs.pos_loc);
       }
@@ -114,7 +114,7 @@ static void vert_ptr_off(ALLEGRO_DISPLAY *display)
 static void color_ptr_on(ALLEGRO_DISPLAY *display, int n, GLint t, int stride, void *v)
 {
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifdef ALLEGRO_CFG_OPENGLES2
+#ifdef ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
       if (display->ogl_extras->varlocs.color_loc >= 0) {
          glVertexAttribPointer(display->ogl_extras->varlocs.color_loc, n, t, false, stride, v);
          glEnableVertexAttribArray(display->ogl_extras->varlocs.color_loc);
@@ -130,7 +130,7 @@ static void color_ptr_on(ALLEGRO_DISPLAY *display, int n, GLint t, int stride, v
 static void color_ptr_off(ALLEGRO_DISPLAY *display)
 {
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifdef ALLEGRO_CFG_OPENGLES2
+#ifdef ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
       if (display->ogl_extras->varlocs.color_loc >= 0) {
          glDisableVertexAttribArray(display->ogl_extras->varlocs.color_loc);
       }
@@ -144,7 +144,7 @@ static void color_ptr_off(ALLEGRO_DISPLAY *display)
 static void tex_ptr_on(ALLEGRO_DISPLAY *display, int n, GLint t, int stride, void *v)
 {
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifdef ALLEGRO_CFG_OPENGLES2
+#ifdef ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
       if (display->ogl_extras->varlocs.texcoord_loc >= 0) {
          glVertexAttribPointer(display->ogl_extras->varlocs.texcoord_loc, n, t, false, stride, v);
          glEnableVertexAttribArray(display->ogl_extras->varlocs.texcoord_loc);
@@ -160,7 +160,7 @@ static void tex_ptr_on(ALLEGRO_DISPLAY *display, int n, GLint t, int stride, voi
 static void tex_ptr_off(ALLEGRO_DISPLAY *display)
 {
    if (display->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifdef ALLEGRO_CFG_OPENGLES2
+#ifdef ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
       if (display->ogl_extras->varlocs.texcoord_loc >= 0) {
          glDisableVertexAttribArray(display->ogl_extras->varlocs.texcoord_loc);
       }
@@ -330,7 +330,7 @@ static void ogl_flush_vertex_cache(ALLEGRO_DISPLAY *disp)
       return;
 
    if (disp->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifdef ALLEGRO_CFG_OPENGLES2
+#ifdef ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
       if (disp->ogl_extras->varlocs.use_tex_loc >= 0) {
          glUniform1i(disp->ogl_extras->varlocs.use_tex_loc, 1);
       }
@@ -346,7 +346,7 @@ static void ogl_flush_vertex_cache(ALLEGRO_DISPLAY *disp)
    glGetIntegerv(GL_TEXTURE_BINDING_2D, (GLint*)&current_texture);
    if (current_texture != disp->cache_texture) {
       if (disp->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifdef ALLEGRO_CFG_OPENGLES2
+#ifdef ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
          /* Use texture unit 0 */
          glActiveTexture(GL_TEXTURE0);
          if (disp->ogl_extras->varlocs.tex_loc >= 0)
@@ -356,7 +356,8 @@ static void ogl_flush_vertex_cache(ALLEGRO_DISPLAY *disp)
       glBindTexture(GL_TEXTURE_2D, disp->cache_texture);
    }
 
-#if !defined ALLEGRO_CFG_OPENGLES && !defined ALLEGRO_MACOSX
+   /* XXX what is this about? */
+#if defined(ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE) && !defined(ALLEGRO_MACOSX)
    if (disp->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
       int stride = sizeof(ALLEGRO_OGL_BITMAP_VERTEX);
       int bytes = disp->num_cache_vertices * stride;
@@ -446,7 +447,7 @@ static void ogl_flush_vertex_cache(ALLEGRO_DISPLAY *disp)
    disp->num_cache_vertices = 0;
 
    if (disp->flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
-#ifdef ALLEGRO_CFG_OPENGLES2
+#ifdef ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
       if (disp->ogl_extras->varlocs.use_tex_loc >= 0)
          glUniform1i(disp->ogl_extras->varlocs.use_tex_loc, 0);
 #endif