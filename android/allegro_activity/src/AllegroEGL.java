77acf94515f0afd1e210d009d6c6f379d1953925
Peter Wang <tjaden@users.sourceforge.net>
2013-12-10 23:21:15 +1100

    android: Delete unused checkGL20Support method.

--- android/allegro_activity/src/AllegroEGL.java
+++ android/allegro_activity/src/AllegroEGL.java
@@ -127,32 +127,6 @@ class AllegroEGL
       egl_attribWork.add(value);
    }
 
-   private boolean checkGL20Support(Context context)
-   {
-      EGL10 egl = (EGL10) EGLContext.getEGL();
-      //EGLDisplay display = egl.eglGetDisplay(EGL10.EGL_DEFAULT_DISPLAY);
-
-      //int[] version = new int[2];
-      //egl.eglInitialize(display, version);
-
-      int[] configAttribs =
-      {
-         EGL10.EGL_RED_SIZE, 4,
-         EGL10.EGL_GREEN_SIZE, 4,
-         EGL10.EGL_BLUE_SIZE, 4,
-         EGL10.EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
-         EGL10.EGL_NONE
-      };
-
-      EGLConfig[] configs = new EGLConfig[10];
-      int[] num_config = new int[1];
-      //egl.eglChooseConfig(display, configAttribs, configs, 10, num_config);
-      egl.eglChooseConfig(egl_Display, configAttribs, configs, 10, num_config);
-      //egl.eglTerminate(display);
-      Log.d(TAG, "" + num_config[0] + " OpenGL ES 2 configurations found.");
-      return num_config[0] > 0;
-   }
-
    /* Return values:
     * 0 - failure
     * 1 - success
