ee9c18350c94414be05b8e813cd89f99ac8f4ea9
Peter Wang <tjaden@users.sourceforge.net>
2013-12-12 19:34:48 +1100

    android: Fix typo in check for ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE.

--- src/android/android_display.c
+++ src/android/android_display.c
@@ -419,8 +419,9 @@ static ALLEGRO_DISPLAY *android_create_display(int w, int h)
    ALLEGRO_DEBUG("begin");
 
    int flags = al_get_new_display_flags();
-#ifndef ALLEGRO_CFG_OPENGLES_PROGRAMMABLE_PIPELINE
+#ifndef ALLEGRO_CFG_OPENGL_PROGRAMMABLE_PIPELINE
    if (flags & ALLEGRO_PROGRAMMABLE_PIPELINE) {
+      ALLEGRO_WARN("Programmable pipeline support not built.\n");
       return NULL;
    }
 #endif
