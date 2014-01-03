9db18cde1386b4a15639f5561dc7eb92571d8634
Peter Wang <tjaden@users.sourceforge.net>
2014-01-04 10:25:36 +1100

    docs: Delete considerations for older Pandoc.
    (cherry picked from commit cf03c926e07180e2fe8d8e8cbdf7652201b07ad7)
    
    Conflicts:
    	docs/CMakeLists.txt

--- docs/scripts/make_html_refs.c
+++ docs/scripts/make_html_refs.c
@@ -5,12 +5,6 @@
  * entry found.  e.g. if foo.txt contains "# API: bar" then we generate:
  *
  *   [bar]: foo.html#bar
- *
- * OPTIONS
- *
- * --strip-underscores
- *    Pandoc 1.2 and 1.2.1 strip underscores when generating HTML identifiers
- *    so we must do the same.
  */
 
 #include <string.h>
@@ -18,17 +12,10 @@
 
 int main(int argc, char *argv[])
 {
-   int strip_underscores = false;
    dstr line;
    dstr file;
    const char *name;
    dstr sec_id;
-
-   if (argc >= 2 && 0 == strcmp(argv[1], "--strip-underscores")) {
-      strip_underscores = true;
-      argc--;
-      argv++;
-   }
 
    d_init(argc, argv);
 
@@ -37,11 +24,7 @@ int main(int argc, char *argv[])
          d_basename(d_filename, ".html", file);
          name = d_after_match;
          d_tolower(name, sec_id);
-         if (strip_underscores) {
-            d_delchr(sec_id, '_');
-         }
-         /* The extra blank lines are to work around Pandoc issue #182. */
-         d_printf("[%s]: %s#%s\n\n", name, file, sec_id);
+         d_printf("[%s]: %s#%s\n", name, file, sec_id);
       }
    }
 
