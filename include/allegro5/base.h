628a6ce287516d4b8c76a75ca2f78d21b702813f
Peter Wang <tjaden@users.sourceforge.net>
2014-01-12 14:31:24 +1100

    Bump version to 5.1.9 (GIT).

--- include/allegro5/base.h
+++ include/allegro5/base.h
@@ -55,7 +55,7 @@
 
 #define ALLEGRO_VERSION          5
 #define ALLEGRO_SUB_VERSION      1
-#define ALLEGRO_WIP_VERSION      8
+#define ALLEGRO_WIP_VERSION      9
 
 /* Not sure we need it, but since ALLEGRO_VERSION_STR contains it:
  * 0 = GIT
@@ -65,9 +65,9 @@
  * Note x.y.z (= x.y.z.0) has release number 1, and x.y.z.1 has release
  * number 2, just to confuse you.
  */
-#define ALLEGRO_RELEASE_NUMBER   1
+#define ALLEGRO_RELEASE_NUMBER   0
 
-#define ALLEGRO_VERSION_STR      "5.1.8 (WIP)"
+#define ALLEGRO_VERSION_STR      "5.1.9 (GIT)"
 #define ALLEGRO_DATE_STR         "2014"
 #define ALLEGRO_DATE             20140112    /* yyyymmdd */
 #define ALLEGRO_VERSION_INT \
