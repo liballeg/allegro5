fcf1d144182c7c6682e4a5bd25b16a2ac65c00ba
Trent Gamblin <trentg@users.sourceforge.net>
2015-01-11 21:20:56 -0700

    Added include directory for preconfigured headers (iOS)

---
+++ misc/Allegro 5 iOS/allegro5/internal/aintern_image_cfg.h
@@ -0,0 +1,12 @@
+#define ALLEGRO_CFG_WANT_NATIVE_IMAGE_LOADER
+
+/* which libraries are present and needed? */
+/* #undef ALLEGRO_CFG_IIO_HAVE_GDIPLUS */
+/* #undef ALLEGRO_CFG_IIO_HAVE_GDIPLUS_LOWERCASE_H */
+/* #undef ALLEGRO_CFG_IIO_HAVE_ANDROID */
+/* #undef ALLEGRO_CFG_IIO_HAVE_PNG */
+/* #undef ALLEGRO_CFG_IIO_HAVE_JPG */
+
+/* which formats are supported and wanted? */
+#define ALLEGRO_CFG_IIO_SUPPORT_PNG
+#define ALLEGRO_CFG_IIO_SUPPORT_JPG
