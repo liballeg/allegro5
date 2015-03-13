ed9057c557750442cb5843e2ec1e1f674a13a4d6
Elias Pschernig <elias@users.sourceforge.net>
2015-03-13 21:12:26 +0100

    [docs] Make [section] links work in HTML.
    Cross-chapter links would not work in multi-page HTML output, so simply keep them in the html_refs file same as the API entries.

--- docs/scripts/make_html_refs.c
+++ docs/scripts/make_html_refs.c
@@ -10,6 +10,17 @@
 #include <string.h>
 #include "dawk.h"
 
+/* Replicate pandoc's function to create a label anchor from the section
+ * name.
+ */
+static void replace_spaces(char *s)
+{
+    while (*s) {
+        if (*s == ' ') *s = '-';
+        s++;
+    }
+}
+
 int main(int argc, char *argv[])
 {
    dstr line;
@@ -20,10 +31,11 @@ int main(int argc, char *argv[])
    d_init(argc, argv);
 
    while (d_getline(line)) {
-      if (d_match(line, "^#+ API: *")) {
+      if (d_match(line, "^#+( API:| ) *")) {
          d_basename(d_filename, ".html", file);
          name = d_after_match;
          d_tolower(name, sec_id);
+         replace_spaces(sec_id);
          d_printf("[%s]: %s#%s\n", name, file, sec_id);
       }
    }
