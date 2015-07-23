3a45cdcdcc6fc8c5d44c41b0026bc147846500ae
Elias Pschernig <elias@users.sourceforge.net>
2015-07-23 12:52:50 -0400

    added jekyll page

---
+++ _posts/con.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+import re
+
+for f in sys.argv:
+    rows = open(f).read().splitlines()
+    f2 = re.sub(r"^news\.", "", f)
+    f2 += "-news"
+    f2 += ".md"
+    o = open(f2, "w")
+    c = "---\n"
+    c += re.sub(".*-.*-.*-", "title:", rows[0])
+    c += "\n---\n"
+    c += "\n".join(rows[1:])
+    o.write(c)
+    print(f)
