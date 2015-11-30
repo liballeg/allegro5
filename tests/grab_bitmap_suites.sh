d3705f8d9b2c0d170bbe62d2f8a6d5d8f22975f1
Elias Pschernig <elias@users.sourceforge.net>
2015-11-29 21:20:51 -0500

    Added a script to download all the bitmaps used in BMP test suites.

---
+++ tests/grab_bitmap_suites.sh
@@ -0,0 +1,17 @@
+# bmp suite 1
+wget -N http://entropymine.com/jason/bmpsuite/bmpsuite.zip
+unzip -u -d bmpsuite bmpsuite.zip
+
+# bmp suite 2
+wget -N http://downloads.sourceforge.net/project/bmptestsuite/bmptestsuite/bmptestsuite-0.9/bmptestsuite-0.9.zip
+unzip -u bmptestsuite-0.9.zip
+
+# bmp suite 3
+wget -N -P wvnet http://circuitebre.cat/diplomes/www/test/images/bmp/test32bfv4.bmp
+wget -N -P wvnet http://circuitebre.cat/diplomes/www/test/images/bmp/test32v5.bmp
+wget -N -P wvnet http://circuitebre.cat/diplomes/www/test/images/bmp/trans.bmp
+
+# bmp suite 4
+wget -N http://entropymine.com/jason/bmpsuite/releases/bmpsuite-2.4.zip
+unzip -u bmpsuite-2.4.zip
+mv bmpsuite-2.4 bmpsuite2
