1632acaf7aa2787ac3b9716c7628b9a65eb7dfff
Pavel Sountsov <siege@google.com>
2016-03-20 10:48:10 -0700

    More ex_logo improvements.
    - Make it work with non-ASCII characters
    - Set the cursor to the end of the field rather than the beginning
    - Fix a crash when the logo text is empty

--- examples/ex_logo.c
+++ examples/ex_logo.c
@@ -336,6 +336,10 @@ static void render(void)
       }
       al_unlock_bitmap(fulllogo);
 
+      if (left == 640)
+         left = 0;
+      if (top == 480)
+         top = 0;
       if (right < left)
          right = left;
       if (bottom < top)
@@ -454,21 +458,21 @@ int main(int argc, char **argv)
                editing = false;
             }
             else {
-               cursor = 0;
+               cursor = strlen(param_values[selection]);
                editing = true;
             }
          }
          else if (event.keyboard.keycode == ALLEGRO_KEY_UP) {
             if (selection > 0) {
                selection--;
-               cursor = 0;
+               cursor = strlen(param_values[selection]);
                editing = false;
             }
          }
          else if (event.keyboard.keycode == ALLEGRO_KEY_DOWN) {
             if (param_names[selection + 1]) {
                selection++;
-               cursor = 0;
+               cursor = strlen(param_values[selection]);
                editing = false;
             }
          }
@@ -478,17 +482,17 @@ int main(int argc, char **argv)
                if (event.keyboard.keycode == ALLEGRO_KEY_BACKSPACE) {
                   if (cursor > 0) {
                      ALLEGRO_USTR *u = al_ustr_new(param_values[selection]);
-                     cursor--;
-                     al_ustr_remove_chr(u, cursor);
-                     strncpy(param_values[selection], al_cstr(u),
-                        sizeof param_values[selection]);
+                     if (al_ustr_prev(u, &cursor)) {
+                        al_ustr_remove_chr(u, cursor);
+                        strncpy(param_values[selection], al_cstr(u),
+                           sizeof param_values[selection]);
+                     }
                      al_ustr_free(u);
                   }
                }
                else if (c >= 32) {
                   ALLEGRO_USTR *u = al_ustr_new(param_values[selection]);
-                  al_ustr_set_chr(u, cursor, c);
-                  cursor++;
+                  cursor += al_ustr_set_chr(u, cursor, c);
                   al_ustr_set_chr(u, cursor, 0);
                   strncpy(param_values[selection], al_cstr(u),
                      sizeof param_values[selection]);
