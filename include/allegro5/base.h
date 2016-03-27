1c7235d3564b85e54fa9b88cbcf726c558362428
Pavel Sountsov <siege@google.com>
2016-03-27 15:27:32 -0700

    Bump version to 5.2.0 (GIT)

--- include/allegro5/base.h
+++ include/allegro5/base.h
@@ -54,8 +54,8 @@
 #endif
 
 #define ALLEGRO_VERSION          5
-#define ALLEGRO_SUB_VERSION      1
-#define ALLEGRO_WIP_VERSION      14
+#define ALLEGRO_SUB_VERSION      2
+#define ALLEGRO_WIP_VERSION      0
 
 #ifdef ALLEGRO_UNSTABLE
    #define ALLEGRO_UNSTABLE_BIT  1 << 31
@@ -73,9 +73,9 @@
  */
 #define ALLEGRO_RELEASE_NUMBER   0
 
-#define ALLEGRO_VERSION_STR      "5.1.14 (GIT)"
+#define ALLEGRO_VERSION_STR      "5.2.0 (GIT)"
 #define ALLEGRO_DATE_STR         "2016"
-#define ALLEGRO_DATE             20160124    /* yyyymmdd */
+#define ALLEGRO_DATE             20160327    /* yyyymmdd */
 #define ALLEGRO_VERSION_INT \
     ((ALLEGRO_VERSION << 24) | (ALLEGRO_SUB_VERSION << 16) | \
     (ALLEGRO_WIP_VERSION << 8) | ALLEGRO_RELEASE_NUMBER | \
