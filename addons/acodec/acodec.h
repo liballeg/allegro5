8ab9d1fb2f6dce8bf981285be50a940cb49ec218
Boris Carvajal <boris2.9@gmail.com>
2016-04-17 00:15:42 -0300

    Opus codec support.

--- addons/acodec/acodec.h
+++ addons/acodec/acodec.h
@@ -58,6 +58,14 @@ ALLEGRO_AUDIO_STREAM *_al_load_ogg_vorbis_audio_stream_f(ALLEGRO_FILE* file,
    size_t buffer_count, unsigned int samples);
 #endif
 
+#ifdef ALLEGRO_CFG_ACODEC_OPUS
+ALLEGRO_SAMPLE *_al_load_ogg_opus(const char *filename);
+ALLEGRO_SAMPLE *_al_load_ogg_opus_f(ALLEGRO_FILE *file);
+ALLEGRO_AUDIO_STREAM *_al_load_ogg_opus_audio_stream(const char *filename,
+   size_t buffer_count, unsigned int samples);
+ALLEGRO_AUDIO_STREAM *_al_load_ogg_opus_audio_stream_f(ALLEGRO_FILE* file,
+   size_t buffer_count, unsigned int samples);
+#endif
 
 
 #endif
