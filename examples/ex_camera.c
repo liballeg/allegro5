ff0681e958672bae03e29341d5be1b2a90e9f873
Hyena <hyena@hyena.net.ee>
2016-08-26 02:18:19 +0300

    Disable DEPTH_TEST Render State Before Drawing HUD (#669)
    Disabled ALLEGRO_DEPTH_TEST render state before drawing HUD related graphics in ex_camera. Without this line geometry is sometimes drawn on top of the HUD. Update contributors.

--- examples/ex_camera.c
+++ examples/ex_camera.c
@@ -315,6 +315,7 @@ static void draw_scene(void)
    al_identity_transform(&t);
    al_use_transform(&t);
    al_use_projection_transform(&projection);
+   al_set_render_state(ALLEGRO_DEPTH_TEST, 0);
 
    /* Draw some text. */
    th = al_get_font_line_height(ex.font);
@@ -402,7 +403,6 @@ static void handle_input(void)
          camera_move_along_direction(&ex.camera, ex.movement_speed * x,
             ex.movement_speed * y);
       }
-         
    }
 
    /* Rotate the camera, either freely or around world up only. */
