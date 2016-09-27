bf88da910f1c955db60f70cb8bf46aaf4d932d8a
Elias Pschernig <elias@users.sourceforge.net>
2016-09-27 00:26:39 -0400

    add two Android convienience functions
    al_android_get_jni_env: returns Allegro's JNI environment
    al_android_get_activity: returns Allegro's Activity

--- include/allegro5/allegro_android.h
+++ include/allegro5/allegro_android.h
@@ -14,6 +14,8 @@
 #ifndef A5_ANDROID_ALLEGRO_H
 #define A5_ANDROID_ALLEGRO_H
 
+#include <jni.h>
+
 #ifdef __cplusplus
    extern "C" {
 #endif
@@ -24,6 +26,8 @@
 void al_android_set_apk_file_interface(void);
 const char *al_android_get_os_version(void);
 void al_android_set_apk_fs_interface(void);
+JNIEnv *al_android_get_jni_env(void);
+jobject al_android_get_activity(void);
 
 /* XXX decide if this should be public */
 void _al_android_set_capture_volume_keys(ALLEGRO_DISPLAY *display, bool onoff);
