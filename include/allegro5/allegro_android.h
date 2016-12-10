0889097ed7bf7cbf94a44aa3d961c1823c997892
Pavel Sountsov <siege@google.com>
2016-12-09 22:38:01 -0800

    Destabilize the new Android API.

--- include/allegro5/allegro_android.h
+++ include/allegro5/allegro_android.h
@@ -26,8 +26,10 @@
 void al_android_set_apk_file_interface(void);
 const char *al_android_get_os_version(void);
 void al_android_set_apk_fs_interface(void);
+#if defined(ALLEGRO_UNSTABLE) || defined(ALLEGRO_INTERNAL_UNSTABLE) || defined(ALLEGRO_SRC)
 JNIEnv *al_android_get_jni_env(void);
 jobject al_android_get_activity(void);
+#endif
 
 /* XXX decide if this should be public */
 void _al_android_set_capture_volume_keys(ALLEGRO_DISPLAY *display, bool onoff);
