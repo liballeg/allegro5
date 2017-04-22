8194e5ec310c1649586998b4759bdc01ae9a77d3
Tom Bass <tom.bass@gmx.net>
2017-04-22 07:28:34 +0200

    added activation flag

--- src/macosx/system.m
+++ src/macosx/system.m
@@ -66,7 +66,7 @@ static void osx_tell_dock(void)
 {
    ProcessSerialNumber psn = { 0, kCurrentProcess };
    TransformProcessType(&psn, kProcessTransformToForegroundApplication);
-   [NSApplication sharedApplication];
+   [[NSApplication sharedApplication] activateIgnoringOtherApps: YES];
 }
 
 
