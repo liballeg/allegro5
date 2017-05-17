71a38151f3d499f5d1b93511759f7f3e8ae853d7
Ivan Mogilko <ikm_spb@yahoo.com>
2017-05-18 00:00:48 +0300

    Make use of <stdint.h> in MSVS 2010 and higher

--- include/allegro/platform/almsvc.h
+++ include/allegro/platform/almsvc.h
@@ -78,8 +78,12 @@
 #define INLINE       __inline
 
 #define LONG_LONG    __int64
-#define int64_t      signed __int64
-#define uint64_t     unsigned __int64
+#if (_MSC_VER >= 1600)
+   #define ALLEGRO_HAVE_STDINT_H
+#else
+   #define int64_t      signed __int64
+   #define uint64_t     unsigned __int64
+#endif
 
 #define AL_CONST     const
 
