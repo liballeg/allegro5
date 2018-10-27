46c99fc5b5a7795c874f9de848c812c0fb4db9a3
Sebastian Krzyszkowiak <dos@dosowisko.net>
2018-10-28 01:16:32 +0200

    SDL: implement touch subsystem

--- src/sdl/sdl_system.c
+++ src/sdl/sdl_system.c
@@ -73,6 +73,11 @@ static void sdl_heartbeat(void)
          case SDL_MOUSEWHEEL:
             _al_sdl_mouse_event(&event);
             break;
+         case SDL_FINGERDOWN:
+         case SDL_FINGERMOTION:
+         case SDL_FINGERUP:
+             _al_sdl_touch_input_event(&event);
+             break;
          case SDL_JOYAXISMOTION:
          case SDL_JOYBUTTONDOWN:
          case SDL_JOYBUTTONUP:
@@ -191,6 +196,11 @@ static ALLEGRO_MOUSE_DRIVER *sdl_get_mouse_driver(void)
    return _al_sdl_mouse_driver();
 }
 
+static ALLEGRO_TOUCH_INPUT_DRIVER *sdl_get_touch_input_driver(void)
+{
+   return _al_sdl_touch_input_driver();
+}
+
 static ALLEGRO_JOYSTICK_DRIVER *sdl_get_joystick_driver(void)
 {
    return _al_sdl_joystick_driver();
@@ -275,7 +285,7 @@ ALLEGRO_SYSTEM_INTERFACE *_al_sdl_system_driver(void)
    vt->get_display_driver = sdl_get_display_driver;
    vt->get_keyboard_driver = sdl_get_keyboard_driver;
    vt->get_mouse_driver = sdl_get_mouse_driver;
-   //vt->get_touch_input_driver = sdl_get_touch_input_driver;
+   vt->get_touch_input_driver = sdl_get_touch_input_driver;
    vt->get_joystick_driver = sdl_get_joystick_driver;
    //vt->get_haptic_driver = sdl_get_haptic_driver;
    vt->get_num_display_modes = sdl_get_num_display_modes;
