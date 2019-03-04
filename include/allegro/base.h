87e764d98d4070bd8d41056a021434da97e1e45d
Pavel Sountsov <siege@google.com>
2019-03-03 17:13:33 -0800

    Bump version to 4.4.4 (SVN)

--- include/allegro/base.h
+++ include/allegro/base.h
@@ -46,8 +46,8 @@
 
 #define ALLEGRO_VERSION          4
 #define ALLEGRO_SUB_VERSION      4
-#define ALLEGRO_WIP_VERSION      3
-#define ALLEGRO_VERSION_STR      "4.4.3"
+#define ALLEGRO_WIP_VERSION      4
+#define ALLEGRO_VERSION_STR      "4.4.4 (SVN)"
 #define ALLEGRO_DATE_STR         "2019"
 #define ALLEGRO_DATE             20190303    /* yyyymmdd */
 
