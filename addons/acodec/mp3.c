3805a6954b67a9515cb43a92b5e72bece386d6f1
Pavel Sountsov <siege@google.com>
2019-04-29 21:34:53 -0700

    Fix MP3 compillation, warnings.

--- addons/acodec/mp3.c
+++ addons/acodec/mp3.c
@@ -60,9 +60,9 @@ ALLEGRO_SAMPLE *_al_load_mp3_f(ALLEGRO_FILE *f)
    }
 
    // Allocate buffer and read all the file
-   uint8_t* mp3data = (uint8_t*)al_malloc(fsize);
-   size_t readbytes = al_fread(f, mp3data, fsize);
-   if (readbytes != fsize) {
+   uint8_t* mp3data = (uint8_t*)al_malloc(filesize);
+   size_t readbytes = al_fread(f, mp3data, filesize);
+   if (readbytes != (size_t)filesize) {
       ALLEGRO_WARN("Failed to read file into memory.\n");
       al_free(mp3data);
       return NULL;
@@ -100,6 +100,9 @@ ALLEGRO_AUDIO_STREAM *_al_load_mp3_audio_stream(const char *filename, size_t buf
 
 ALLEGRO_AUDIO_STREAM *_al_load_mp3_audio_stream_f(ALLEGRO_FILE* f, size_t buffer_count, unsigned int samples)
 {
+   (void)f;
+   (void)buffer_count;
+   (void)samples;
    mp3_initminimp3(); // Make sure library is initialised
    return NULL;
 }
