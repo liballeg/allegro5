a36a8a6f66482524ba0ba9cd6ecd8a7376526884
Todd Cope <todd@t3-i.com>
2023-04-25 15:15:59 -0500

    Use 'mach_absolute_time()' when targeting MacOSX versions < 10.12 to prevent runtime error. Fixes #1434.

--- src/unix/utime.c
+++ src/unix/utime.c
@@ -32,13 +32,33 @@ ALLEGRO_STATIC_ASSERT(utime,
 struct timespec _al_unix_initial_time;
 
 
+/* implement clock_gettime() for MacOS < 10.12 */
+#if defined(ALLEGRO_MACOSX) && MAC_OS_X_VERSION_MIN_REQUIRED < 101200
+#include <mach/mach_time.h>
+int _internal_clock_gettime(clockid_t clock_id, struct timespec* t)
+{
+    mach_timebase_info_data_t timebase;
+    mach_timebase_info(&timebase);
+    uint64_t time;
+    time = mach_absolute_time();
+    double nseconds = ((double)time * (double)timebase.numer)/((double)timebase.denom);
+    double seconds = ((double)time * (double)timebase.numer)/((double)timebase.denom * 1e9);
+    t->tv_sec = seconds;
+    t->tv_nsec = nseconds;
+    return 0;
+}
+#define _al_clock_gettime _internal_clock_gettime
+#else
+#define _al_clock_gettime clock_gettime
+#endif
+
 
 /* _al_unix_init_time:
  *  Called by the system driver to mark the beginning of time.
  */
 void _al_unix_init_time(void)
 {
-   clock_gettime(CLOCK_REALTIME, &_al_unix_initial_time);
+   _al_clock_gettime(CLOCK_REALTIME, &_al_unix_initial_time);
 }
 
 
@@ -48,7 +68,7 @@ double _al_unix_get_time(void)
    struct timespec now;
    double time;
 
-   clock_gettime(CLOCK_REALTIME, &now);
+   _al_clock_gettime(CLOCK_REALTIME, &now);
    time = (double) (now.tv_sec - _al_unix_initial_time.tv_sec)
       + (double) (now.tv_nsec - _al_unix_initial_time.tv_nsec) * 1.0e-9;
    return time;
@@ -79,7 +99,7 @@ void _al_unix_init_timeout(ALLEGRO_TIMEOUT *timeout, double seconds)
 
    ASSERT(ut);
 
-   clock_gettime(CLOCK_REALTIME, &now);
+   _al_clock_gettime(CLOCK_REALTIME, &now);
 
    if (seconds <= 0.0) {
       ut->abstime.tv_sec = now.tv_sec;
