5114ee60410c9aac6a0f23402570eb41a4cc9095
Pavel Sountsov <siege@google.com>
2024-09-15 13:41:55 -0700

    Improve ex_timedwait

--- examples/ex_timedwait.c
+++ examples/ex_timedwait.c
@@ -6,11 +6,12 @@
 
 
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
 
 #include "common.c"
 
-static void test_relative_timeout(ALLEGRO_EVENT_QUEUE *queue);
-static void test_absolute_timeout(ALLEGRO_EVENT_QUEUE *queue);
+static bool test_relative_timeout(ALLEGRO_FONT *font, ALLEGRO_EVENT_QUEUE *queue);
+static bool test_absolute_timeout(ALLEGRO_FONT *font, ALLEGRO_EVENT_QUEUE *queue);
 
 
 
@@ -18,6 +19,7 @@ int main(int argc, char **argv)
 {
    ALLEGRO_DISPLAY *dpy;
    ALLEGRO_EVENT_QUEUE *queue;
+   ALLEGRO_FONT *font;
 
    (void)argc;
    (void)argv;
@@ -25,26 +27,39 @@ int main(int argc, char **argv)
    if (!al_init()) {
       abort_example("Could not init Allegro.\n");
    }
+   if (!al_init_font_addon()) {
+      abort_example("Could not init Font addon.\n");
+   }
 
    al_install_keyboard();
 
    dpy = al_create_display(640, 480);
    if (!dpy) {
-      abort_example("Unable to set any graphic mode\n");
+      abort_example("Could not create a display.\n");
+   }
+
+   font = al_create_builtin_font();
+   if (!font) {
+      abort_example("Could not create builtin font.\n");
    }
 
    queue = al_create_event_queue();
    al_register_event_source(queue, al_get_keyboard_event_source());
+   al_register_event_source(queue, al_get_display_event_source(dpy));
 
-   test_relative_timeout(queue);
-   test_absolute_timeout(queue);
+   while (true) {
+      if (!test_relative_timeout(font, queue))
+         break;
+      if (!test_absolute_timeout(font, queue))
+         break;
+   }
 
    return 0;
 }
 
 
 
-static void test_relative_timeout(ALLEGRO_EVENT_QUEUE *queue)
+static bool test_relative_timeout(ALLEGRO_FONT *font, ALLEGRO_EVENT_QUEUE *queue)
 {
    ALLEGRO_EVENT event;
    float shade = 0.1;
@@ -53,11 +68,14 @@ static void test_relative_timeout(ALLEGRO_EVENT_QUEUE *queue)
       if (al_wait_for_event_timed(queue, &event, 0.1)) {
          if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
             if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
-               return;
+               return true;
             }
             else {
-               shade = 0.1;
+               shade = 0.0;
             }
+         }
+         else if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+            return false;
          }
       }
       else {
@@ -68,13 +86,15 @@ static void test_relative_timeout(ALLEGRO_EVENT_QUEUE *queue)
       }
 
       al_clear_to_color(al_map_rgba_f(0.5 * shade, 0.25 * shade, shade, 0));
+      al_draw_textf(font, al_map_rgb_f(1., 1., 1.), 320, 240, ALLEGRO_ALIGN_CENTRE, "Relative timeout (%.2f)", shade);
+      al_draw_text(font, al_map_rgb_f(1., 1., 1.), 320, 260, ALLEGRO_ALIGN_CENTRE, "Esc to move on, Space to restart.");
       al_flip_display();
    }
 }
 
 
 
-static void test_absolute_timeout(ALLEGRO_EVENT_QUEUE *queue)
+static bool test_absolute_timeout(ALLEGRO_FONT *font, ALLEGRO_EVENT_QUEUE *queue)
 {
    ALLEGRO_TIMEOUT timeout;
    ALLEGRO_EVENT event;
@@ -86,10 +106,13 @@ static void test_absolute_timeout(ALLEGRO_EVENT_QUEUE *queue)
       while ((ret = al_wait_for_event_until(queue, &event, &timeout))) {
          if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
             if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
-               return;
+               return true;
             } else {
-               shade = 0.1;
+               shade = 0.;
             }
+         }
+         else if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+            return false;
          }
       }
 
@@ -101,6 +124,8 @@ static void test_absolute_timeout(ALLEGRO_EVENT_QUEUE *queue)
       }
 
       al_clear_to_color(al_map_rgba_f(shade, 0.5 * shade, 0.25 * shade, 0));
+      al_draw_textf(font, al_map_rgb_f(1., 1., 1.), 320, 240, ALLEGRO_ALIGN_CENTRE, "Absolute timeout (%.2f)", shade);
+      al_draw_text(font, al_map_rgb_f(1., 1., 1.), 320, 260, ALLEGRO_ALIGN_CENTRE, "Esc to move on, Space to restart.");
       al_flip_display();
    }
 }
