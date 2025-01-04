8f89deacd8e4a947eb1d53a0c2e46ccbe5fb36cd
Pavel Sountsov <siege@google.com>
2025-01-04 11:47:11 -0800

    Point to liballeg.org rather than tuxfamily for the bitmap suite

--- tests/grab_bitmap_suites.sh
+++ tests/grab_bitmap_suites.sh
@@ -1,17 +1,17 @@
 # bmp suite 1
-wget -N https://download.tuxfamily.org/allegro/files/bmpsuite/bmpsuite.zip
+wget -N https://liballeg.org/files/bmpsuite/bmpsuite.zip
 unzip -u -d bmpsuite bmpsuite.zip
 
 # bmp suite 2
-wget -N https://download.tuxfamily.org/allegro/files/bmpsuite/bmptestsuite-0.9.zip
+wget -N https://liballeg.org/files/bmpsuite/bmptestsuite-0.9.zip
 unzip -u bmptestsuite-0.9.zip
 
 # bmp suite 3
-wget -N -P wvnet https://download.tuxfamily.org/allegro/files/bmpsuite/wvnet/test32bfv4.bmp
-wget -N -P wvnet https://download.tuxfamily.org/allegro/files/bmpsuite/wvnet/test32v5.bmp
-wget -N -P wvnet https://download.tuxfamily.org/allegro/files/bmpsuite/wvnet/trans.bmp
+wget -N -P wvnet https://liballeg.org/files/bmpsuite/wvnet/test32bfv4.bmp
+wget -N -P wvnet https://liballeg.org/files/bmpsuite/wvnet/test32v5.bmp
+wget -N -P wvnet https://liballeg.org/files/bmpsuite/wvnet/trans.bmp
 
 # bmp suite 4
-wget -N https://download.tuxfamily.org/allegro/files/bmpsuite/bmpsuite-2.4.zip
+wget -N https://liballeg.org/files/bmpsuite/bmpsuite-2.4.zip
 unzip -u bmpsuite-2.4.zip
 mv bmpsuite-2.4 bmpsuite2
