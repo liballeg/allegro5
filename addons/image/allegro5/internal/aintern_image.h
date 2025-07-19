0d2c8a85bbc37fb4e451e26d12542b0267d3f906
Pavel Sountsov <siege@google.com>
2025-07-19 15:09:30 -0700

    Remove the native MacOS image loader.
    It is too limited and is causing issues, please use libpng and libjpeg
    libraries instead.
    
    Fixes #1531

--- addons/image/allegro5/internal/aintern_image.h
+++ addons/image/allegro5/internal/aintern_image.h
@@ -16,10 +16,6 @@ ALLEGRO_IIO_FUNC(ALLEGRO_BITMAP *, _al_iphone_load_image, (const char *filename,
 ALLEGRO_IIO_FUNC(ALLEGRO_BITMAP *, _al_iphone_load_image_f, (ALLEGRO_FILE *f, int flags));
 #endif
 
-#ifdef ALLEGRO_MACOSX
-ALLEGRO_IIO_FUNC(bool, _al_osx_register_image_loader, (void));
-#endif
-
 #endif
 
 ALLEGRO_IIO_FUNC(ALLEGRO_BITMAP *, _al_load_pcx, (const char *filename, int flags));
